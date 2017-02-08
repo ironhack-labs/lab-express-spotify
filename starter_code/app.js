5f32bab5da62293a8e4d26671ea0a2d320c543a1
Gonzalo Sanz <gonzalo.orio@gmail.com>
2017-02-09 00:24:57 +0100

    iteration 1 step 2 not finish

---
+++ starter_code/app.js
@@ -0,0 +1,82 @@
+/*jshint esversion:6*/
+const SpotifyWebApi = require('spotify-web-api-node');
+const spotify = new SpotifyWebApi();
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.set('views', `views`);
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('index');
+});
+
+app.get('/', function(request, response) {
+  // const artists = request.body.artist;
+  spotify.searchArtists("The Beatles", {
+    limit: 3,
+    offset: 1
+  }).then(function(groups) {
+    response.render('artists', {
+      groups: groups
+    }); // to stuff here
+  }).catch(function(err) {
+    console.log(err);
+  });
+});
+
+
+// app.post('/', function(request, response) {
+//   // const artists = request.body.artist;
+//   spotify.searchArtists("The Beatles", {}, (err, data) => {
+//     if (err) throw err;
+//
+//     let artists = data.body.artists.items;
+//     console.log(artists);
+//   });
+// });
+
+// app.post('/', function(request, response) {
+//   const artists = request.body.artist;
+//   spotify.searchArtists(function(artists) {
+//     response.send('artists', {
+//       artists: artists
+//     }, (err, data) => {
+//       if (err) throw err;
+//     });
+//   });
+// });
+
+// app.post('/', function(request, response) {
+//   const name = request.body.artists.name;
+//   response.send('artists');
+// });
+
+app.get('/artists', function(request, response) {
+  response.render('artists');
+});
+
+spotify.searchArtists("The Beatles", {}, (err, data) => {
+  if (err) throw err;
+
+  let artists = data.body.artists.items;
+  console.log(artists);
+});
+
+
+
+
+app.listen(3005, () => {
+  console.log('Spotify Express!');
+});
