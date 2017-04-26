2d7de9872dad30d59391641edc1fb2c1e94f53d8
Isak La Fleur <isak.engdahl@gmail.com>
2017-04-26 06:57:06 +0200

    setup express server.

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+// index route
+app.get('/', (request, response, next) => {
+  console.log(request);
+  response.send('Hello Isak');
+});
+
+// start server
+app.listen(3000, () => console.log('The server has started'));
+
+/*
 const SpotifyWebApi = require('spotify-web-api-node');
 
 const spotify = new SpotifyWebApi();
@@ -8,3 +22,4 @@ spotify.searchArtists('The Beatles', {}, (err, data) => {
   let artists = data.body.artists.items;
   console.log(artists);
 });
+*/
