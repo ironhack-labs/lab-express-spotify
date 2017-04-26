d11ca6674395cc038cd84323e7a6c1fdbdbac757
Isak La Fleur <isak.engdahl@gmail.com>
2017-04-26 13:21:01 +0200

    added track and album files.

--- app.js
+++ app.js
@@ -1,39 +1,65 @@
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
+const spotify = new SpotifyWebApi();
+
+// instruct the app to use the `bodyParser()` middleware for all routes
+// http://stackoverflow.com/questions/24330014/bodyparser-is-deprecated-express-4
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
 
 app.use(express.static('public'));
+app.use(morgan('dev'));
 
-app.use(expressLayouts);
-app.set('layout', 'layouts/main-layout');
-app.set('views', __dirname + '/views');
+app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
 
 // Index Route:
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
 
 // Artist Route:
-app.get('/artists', (request, response, next) => {
-  response.send('Welcome to the Artist Page');
-  next();
+app.post('/artists/', (req, res) => {
+  const artistName = req.body.artist;
+  spotify.searchArtists(artistName, {}, (err, data) => {
+    if (err) throw err;
+    const artists = data.body.artists.items;
+    //console.log(artists[0]);
+    res.render('artists', { artists });
+  });
+});
+
+// Album Route:
+app.get('/albums/:artistId', (req, res) => {
+  // const artistid2 = req.body;
+  const artistId = '2ye2Wgw4gimLv2eAKyk1NB';
+  spotify.getArtistAlbums(artistId, {}, (err, data) => {
+    if (err) throw err;
+    // console.log(data.body);
+    // console.log(req.body.name);
+    const albums = data.body;
+    res.render('albums', { albums });
+  });
+});
+
+// Track
+app.get('/tracks/:trackid', (req, res) => {
+  const albumId = '4kwN2OnnrwY2ZBcm379Ahn';
+  spotify.getAlbumTracks(albumId, {}, (err, data) => {
+    if (err) throw err;
+    console.log(data.body);
+    const tracks = data.body;
+    console.log(tracks);
+    res.render('tracks', { tracks });
+  });
 });
 
 // start server
 app.listen(3000, () => console.log('The server has started'));
-
-/*
-const SpotifyWebApi = require('spotify-web-api-node');
-
-const spotify = new SpotifyWebApi();
-
-spotify.searchArtists('The Beatles', {}, (err, data) => {
-  if (err) throw err;
-
-  let artists = data.body.artists.items;
-  console.log(artists);
-});
-*/
