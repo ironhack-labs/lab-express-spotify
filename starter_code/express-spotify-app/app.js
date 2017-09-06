350bfe9da0ed671d138123fa34271346f1d2cd3c
Philip Wisner <philipwisner@gmail.com>
2017-09-06 18:25:16 +0200

    added credentials

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,6 +1,23 @@
 const SpotifyWebApi   = require('spotify-web-api-node');
 const spotify         = new SpotifyWebApi();
 
+// Remember to paste here your credentials
+var clientId = '8f1e69c9802e435e997e1983ec9348d0',
+    clientSecret = '4037307a11fd4f2ebc93931ea9b99e25';
+
+var spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+// Retrieve an access token.
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
+});
+
 spotify.searchArtists("The Beatles", {}, (err, data) => {
   if (err) throw err;
 
