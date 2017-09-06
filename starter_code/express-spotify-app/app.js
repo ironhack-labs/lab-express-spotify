00a38dcab98b31e2f0e50c5da5b81e10a0d25c39
raquel500c <raquel500c@gmail.com>
2017-09-07 01:05:23 +0200

    Iteration 2 | Express Setup - Done

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,12 +1,29 @@
-var SpotifyWebApi = require('spotify-web-api-node');
+const SpotifyWebApi = require('spotify-web-api-node');
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const prettyjson = require('prettyjson');
 
-// Remember to paste here your credentials
-var clientId = '1c30624cba6742dcb792991caecae571',
-    clientSecret = '746977b1e77240faa9d0d2411c3e0efe';
+// Express server handling requests and responses
+const app = express();
 
-var spotifyApi = new SpotifyWebApi({
-  clientId : clientId,
-  clientSecret : clientSecret
+app.use(expressLayouts);
+app.set('layout', 'main-layout');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.listen(3000);
+
+// My credentials:
+const clientId = '2e99a7bf2af347c1a21b1ec96e578658';
+const clientSecret = 'c4065fa08f3f4f20bd000f0613fda3b5';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret
 });
 
 // Retrieve an access token.
@@ -15,4 +32,4 @@ spotifyApi.clientCredentialsGrant()
     spotifyApi.setAccessToken(data.body['access_token']);
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
-});
+  });
