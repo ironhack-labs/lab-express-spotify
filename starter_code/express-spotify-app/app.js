6edb7d2e5cba5bf8c3a23a1c020006975cdffbee
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-09-06 23:12:49 +0200

    no funciona nada

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,8 +1,24 @@
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+
+app.use(expressLayouts);
+app.set('layout', 'index');
+
+app.set('views', __dirname + '/views/layouts');
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
 var SpotifyWebApi = require('spotify-web-api-node');
 
-// Remember to paste here your credentials
-var clientId = '1c30624cba6742dcb792991caecae571',
-    clientSecret = '746977b1e77240faa9d0d2411c3e0efe';
+var clientId = '768cc38921184612a70af965b367911b',
+    clientSecret = 'd383d23da69d40c3b579f934debce4d3';
 
 var spotifyApi = new SpotifyWebApi({
   clientId : clientId,
@@ -15,4 +31,30 @@ spotifyApi.clientCredentialsGrant()
     spotifyApi.setAccessToken(data.body['access_token']);
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
+});
+
+
+app.get('/artist', (req, res) => {
+  res.render('index');
+});
+
+
+app.post('/artist', (req, res) => {
+  res.render('index');
+  spotifyApi.searchArtists(req.body.artist)
+  .then((response) => {
+    res.render('artist', {
+      artist: response
+    });
+  }).catch((err) => {
+    console.log("Unable to find the requested artist.");
+  });
+});
+
+
+
+
+let port = 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
 });
