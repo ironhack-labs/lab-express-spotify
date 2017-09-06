f4cd166d98ce5054851994ced5e7ac62a15eef20
VeroGlez <veronicagonzalezperez@MacBook-Pro-de-Veronica.local>
2017-09-07 00:54:34 +0200

    Iteration 3 done

---
+++ starter_code/express-spotify-app/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+var SpotifyWebApi = require('spotify-web-api-node');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+var clientId = 'd6402f66965a474594db4e0b75b53369',
+    clientSecret = 'b5e4aebd5a584e3c8693cee90843db0e';
+
+var spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+app.use(express.static('public'));
+app.use(expressLayouts);
+app.set('layout', 'layouts/main');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req,res) =>{
+  res.render('index');
+});
+
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
+});
+
+app.post('/artists', (req,res) =>{
+  spotifyApi.searchArtists(req.body.artistName)
+  .then((response) => {
+    res.render('artists', {
+      artistInfo: response.body.artists.items,
+    });
+  }).catch(err => {})
+});
+
+
+
+// Server Started
+let port = 3000;
+app.listen(port, () => {
+  console.log(`My first app listening on port ${port}!`)
+});
