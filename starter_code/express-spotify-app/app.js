2c471ab9876528bd71bc3e330a60a2f9eeb1dda6
Philip Wisner <philipwisner@gmail.com>
2017-09-06 19:09:48 +0200

    created artists

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,11 +1,23 @@
 const SpotifyWebApi   = require('spotify-web-api-node');
 const spotify         = new SpotifyWebApi();
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('layout', 'layouts/main-layout');
+
+
+app.use(express.static('public'));
+app.use(expressLayouts);
+
 
 // Remember to paste here your credentials
-var clientId = '8f1e69c9802e435e997e1983ec9348d0',
+const clientId = '8f1e69c9802e435e997e1983ec9348d0',
     clientSecret = '4037307a11fd4f2ebc93931ea9b99e25';
 
-var spotifyApi = new SpotifyWebApi({
+const spotifyApi = new SpotifyWebApi({
   clientId : clientId,
   clientSecret : clientSecret
 });
@@ -18,9 +30,21 @@ spotifyApi.clientCredentialsGrant()
     console.log('Something went wrong when retrieving an access token', err);
 });
 
-spotify.searchArtists("The Beatles", {}, (err, data) => {
-  if (err) throw err;
+app.get('/', (request, response, next) => {
+  response.render('index');
+})
 
-  let artists = data.body.artists.items;
-  console.log(artists)
-});
+app.get('/artists', (request, response, next) => {
+  response.render('artists');
+})
+
+app.listen(3000, () => console.log("HEYO"));
+
+
+
+// spotify.searchArtists("The Beatles", {}, (err, data) => {
+//   if (err) throw err;
+//
+//   let artists = data.body.artists.items;
+//   console.log(artists)
+// });
