746b84d21b71c72d20ae226b3204b6c1c0ded842
Philip Wisner <philipwisner@gmail.com>
2017-09-06 16:09:20 +0200

    added modules and created app.js

---
+++ starter_code/express-spotify-app/app.js
@@ -0,0 +1,9 @@
+const SpotifyWebApi   = require('spotify-web-api-node');
+const spotify         = new SpotifyWebApi();
+
+spotify.searchArtists("The Beatles", {}, (err, data) => {
+  if (err) throw err;
+
+  let artists = data.body.artists.items;
+  console.log(artists)
+});
