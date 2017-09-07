ac9b8a5748d9439fd9ad14d5ae2f5359e4edb64f
lele <lele@crackberry.local>
2017-09-08 00:33:11 +0200

    spotify app and access token api

---
+++ starter_code/app.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+const SpotifyWebApi   = require('spotify-web-api-node');
+const expressLayouts  = require('express-ejs-layouts');
+
+const SpotifyTokenApi   = require('api/spotify.js');
+
+var index = require('./routes/index');
+var search = require('./routes/search');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/search', search);
+
+app.get('/artist', (req, res, next) => {
+    spotify.searchArtists(req.query.artist, {}, (err, data) => {
+    if (err) {throw err};
+    
+    let artists = data.body.artists.items;
+    res.render('artists',{ artists });
+    });
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
