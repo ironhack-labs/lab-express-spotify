b50317d72d91916c1566124b62bba2292b898bde
trigoporres <santiagotrigoporres@gmail.com>
2017-09-07 09:12:08 +0200

    Da error al renderizar index

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -9,7 +9,7 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const app = express();
 
 app.use(expressLayouts);
-app.set('layout', 'main-layout');
+app.set('layout', 'index');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -17,6 +17,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+   res.render('index')
+});
 
 var clientId = '393c7d31b6ec450bba7159ba92f752e4',
     clientSecret = '3d064fdac1674abb8eecbc19ba8d75a3';
@@ -32,4 +36,10 @@ spotifyApi.clientCredentialsGrant()
     spotifyApi.setAccessToken(data.body['access_token']);
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
+});
+
+// Server Started
+let port = 3000;
+app.listen(port, () => {
+  console.log(`My first app listening on port ${port}!`);
 });
