997bc7875eea252af1d9d8356d18326048177b48
Dario Vigil <dariovigil@gmail.com>
2017-11-08 22:47:13 +0100

    Spotify artist request but can't render view.

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.set('views', __dirname + '/views/layouts');
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
-app.set('layout', __dirname + '/views/layouts/home');
+app.set('layout', __dirname + '/views/layouts/index');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -28,12 +28,22 @@ spotifyApi.clientCredentialsGrant()
   console.log('Something went wrong when retrieving an access token', err);
 });
 
-app.get('/',(req,res)=>{
+app.get('/',(req,res) => {
   // console.log("la query es: ");
   // console.log(req.query);
-  res.render('home');
+  res.render('index');
 });
 
+app.get('/artists', (req,res) => {
+  let artistsResults = req.query.searchName;
+  console.log(`Artist search results: ${artistsResults}`);
+  spotifyApi.searchArtists(artistsResults)
+  .then(function(data) {
+    let spotifyArtistsResults = data.body.artists.items;
+    console.log(spotifyArtistsResults);
+    res.render('artists', {artists : spotifyArtistsResults});
+  });
+});
 
 app.listen(3000, () => {
   console.log('ironspotify app listening on port 3000!');
