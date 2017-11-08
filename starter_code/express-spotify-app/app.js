c5f2c252d50db69e8c60714728dff746b0cf8b14
Camila Maya <camilamaya21@gmail.com>
2017-11-08 19:57:48 +0100

    Iteration 3 | Search for an Artist

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,10 +1,13 @@
-var SpotifyWebApi = require('spotify-web-api-node');
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const SpotifyWebApi = require('spotify-web-api-node');
+const clientId = '130309a175f5439691f29b71820e47a5',
+      clientSecret = 'a833cbef9f5d42aba52ae37462f2b66c';
 
-// Remember to paste here your credentials
-var clientId = '1c30624cba6742dcb792991caecae571',
-    clientSecret = '746977b1e77240faa9d0d2411c3e0efe';
-
-var spotifyApi = new SpotifyWebApi({
+const spotifyApi = new SpotifyWebApi({
   clientId : clientId,
   clientSecret : clientSecret
 });
@@ -15,4 +18,38 @@ spotifyApi.clientCredentialsGrant()
     spotifyApi.setAccessToken(data.body['access_token']);
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
+});
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+app.set('layout', __dirname + '/views/layout/main-layout');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+  res.render('index');
+});
+
+app.get('/artist', (req,res)=>{
+  let artist = req.query.artist;
+  spotifyApi.searchArtists(artist)
+  .then(function(data) {
+    console.log('Search this '+ artist, data.body);
+    res.render('artist', {artists: data.body.artists.items});
+  }, function(err) {
+    console.error(err);
+  });
+});
+
+
+
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!');
 });
