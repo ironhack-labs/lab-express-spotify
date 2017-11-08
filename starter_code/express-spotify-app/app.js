fd279015c4eb499fca09e4be38683e96ae09efd7
Rocio Garcia <gooblyn@gmail.com>
2017-11-08 18:32:16 +0100

    Third Iteration, step 1, done

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,10 +1,16 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
 var SpotifyWebApi = require('spotify-web-api-node');
 
 // Remember to paste here your credentials
-var clientId = '1c30624cba6742dcb792991caecae571',
-    clientSecret = '746977b1e77240faa9d0d2411c3e0efe';
+var clientId = '6decf53c4bbb4f0bb5c0f70d4cec515a',
+    clientSecret = '4cbcaca73e84487ba9baa6a578835d8b';
 
-var spotifyApi = new SpotifyWebApi({
+const spotifyApi = new SpotifyWebApi({
   clientId : clientId,
   clientSecret : clientSecret
 });
@@ -15,4 +21,24 @@ spotifyApi.clientCredentialsGrant()
     spotifyApi.setAccessToken(data.body['access_token']);
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
+});
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+app.set('layout', __dirname + '/views/layout/main_layout');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+  res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!');
 });
