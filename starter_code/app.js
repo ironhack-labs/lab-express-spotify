58eaea235f990b98149438357a959849bc83bcd4
nadiacarreiranunez <nadiacarreiranunez@gmail.com>
2017-11-08 22:18:58 +0100

    itineration without tracks

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,10 +1,17 @@
-var SpotifyWebApi = require('spotify-web-api-node');
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+const SpotifyWebApi = require('spotify-web-api-node');
 
 // Remember to paste here your credentials
-var clientId = '1c30624cba6742dcb792991caecae571',
-    clientSecret = '746977b1e77240faa9d0d2411c3e0efe';
+const clientId = '6decf53c4bbb4f0bb5c0f70d4cec515a',
+    clientSecret = '4cbcaca73e84487ba9baa6a578835d8b';
 
-var spotifyApi = new SpotifyWebApi({
+const spotifyApi = new SpotifyWebApi({
   clientId : clientId,
   clientSecret : clientSecret
 });
@@ -15,4 +22,58 @@ spotifyApi.clientCredentialsGrant()
     spotifyApi.setAccessToken(data.body['access_token']);
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
+});
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+app.set('layout', __dirname + '/views/layout/main_layout');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+  res.render('index');
+});
+
+app.get('/artists',(req,res)=>{
+  let artist = req.query.artist;
+  spotifyApi.searchArtists(artist)
+  .then(function(data) {
+    res.render('artists', {artists: data.body.artists.items});
+  }, function(err) {
+    console.error(err);
+  });
+});
+
+app.get('/artist/:id', (req, res) => {
+  let artistId = req.params.id;
+  spotifyApi.getArtistAlbums(artistId)
+  .then(function(data) {
+    let artistAlbum = data.body.items;
+    console.log('Artist albums', data.body.items);
+    res.render('albums', {albums: artistAlbum});
+  }, function(err) {
+    console.error(err);
+  });
+});
+
+app.get('/album/:id', (req, res) => {
+  let albumId = req.params.id;
+  spotifyApi.getAlbumTracks(albumId)
+  .then(function(data) {
+    let albumTracks = data.body.items;
+    console.log(data);
+    res.render('tracks',{tracks: albumTracks});
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!');
 });
