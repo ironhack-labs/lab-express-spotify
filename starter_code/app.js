1d7eb7bec8ffc92abf6e4b524e3fae6ee19b5595
Maria Ruiz <mariaruizestevez@gmail.com>
2017-11-09 09:14:18 +0100

    Added some modifications

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,29 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+app.set('layout', __dirname + '/views/layout/home');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
+
+app.use(express.static('public'));
+
+
+
+
 var SpotifyWebApi = require('spotify-web-api-node');
 
 // Remember to paste here your credentials
-var clientId = '1c30624cba6742dcb792991caecae571',
-    clientSecret = '746977b1e77240faa9d0d2411c3e0efe';
+var clientId = '356b11ed3da546fdb68080dec63fc3c9',
+    clientSecret = '52f925bdc7c344d3ac6f211e80be5b7f';
 
 var spotifyApi = new SpotifyWebApi({
   clientId : clientId,
@@ -15,4 +36,21 @@ spotifyApi.clientCredentialsGrant()
     spotifyApi.setAccessToken(data.body['access_token']);
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
+});
+
+app.get('/',(req,res)=>{
+  res.render('home');
+});
+
+// app.post('/artist',(req,res) => {
+//   console.log("el body es: ");
+//   console.log(req.body.searchArtist);
+//   res.render('artist', {
+//   });
+// });
+
+
+
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!');
 });
