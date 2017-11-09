08de7a2465ca6165158f31ca6e52986d00b14568
alm00001 <alm00001@gmail.com>
2017-11-09 09:04:30 +0100

    SecondIterationNotFinished

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,4 +1,10 @@
-var SpotifyWebApi = require('spotify-web-api-node');
+const SpotifyWebApi = require('spotify-web-api-node');
+const express = require('express');
+const ejs = require('ejs');
+const expressLayouts = require('express-ejs-layouts');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
 
 // Remember to paste here your credentials
 var clientId = '545c564be843429eb66527d7fd4d0d31',
@@ -9,10 +15,31 @@ var spotifyApi = new SpotifyWebApi({
   clientSecret : clientSecret
 });
 
+app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+app.set('layout', __dirname + '/views/layouts');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+  res.render('home');
+});
+
+
+
+
 // Retrieve an access token.
 spotifyApi.clientCredentialsGrant()
   .then(function(data) {
     spotifyApi.setAccessToken(data.body['access_token']);
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
+});
+
+app.listen(3000, () =>{
+  console.log('My first app listening on port 3000');
 });
