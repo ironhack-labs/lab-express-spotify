124a1e5f0644717b9bc8492f00d1ff7b58b3c107
VivianSolide <vivian.sarazin@gmail.com>
2017-12-21 22:31:23 +0100

    xmas

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -4,32 +4,37 @@ var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
- 
+
 // Remember to paste here your credentials
 var clientId = '20160c41f58543b9805c1dc5dcf640e1',
-    clientSecret = 'c9ccb36592b8436395e68c7f6149fc02';
+  clientSecret = 'c9ccb36592b8436395e68c7f6149fc02';
 
 var spotifyApi = new SpotifyWebApi({
-  clientId : clientId,
-  clientSecret : clientSecret
+  clientId: clientId,
+  clientSecret: clientSecret
 });
 
 // Retrieve an access token.
 spotifyApi.clientCredentialsGrant()
-  .then(function(data) {
+  .then(function (data) {
     spotifyApi.setAccessToken(data.body['access_token']);
-  }, function(err) {
+  }, function (err) {
     console.log('Something went wrong when retrieving an access token', err);
-});
+  });
 
 app.get('/', (req, res, next) => {
   res.render('index');
   next();
 })
 
-app.get('/artists', (req, res, next) => {
-  res.render('artists');
-  next();
+app.get('/artist', (req, res, next) => {
+  spotifyApi.searchArtists(req.query.artist)
+    .then(function (data) {
+      console.log(data);
+      res.render('artist', {name:req.query.artist, songs:data.body.artists.items, href:data.body.artists.href, artists:data.body.artists});
+    }, function (err) {
+      console.error(err);
+    });
 })
 
 app.get('/public', (req, res, next) => {
@@ -37,5 +42,5 @@ app.get('/public', (req, res, next) => {
 })
 
 app.listen(3000, () => {
-  console.log('yolo');
+  console.log('server is running, bitch!');
 })
