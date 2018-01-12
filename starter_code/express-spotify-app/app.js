4f54f5a3a805a6bf50b546de6192856d78bfd437
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-01-12 11:19:40 +0100

    Feat: Step 1

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -2,20 +2,20 @@ var SpotifyWebApi = require('spotify-web-api-node');
 
 // Remember to paste here your credentials
 var clientId = '1c30624cba6742dcb792991caecae571',
-    clientSecret = '746977b1e77240faa9d0d2411c3e0efe';
+  clientSecret = '746977b1e77240faa9d0d2411c3e0efe';
 
 var spotifyApi = new SpotifyWebApi({
-  clientId : clientId,
-  clientSecret : clientSecret
+  clientId: clientId,
+  clientSecret: clientSecret
 });
 
 // Retrieve an access token.
 spotifyApi.clientCredentialsGrant()
-  .then(function(data) {
+  .then(function (data) {
     spotifyApi.setAccessToken(data.body['access_token']);
-  }, function(err) {
+  }, function (err) {
     console.log('Something went wrong when retrieving an access token', err);
-});
+  });
 
 const express = require('express');
 const app = express();
@@ -34,6 +34,18 @@ app.get('/home', (request, res) => {
 app.get('/', (request, res, next) => {
   res.render('home');
 });
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.post('/home', function (req, res) {
+  console.log(req.body.art);
+  
+  res.render('artist', {
+    artist: req.body.art
+  });
+});
+
 
 // app.get('/random', (request, res, next) => {
 //   // Retrieve a random chuck joke
@@ -78,26 +90,11 @@ app.get('/', (request, res, next) => {
 //   res.render('search-form');
 // });
 
-// app.use(bodyParser.urlencoded({
-//   extended: true
-// }));
-// app.post('/search', function (req, res) {
-//   client.search(req.body.categ)
-//     .then(function (response) {
-//       let arraData=[];
-//       response.items.forEach(element => {
-//         arraData.push(element.value)
-//       });
-//       let arraObj={jokes:arraData};
-//       res.render('search-result',arraObj);
-//     }).catch(function (err) {
-//       // handle error
-//     });
-// });
+
 
 
 
 // Server Started
 app.listen(3000, () => {
-  console.log('My first app listening on port 3000!');
+  console.log('Server ONLINE!');
 });
