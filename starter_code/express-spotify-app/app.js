00f3cc81cfa6e24245d4bde6d28a70fbf1eeca46
Guillermo Alarcón <xxmgaxx@gmail.com>
2018-01-14 23:15:41 +0100

    feat: iteration 3 step search

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,4 +1,20 @@
-var SpotifyWebApi = require('spotify-web-api-node');
+/* require modules */
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require("body-parser");
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(expressLayouts);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.set('layout', 'layouts/main-layout');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 
 // Remember to paste here your credentials
 var clientId = 'f89c033c9e1c4fbfa41a935c6cb8e06d',
@@ -16,3 +32,25 @@ spotifyApi.clientCredentialsGrant()
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
 });
+
+
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+app.get('/artists', (req, res) => {
+  let artist = req.query.artist;
+
+  spotifyApi.searchArtists(req.query.artist)
+    .then(function(data) {
+      // console.log(data.body.artists.items[0])
+      res.render('artists', {
+        artists: data.body.artists
+      });
+    }, function(err) {
+      console.log('Something went wrong!', err);
+    });
+});
+
+app.listen(3000, () => console.log("Ready!"));
