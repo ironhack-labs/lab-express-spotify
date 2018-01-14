fb908390f33d93969d59c0670dc4f6b6183f690d
christiangar <32417561+christiangar@users.noreply.github.com>
2018-01-14 15:52:09 +0100

    Feat: show albums

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,3 +1,50 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require("body-parser");
+const app = express();
+
+
+app.set('layout', 'layout/main-layout');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/index', (require, response, next) => {
+  response.render('index');
+});
+
+app.post('/artists', (req,res) =>{
+  let nameOfArtist = req.body.artist;
+  spotifyApi.searchArtists(nameOfArtist)
+  .then(function(data) {
+    // console.log('Search artists by "Love"', data.body);
+    res.render('artists',{
+      nameOfArtist: nameOfArtist,
+      artistItems: data.body.artists.items
+    })
+    // console.log(data.body.artists.items)
+  }, function(err) {
+    console.error(err);
+    res.render('artists')
+  });
+})
+
+app.get('/albums/:artistID',(req,res) =>{
+     let artistID = req.params.artistID ;
+  // console.log(artistID)
+  spotifyApi.getArtistAlbums(req.params.artistID)
+  .then(function(data) {
+    res.render('albums',{
+      artistAlbums: data.body.items
+    })
+    console.log(data.body.items);
+  }, function(err) {
+    console.error(err);
+  });
+})
+
 var SpotifyWebApi = require('spotify-web-api-node');
 
 // Remember to paste here your credentials
@@ -16,3 +63,6 @@ spotifyApi.clientCredentialsGrant()
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
 });
+
+
+app.listen(3000, () => console.log("Ready!"));
