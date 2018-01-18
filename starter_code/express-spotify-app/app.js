1800881b5aa86855409d8f2b7ab913035229822e
Juancho Arregui <juan.arregui.lopez@gmail.com>
2018-01-18 17:56:07 +0100

    finished!

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -19,6 +19,7 @@ const spotifyApi = new SpotifyWebApi({
 spotifyApi.clientCredentialsGrant()
   .then(function(data) {
     spotifyApi.setAccessToken(data.body['access_token']);
+    console.log('Ouh yeah, connected to Spotify!!');
   }, function(err) {
     console.log('Something went wrong when retrieving an access token from Spotify', err);
 });
@@ -29,7 +30,7 @@ app.use(express.static('public'));
 app.use(expressLayouts);
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('layout', 'layout');
+app.set('layout', 'layouts/layout');
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
@@ -45,9 +46,11 @@ app.get('/', (req, res)=>{
 
 //esto configura lo que va a mostrar cuando le pidamos un artista en el formulario
 app.get('/artists', (req, res) => {
+    console.log(`Esta es la request que recibe del formulario:`, req.query);
+
     let artist = req.query.artist;
   
-    spotifyApi.searchArtists(req.query.artist)
+    spotifyApi.searchArtists(artist)
       .then(function(data) {
         res.render('artists', {
           artists: data.body.artists.items
@@ -57,6 +60,29 @@ app.get('/artists', (req, res) => {
       });
   });
 
+//esto configura lo que va a mostrar cuando le pidamos un album
+  app.get('/albums/:artistId', (req, res, next) => {
+    spotifyApi.getArtistAlbums(req.params.artistId)
+      .then(function(data) {
+        res.render('albums', {
+          albums: data.body.items
+        });
+      }, function(err) {
+        console.log('Something went wrong!', err);
+      });
+  });
+
+  //esto configura lo que va a mostrar cuando le pidamos una pista
+  app.get('/tracks/:albumId', (req, res, next) => {
+    spotifyApi.getAlbumTracks(req.params.albumId)
+      .then(function(data) {
+        res.render('tracks', {
+          tracks: data.body.items
+        });
+      }, function(err) {
+        console.log('Something went wrong!', err);
+      });
+  });
 
 
 // Levantamos el servidor
