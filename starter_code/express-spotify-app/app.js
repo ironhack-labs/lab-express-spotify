31e7ff828ecb989cab749cfedb2b6d90c4c7825b
nachobaile <nachobaile@gmail.com>
2018-01-31 21:03:54 +0100

    server connect

---
+++ starter_code/express-spotify-app/app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var SpotifyWebApi = require('spotify-web-api-node');
+const expressLayouts = require('express-ejs-layouts');
+
+// Remember to paste here your credentials
+var clientId = "616a1dccb0b54728b0c05a04ff0d4fa5",
+    clientSecret =  "d09d99f4a31942d5b8595710a9a744ab";
+
+var spotifyApi = new SpotifyWebApi({
+    clientId: clientId,
+    clientSecret: clientSecret
+});
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// Retrieve an access token.
+spotifyApi.clientCredentialsGrant()
+    .then(function (data) {
+        spotifyApi.setAccessToken(data.body['access_token']);
+    }, function (err) {
+        console.log('Something went wrong when retrieving an access token', err);
+    });
+
+app.get('/', (req, res, next) => {
+    // Retrieve a random chuck joke
+    res.send('hello world');
+});
+
+app.listen(3000, () => {
+    console.log('My Spotify API listening on port 3000!')
+});
