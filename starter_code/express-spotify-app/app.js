9a065fa9a12fc64269a4ee373789d51bdc309e85
Lucas Martin Calderon <lucasmartincalderon1@gmail.com>
2018-02-01 00:22:54 +0100

    Done

---
+++ starter_code/express-spotify-app/app.js
@@ -0,0 +1,41 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+
+// Remember to paste here your credentials
+var clientId = '1c30624cba6742dcb792991caecae571',
+    clientSecret = '746977b1e77240faa9d0d2411c3e0efe';
+
+var spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+// Retrieve an access token.
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
+});
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+app.get("/", (req, res, next) => {
+  console.log('I am getting in the root page..');
+  res.render('index');
+});
+
+app.get("/artist", (req, res, next) => {
+  let artist = req.query.artist;
+  spotifyApi.searchArtists(artist)
+  .then((response) => {
+    let data = {name: artist, main: response}
+    res.render('artist', data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });     
+});
+
