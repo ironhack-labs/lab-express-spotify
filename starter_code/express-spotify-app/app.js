19157138a5bc5f8c5c85895db0ecc045604207f5
JurgenBlitz <jorge.gopae@gmail.com>
2018-01-31 21:14:47 +0100

    Tercera iteración a medias

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,5 +1,10 @@
 var SpotifyWebApi = require('spotify-web-api-node');
-
+// var BodyParser= require('body-parser');
+var express= require('express');
+var app = express();
+var Ejs= require('ejs');
+var Layouts= require('express-ejs-layouts');
+var Morgan= require('morgan');
 // Remember to paste here your credentials
 var clientId = 'ef853ce61c9e42c4b5421ee1ebf6f468',
     clientSecret = 'f16f9e34875e4285914a04d5282e31fc';
@@ -9,6 +14,12 @@ var spotifyApi = new SpotifyWebApi({
   clientSecret : clientSecret
 });
 
+app.get('/', (req, res, next) => {
+    res.render('index.ejs');
+  });
+
+
+
 // Retrieve an access token.
 spotifyApi.clientCredentialsGrant()
   .then(function(data) {
@@ -16,3 +27,14 @@ spotifyApi.clientCredentialsGrant()
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
 });
+
+// app.get("/artist", (req, res, next) => {
+//     client
+//       .getRandomJoke()
+//       .then(joke => {
+//         let randomJoke = joke.value;
+//         res.render("index", { name: randomJoke });
+//       })
+//       .catch(err => {
+//       }
+//       )})
