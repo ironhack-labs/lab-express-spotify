ce7c8f404210ab869825e4162aa6ea2a3a28574d
xarliex <carlos.lorente@hotmail.com>
2018-02-01 10:00:17 +0100

    hasta aquí hemos llegado

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,56 +1,49 @@
-
 const express = require("express");
 const app = express();
-const SpotifyWebApi = require('spotify-web-api-node');
-const bodyParser = require('body-parser');
+const SpotifyWebApi = require("spotify-web-api-node");
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require("body-parser");
 const spotifyApi = new SpotifyWebApi({
-  clientId : 'f53d912621d942c694520248cd84db89',
-  clientSecret : '8ed48a0bcdc447faaba8e833a0bd60c0'
+  clientId: "f53d912621d942c694520248cd84db89",
+  clientSecret: "8ed48a0bcdc447faaba8e833a0bd60c0"
 });
-
+let data = {
+  clientId: "f53d912621d942c694520248cd84db89",
+  clientSecret: "8ed48a0bcdc447faaba8e833a0bd60c0"
+};
+spotifyApi.clientCredentialsGrant().then(
+  function(data) {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+    console.log("conectado a spotify");
+  },
+  function(err) {
+    console.log("Something went wrong when retrieving an access token", err);
+  }
+);
+app.use(expressLayouts); 
+app.set('layout', 'layouts/main-layout');
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
 // USE BODY PARSER
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res, next) => { 
-  let data = {
-    clientId : 'f53d912621d942c694520248cd84db89',
-    clientSecret : '8ed48a0bcdc447faaba8e833a0bd60c0' };
-  res.render('index', data); });
-
-// Retrieve an access token.
-// spotifyApi.clientCredentialsGrant()
-//   .then(function(data) {
-//     spotifyApi.setAccessToken(data.body['access_token']);
-//   }, function(err) {
-//     console.log('Something went wrong when retrieving an access token', err);
-// });
-
-app.get("/random", (req, res, next) => {
-  spotifyApi.clientCredentialsGrant()
-    .then(i => {
-      res.render("index", { artist: i.value });
-    })
-    .catch(err => {
-      console.log('Something went wrong when retrieving an access token', err);});
-});
-
-// DISPLAY A FORM - NEW ROUTE: 
-app.get('/homepage', (req, res) => { 
-  res.render('homepage')
+app.get("/", (req, res, next) => {
+  res.render("index", data);
 });
 
 // POST ROUTE
-app.post('/homepage', (req, res) => { 
-  let artist = req.body.artist; 
-  res.send(`Artist: ${artist}`); });
-
+app.post("/homepage", (req, res) => {
+    spotifyApi.searchArtists(req.body.artist, { limit: 10, offset: 20 }, (err, data) =>{
+      if (err) {
+        console.error('Something went wrong!');
+      } else {
+        console.log(data.body);
+      }
+    });
+});
 
 // Server Started
 app.listen(3000, () => {
   console.log("My first app listening on port 3000!");
 });
-
-
