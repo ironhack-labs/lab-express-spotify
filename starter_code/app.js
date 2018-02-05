9fd41970318b742ed26585da37f4da2990f2ab0b
Alfio <alfioanr@gmail.com>
2018-02-05 09:16:20 +0100

    no logre sacarle informacion al api

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,20 +1,32 @@
-var express = require("express");
-var app = express();
-var SpotifyWebApi = require("spotify-web-api-node");
+const express = require("express");
+const app = express();
+const SpotifyWebApi = require("spotify-web-api-node");
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 // Remember to paste here your credentials
-var clientId = "c0e409f724564333a240dbd7d5ab5f75",
-  clientSecret = "c4919a5eec624e31ba1973b395370c3b";
+app.use(express.static("public"));
+app.use(expressLayouts);
 
-var spotifyApi = new SpotifyWebApi({
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  morgan(
+    `Request Method: :method, Request URL: :url, Response Time: :response-time(ms)`
+  )
+);
+const clientId = "c0e409f724564333a240dbd7d5ab5f75";
+const clientSecret = "c4919a5eec624e31ba1973b395370c3b";
+
+const spotifyApi = new SpotifyWebApi({
   clientId: clientId,
   clientSecret: clientSecret
 });
 
+
+app.set("layout", "layouts/main-layout");
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-
 // Retrieve an access token.
 spotifyApi.clientCredentialsGrant().then(
   function(data) {
@@ -25,10 +37,38 @@ spotifyApi.clientCredentialsGrant().then(
   }
 );
 
+var buscar =function (data) {
+    
+}
 app.get("/", (req, res, next) => {
   // Retrieve a random chuck joke
-  res.send("hello world");
+  res.render("form");
+  
+  
 });
+app.get("/artist", (req, res, next) => {
+  // Retrieve a random chuck joke
+  res.render("form");
+});
+
+app.post("/artist", (req, res, next) => {
+  let artistName=req.body.artistName;
+  
+  spotifyApi.searchArtists("elton").then(function(data) {
+      console.log('Search artists by "' + artistName + '"', data.body);
+
+
+      res.render('resultado', {items:data.body.items,limit:data.body.limit})
+      // res.send(data.body);
+    }, function(err) {
+      console.error(err);
+    });
+    
+  // console.log(algo);
+    
+});
+
+
 
 app.listen(3000, () => {
   console.log("My Spotify API listening on port 3000!");
