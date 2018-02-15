3f8cbd50ec0f3431bf36f2ac152cf25380ba5429
Bet Mendoza <sbm2391@hotmail.com>
2018-02-14 18:39:33 -0600

    step1 done

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,8 +1,27 @@
+//para tener un servidor express
+//1-. importacion de la libreria
+const express = require("express");
+const bodyParser = require("body-parser");
+
+//2-. instancia de express
+const app = express();
+//body parser
+app.use(bodyParser.urlencoded({extendes: true}))
+//es para que pueda leer los archivos de la carpeta public
+app.use(express.static("public"))
 var SpotifyWebApi = require('spotify-web-api-node');
+//indicamos el motor de vistas
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  res.render("index");
+
+});
 
 // Remember to paste here your credentials
-var clientId = '1c30624cba6742dcb792991caecae571',
-    clientSecret = '746977b1e77240faa9d0d2411c3e0efe';
+var clientId = 'fa967eff37de4e2db29c46617ffd5c5e',
+    clientSecret = '0c3432f372694b8eb477d5d7902848c0';
 
 var spotifyApi = new SpotifyWebApi({
   clientId : clientId,
@@ -16,3 +35,9 @@ spotifyApi.clientCredentialsGrant()
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
 });
+
+// siempre va al final, es lo que escucha
+app.listen(3000, function(err){
+  if(err) console.log(err);
+  console.log("Tu servidor está funcionando")
+})
