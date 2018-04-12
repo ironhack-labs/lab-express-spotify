8d522639c9afd9eb86d7dbaff921e6bae6241c50
Pablo García <heypablete@gmail.com>
2018-04-12 20:50:04 +0200

    empezando la iteracion 4

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,12 +1,23 @@
-var SpotifyWebApi = require('spotify-web-api-node');
-
-var clientId = 'b15e36f5256b4c158d7b6d8c0e39b826',
-    clientSecret = 'c802787ea4ac490facdff0c47edaab7c';
-
-var spotifyApi = new SpotifyWebApi({
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const SpotifyWebApi = require('spotify-web-api-node');
+const path = require('path');
+const port = 3000;
+const clientId = 'b15e36f5256b4c158d7b6d8c0e39b826';
+const clientSecret = 'c802787ea4ac490facdff0c47edaab7c';
+let spotifyApi = new SpotifyWebApi({
   clientId : clientId,
   clientSecret : clientSecret
 });
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join( __dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+//hbs.registerPartials( __dirname + '/views/partials')
+
+
 
 // Retrieve an access token.
 spotifyApi.clientCredentialsGrant()
@@ -15,3 +26,28 @@ spotifyApi.clientCredentialsGrant()
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
 });
+
+
+
+// Rutas
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/artists', (req, res) => {
+  let artistName = req.body.artistName;
+  spotifyApi.searchArtists(artistName)
+  .then(data => {
+    let arrayArtist = data.body.artists.items[0];
+    //console.log("-----------");
+    console.log(data.body.artists.items[0]);
+    arrayArtist.forEach(element => {
+      
+    });
+  })
+  .catch(err => { console.log(err); })
+  res.render('artists');
+});
+
+
+app.listen(port);
