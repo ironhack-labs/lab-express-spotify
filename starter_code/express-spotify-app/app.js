598c5c5d1361ea7dcc6447d4c568d4e0b5941c5a
Raul.dll <rdl.llave@gmail.com>
2018-04-12 21:12:51 +0200

    iteration3

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,31 +1,46 @@
-var SpotifyWebApi = require('spotify-web-api-node');
-const express = require('express');
+var SpotifyWebApi = require("spotify-web-api-node");
+const express = require("express");
 const app = express();
-const hbs = require('hbs');
-const path = require("path")
+const hbs = require("hbs");
+const path = require("path");
 // Remember to paste here your credentials
-var clientId = '698d9a0ec4044bdfbd2d49c110bb4377',
-    clientSecret = 'b2a8eea3329b4a5fab688818e9478c55';
+var clientId = "698d9a0ec4044bdfbd2d49c110bb4377",
+  clientSecret = "b2a8eea3329b4a5fab688818e9478c55";
 
 var spotifyApi = new SpotifyWebApi({
-  clientId : clientId,
-  clientSecret : clientSecret
+  clientId: clientId,
+  clientSecret: clientSecret
 });
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, "public")));
 
 // Retrieve an access token.
-spotifyApi.clientCredentialsGrant()
-  .then(function(data) {
-    spotifyApi.setAccessToken(data.body['access_token']);
-  }, function(err) {
-    console.log('Something went wrong when retrieving an access token', err);
+spotifyApi.clientCredentialsGrant().then(
+  function(data) {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  },
+  function(err) {
+    console.log("Something went wrong when retrieving an access token", err);
+  }
+);
+
+app.get("/", function(req, res) {
+  res.render("index");
 });
 
-app.get('/', function (req, res) {
-  res.render('index')
-})
+app.get("/artist", function(req, res) {
+  spotifyApi
+    .searchArtists(req.query.artist)
+    .then(data => {
+      console.log(data.body.artists.items);
+      res.render("artist");
+    })
+
+    .catch(err => {
+      // ----> 'HERE WE CAPTURE THE ERROR'
+    });
+});
 
 app.listen(3000);
