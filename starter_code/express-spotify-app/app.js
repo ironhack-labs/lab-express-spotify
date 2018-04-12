094c1a1c8587ba9f8bb00d30d0a9cdc9361c7d84
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-04-12 09:30:10 +0200

    Iteration 1 complete

---
+++ starter_code/express-spotify-app/app.js
@@ -0,0 +1,18 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+
+// Remember to paste here your credentials
+var clientId = '1c30624cba6742dcb792991caecae571',
+    clientSecret = '746977b1e77240faa9d0d2411c3e0efe';
+
+var spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+// Retrieve an access token.
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
+});
