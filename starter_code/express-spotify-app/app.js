bf7cf688c68209d6e9927db7be015513f416ee41
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-04-12 16:16:23 +0200

    Setup sass and other files

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,22 +1,33 @@
-const SpotifyWebApi = require('spotify-web-api-node');
-const express = require('express');
-const hbs = require('hbs');
+const SpotifyWebApi = require("spotify-web-api-node");
+const express = require("express");
+const hbs = require("hbs");
+const path = require("path");
 
 const app = express();
 
-// Remember to paste here your credentials
-const clientId = '3bf666e055684c42bc6eaa9789b34e37',
-    clientSecret = '578bb2451be04f34aa57767599d67f86';
+app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+hbs.registerPartials(path.join(__dirname, "views", "partials"));
+
+const clientId = "3bf666e055684c42bc6eaa9789b34e37",
+  clientSecret = "578bb2451be04f34aa57767599d67f86";
 
 const spotifyApi = new SpotifyWebApi({
-  clientId : clientId,
-  clientSecret : clientSecret
+  clientId: clientId,
+  clientSecret: clientSecret
 });
 
 // Retrieve an access token.
-spotifyApi.clientCredentialsGrant()
-  .then(function(data) {
-    spotifyApi.setAccessToken(data.body['access_token']);
-  }, function(err) {
-    console.log('Something went wrong when retrieving an access token', err);
-});
+spotifyApi.clientCredentialsGrant().then(
+  function(data) {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  },
+  function(err) {
+    console.log("Something went wrong when retrieving an access token", err);
+  }
+);
+
+const port = 3000;
+app.listen(port, () => console.log(`Listening to port ${port}`));
