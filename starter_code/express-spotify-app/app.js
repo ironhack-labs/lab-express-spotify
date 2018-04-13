4e6f4e0ca384686760e760f964807a1d8eb2fdb6
Luna Fidalgo <lunafidalgo@gmail.com>
2018-04-13 09:19:37 +0200

    DONE

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,22 +1,91 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const hbs = require('hbs');
+const hbs = require("hbs");
+const path = require("path");
+const bodyParser = require("body-parser");
+const SpotifyWebApi = require("spotify-web-api-node");
 
-const SpotifyWebApi = require('spotify-web-api-node');
+/*CONFIG EXPRESS*/
 
-// Remember to paste here your credentials
-var clientId = '1c30624cba6742dcb792991caecae571',
-    clientSecret = '746977b1e77240faa9d0d2411c3e0efe';
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+
+app.use(express.static(path.join(__dirname, "public")));
+console.log(__dirname + "public");
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+//Remember to paste here your credentials
+var clientId = "73e3b1ab2b874ef98bf693131ec78453",
+  clientSecret = "88d8a8d1695d41a48b5bd693586f24d0";
 
 var spotifyApi = new SpotifyWebApi({
-  clientId : clientId,
-  clientSecret : clientSecret
+  clientId: clientId,
+  clientSecret: clientSecret
 });
 
 // Retrieve an access token.
-spotifyApi.clientCredentialsGrant()
-  .then(function(data) {
-    spotifyApi.setAccessToken(data.body['access_token']);
-  }, function(err) {
-    console.log('Something went wrong when retrieving an access token', err);
+spotifyApi.clientCredentialsGrant().then(
+  function(data) {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  },
+  function(err) {
+    console.log("Something went wrong when retrieving an access token", err);
+  }
+);
+
+app.post("/artist", (req, res) => {
+  spotifyApi
+    .searchArtists(req.body.artist)
+    .then(data => {
+      const artist = {
+        list: data.body.artists.items
+      };
+      res.render("artist", artist);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
+
+app.get("/albums/:artistId", (req, res) => {
+  //console.log(req.params.artistId);
+  spotifyApi.getArtistAlbums(req.params.artistId).then(
+    function(data) {
+      console.log(data.body.items);
+      const artist = {
+        list: data.body.items
+      };
+      //console.log(artist.list)
+      res.render("album", artist);
+      // console.log('Albums information', data.body);
+    },
+    function(err) {
+      console.error(err);
+    }
+  );
+});
+
+/*TRACKS*/
+app.get("/songs/:artistId", (req, res) => {
+  spotifyApi.getAlbumTracks(req.params.artistId, { limit: 1, offset: 1 }).then(
+    function(data) {
+      // console.log(data.body)
+      const songs = {
+        list: data.body.items
+      };
+      // console.log(songs)
+      res.render("song", songs);
+    },
+    function(err) {
+      console.log("Something went wrong!", err);
+    }
+  );
+});
+
+app.listen(3000);
