ee4d0c2d5562c8b2591c6e850a9d07aaab76aad4
fran <fmls1989@gmail.com>
2018-06-25 20:56:22 +0200

    songs added

--- starter_code/app.js
+++ starter_code/app.js
@@ -18,51 +18,67 @@ app.get('/', (req,res, next)=>{
     res.render('home');
 });
 
+//artists
 app.get('/artists', (req,res, next)=>{  
-
     var artist = req.query.queryArtist;
     
     spotifyApi.searchArtists(artist)
     
     .then(data => {   
-                 
+        
         var elements = data.body.artists.items;   
         res.render('artists', { data:elements});
+    })
+    .catch(err => next(err));  
+});
+
+//songs
+app.get('/songs', (req,res, next)=>{  
+    var song = req.query.querySong;
+    
+    spotifyApi.searchTracks(song)
+    
+    .then(data => {   
+        
+        var elements = data.body.tracks.items;                    
+        res.render('songs', {data: elements});
     })
     .catch(err => next(err));
     
 });
 
+//albums
 app.get('/albums/:artistId', (req, res, next)=>{
-
+    
     var artistId = req.params.artistId;
     
     spotifyApi.getArtistAlbums(artistId)
     .then(data =>{
-
+        
         var elements = data.body.items;
         // console.log(elements);
         
         res.render('albums', { data: elements});
     })
     .catch(err => next(err));
- 
+    
 });
 
+//tracks
 app.get('/tracks/:albumId', (req, res, next)=>{
     
     var albumId = req.params.albumId;
     
     spotifyApi.getAlbumTracks(albumId)
     .then(data =>{
-
+        
         var elements = data.body.items;    
         console.log(elements);
-            
+        
         res.render('tracks', { data: elements});
     })
     .catch(err => next(err));
- 
+    
 });
 
 app.listen(PORT, ()=> console.log(`running on port ${PORT}`));
