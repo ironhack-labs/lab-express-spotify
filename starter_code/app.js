1952ea741294b90b3d426a6282007908e532e09f
Gabriel Garcia <gabogarciam71@gmail.com>
2018-07-05 20:34:50 +0200

    index page done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,20 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const path = require('path');
 const SpotifyWebApi = require('spotify-web-api-node');
 
+app.set('views', path.join(__dirname, 'views/layouts'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+    res.render('index');
+});
+
 // Remember to paste here your credentials
-const clientId = '648aa623157a4caba3b8d1e90d3ad33d',
-    clientSecret = 'd467fde767834e8686fd281cd0d30dbd';
+const clientId = '648aa623157a4caba3b8d1e90d3ad33d';
+const clientSecret = 'd467fde767834e8686fd281cd0d30dbd';
 
 const spotifyApi = new SpotifyWebApi({
   clientId : clientId,
@@ -16,3 +28,6 @@ spotifyApi.clientCredentialsGrant()
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
 });
+
+
+app.listen(3000);
