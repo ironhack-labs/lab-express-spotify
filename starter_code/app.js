45da4ee9e82bbc5e18392309aafad6e8dad98777
Verónica Reina <veroreinah@gmail.com>
2018-07-05 18:03:14 +0200

    Iteration 3 | Search for an Artist. Step 1 | Create a Homepage

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,15 @@
 const SpotifyWebApi = require("spotify-web-api-node");
-const express = require('express');
-const hbs = require('hbs');
+const express = require("express");
+const hbs = require("hbs");
+const path = require("path");
 
 const app = express();
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+hbs.registerPartials(__dirname + "/views/partials");
 
 const clientId = "9b2f2eda8a824a01a67f3bd272d1191d",
   clientSecret = "f2092c65231444e799c1547220186feb";
@@ -21,3 +28,11 @@ spotifyApi.clientCredentialsGrant().then(
     console.log("Something went wrong when retrieving an access token", err);
   }
 );
+
+app.get("/", (req, res, next) => {
+  res.render("home", {
+    title: "Spotify"
+  });
+});
+
+app.listen(3000);
