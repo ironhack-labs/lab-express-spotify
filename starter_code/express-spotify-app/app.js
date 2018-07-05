5dc11f7ee5595014a2df1b3769fc33b915139204
Minyu Li <minyuli1997@gmail.com>
2018-07-05 19:43:03 +0200

    done

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -14,17 +14,44 @@ app.get('/', (req, res, next) => {
 });
 
 app.get('/artists', (req, res, next) => {
-    console.log(req.query);
+    //console.log(req.query);
     spotifyApi.searchArtists(req.query.artist)
         .then(data => {
-            console.log(data.body.artists.items);
+            //console.log(data.body.artists.items);
             var allResults = data.body.artists.items;
-            res.render('artists',{info: allResults});
+            res.render('artists', { info: allResults });
         })
         .catch(err => {
-            console.log(error);
+            console.log(err);
         })
 });
+
+app.get('/albums/:artistId', (req, res) => {
+    var arId = req.params.artistId;
+    //console.log(arId);
+    spotifyApi.getArtistAlbums(arId)
+    .then(data=>{
+        //console.log(data.body.items);
+        var allAlbums = data.body.items;
+        res.render('albums', {albuminfo: allAlbums});
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+});
+
+app.get('/tracks/:albumId', (req,res) =>{
+    var albId = req.params.albumId;
+    spotifyApi.getAlbumTracks(albId)
+    .then(data=>{
+        console.log(data.body.items);
+        var allTrakcs = data.body.items;
+        res.render('tracks',{trackinfo: allTrakcs});
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+})
 
 // Remember to paste here your credentials
 var clientId = '8c5bbbcf4fe7491491ec1ed5f248e71f',
