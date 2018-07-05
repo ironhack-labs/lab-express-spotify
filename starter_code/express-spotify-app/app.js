4933b5372bbcc8ec8cc11b26e9451a89ebbff0a2
Raúl Valenzuela <raulvalen7@hotmail.com>
2018-07-05 17:48:35 +0200

    Iteration 2

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,8 +1,12 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
 var SpotifyWebApi = require('spotify-web-api-node');
 
 // Remember to paste here your credentials
-var clientId = '1c30624cba6742dcb792991caecae571',
-    clientSecret = '746977b1e77240faa9d0d2411c3e0efe';
+var clientId = 'b55518b95e334e1aa2d2495b3867ae55',
+    clientSecret = '673af2f0c83a4eda81f1441acf0da72b';
 
 var spotifyApi = new SpotifyWebApi({
   clientId : clientId,
