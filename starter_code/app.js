999ea8d6e935e4b3898565932c0b07b8d38bb221
Verónica Reina <veroreinah@gmail.com>
2018-07-05 18:55:27 +0200

    Iteration 3 | Search for an Artist. Step 2 | Display results for artist search

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,14 +2,17 @@ const SpotifyWebApi = require("spotify-web-api-node");
 const express = require("express");
 const hbs = require("hbs");
 const path = require("path");
+const bodyparser = require("body-parser");
 
 const app = express();
 
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
 
-hbs.registerPartials(__dirname + "/views/partials");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyparser.urlencoded({ extended: true }));
+
+hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 const clientId = "9b2f2eda8a824a01a67f3bd272d1191d",
   clientSecret = "f2092c65231444e799c1547220186feb";
@@ -29,10 +32,39 @@ spotifyApi.clientCredentialsGrant().then(
   }
 );
 
+// Home page
 app.get("/", (req, res, next) => {
   res.render("home", {
     title: "Spotify"
   });
+});
+
+// Artist page
+app.post("/artists", (req, res, next) => {
+  var { artist } = req.body;
+
+  spotifyApi.searchArtists(artist)
+    .then(data => {
+      var artists = data.body.artists.items;
+
+      artists.forEach(element => {
+        if (element.images.length > 1) {
+          element.imageUrl = element.images[1].url;
+        } else if (element.images.length) {
+          element.imageUrl = element.images[0].url;
+        } else {
+          element.imageUrl = "http://cdn.onlinewebfonts.com/svg/img_508643.png";
+        }
+      });
+
+      res.render("artists", {
+        title: "Artists - Spotify",
+        artists: artists
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 app.listen(3000);
