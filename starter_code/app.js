dc862b312c460abd62cdc8ec34bfc03eb444475c
Gabriel Garcia <gabogarciam71@gmail.com>
2018-07-05 20:03:07 +0200

    nmp morgan, folders

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,3 +1,18 @@
-// app.js
-const express = require('express')
-const app = express();
+const SpotifyWebApi = require('spotify-web-api-node');
+
+// Remember to paste here your credentials
+const clientId = '648aa623157a4caba3b8d1e90d3ad33d',
+    clientSecret = 'd467fde767834e8686fd281cd0d30dbd';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+// Retrieve an access token.
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
+});
