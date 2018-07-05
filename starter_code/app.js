1ceeb25f2dd23d1e43af816ad3b588f18240ef31
Will Schaller <willschaller@gmail.com>
2018-07-05 19:17:35 +0200

    Connect to spotify API

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,7 +6,19 @@ const hbs = require('hbs');
 const path = require('path');
 const SpotifyWebApi = require('spotify-web-api-node');
 const credentials = require('./credentials.js')
+const spotifyApi = new SpotifyWebApi({
+  clientId: credentials.clientId,
+  clientSecret: credentials.clientSecret
+});
 
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+spotifyApi.clientCredentialsGrant()
+  .then(function (data) {
+    spotifyApi.setAccessToken(data.body['access_token']);
+    console.log('Spotify client credentials granted successfully')
+  }, function (err) {
+    console.log('Something went wrong when retrieving an access token', err);
+  });
