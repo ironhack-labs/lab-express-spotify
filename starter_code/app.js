c9fde81ee0660a1cc60dcb50bcebd86e4e07e3b4
Will Schaller <willschaller@gmail.com>
2018-07-05 19:12:35 +0200

    Add spotify credentials

--- starter_code/app.js
+++ starter_code/app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 const hbs = require('hbs');
 const path = require('path');
+const SpotifyWebApi = require('spotify-web-api-node');
+const credentials = require('./credentials.js')
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
