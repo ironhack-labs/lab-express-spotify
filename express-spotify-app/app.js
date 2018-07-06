7f4e30abb2246e2eb0d3cd8661efce1dd376a124
Julien Chil Hagopian <julienchilhagopian@gmail.com>
2018-07-06 09:28:34 +0200

    blank page commit

--- express-spotify-app/app.js
+++ express-spotify-app/app.js
@@ -4,16 +4,6 @@ const app = express();
 const hbs = require('hbs');
 const path = require('path');
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, '/views'));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/index', (req, res, next) => {
-  res.render('index')
-})
-
-
-// Remember to paste here your credentials
 const clientId = '456b82e403914c6f85d795481c50c909',
     clientSecret = 'edfefe26f9a44cd19d13cdf1b7467ba3';
 
@@ -29,6 +19,34 @@ spotifyApi.clientCredentialsGrant()
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
 });;
+
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, '/views/partials'))
+
+app.get('/', (req, res, next) => {
+  res.render('index') 
+})  
+
+app.get('/artists', (req, res, next) => {
+  spotifyApi.searchArtists(function (req, res){
+    req.body.artist
+  })
+    .then(data => {
+      res.render('artists', { data })
+    })  
+    .catch(error => {
+      console.log(error); 
+    })  
+    
+  
+  res.render('artists')   
+})  
+
+
+// Remember to paste here your credentials
 
 
 
