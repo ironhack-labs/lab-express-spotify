df54ddaba127c7bb4b3236ed32c92d3f12559eef
Pablo Rezola <pablorezola20@yahoo.es>
2018-07-06 08:20:08 +0200

    iteracion 3/4

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,6 +1,19 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const path = require('path');
+const bodyparser = require("body-parser");
+const debug = require('debug')('spotify:app');
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 
-// Remember to paste here your credentials
+var SpotifyWebApi = require('spotify-web-api-node');
+
+
 var clientId = 'ca761f6905f1401ca68f5a313b292d8e',
     clientSecret = '6bbab67fcd024ea4a4a965145909dbbf';
 
@@ -17,51 +30,29 @@ spotifyApi.clientCredentialsGrant()
     console.log('Something went wrong when retrieving an access token', err);
 });
 
-var SpotifyWebApi = require('spotify-web-api-node');
-const express = require('express');
-const app = express();
-const hbs = require('hbs');
-const path = require('path');
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-
-
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-const punkAPI = new PunkAPIWrapper();
 
 
 
-app.use(express.static(path.join(__dirname, 'public')));
-hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
 
   res.render('index');
 });
 
-app.get('/beers', (req, res, next) => {
-  punkAPI.getBeers().then(beers => {
-    console.log(beers);
-    res.render('beers', {
-      beers
-    });
-  })
-});
-
-app.get('/random-beer', (req, res, next) => {
-  punkAPI.getRandom()
-    .then(beers => {
-      res.render('random-beer', {
-        beers
-      })
-    })
-    .catch(error => {
-      console.log(error)
-    })
-
-})
-
-app.listen(3000);
+app.get("/artist", (req, res, next) => {
+    
+    spotifyApi.searchArtists(req.query.artist)
+      .then(data => {
+          console.log(data)
+          res.render('artist', {data: data.body.artist.items});
+        })
+        .catch(error => {
+            console.log(error);
+          });
+    
+        });
+    
+app.listen(3000, () => console.log("Escucho"));
 
 
 
