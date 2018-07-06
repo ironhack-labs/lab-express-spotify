303935748faf7ec4f7524bbd557f17d3235e5093
Gabriel Garcia <gabogarciam71@gmail.com>
2018-07-06 10:23:30 +0200

    10% done

--- starter_code/app.js
+++ starter_code/app.js
@@ -7,10 +7,7 @@ const SpotifyWebApi = require('spotify-web-api-node');
 app.set('views', path.join(__dirname, 'views/layouts'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res, next) => {
-    res.render('index');
-});
+//app.use(bodyParser.urlencoded({ extended: true }));
 
 // Remember to paste here your credentials
 const clientId = '648aa623157a4caba3b8d1e90d3ad33d';
@@ -29,5 +26,24 @@ spotifyApi.clientCredentialsGrant()
     console.log('Something went wrong when retrieving an access token', err);
 });
 
+app.get('/', (req, res, next) => {
+    res.render('index');
+});
+
+app.get('/artists', (req, res, next) => {
+    const artista = req.query.artist;
+    console.log(req.query.artist);
+    spotifyApi.searchArtists(req.query.artist)
+    .then(data => {
+        //----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+        //console.log('Search artists by ', data.body.artists);
+        console.log('Search artists by ', data.body.artists.items);
+        res.render('artists', {artists: data.body.artists.items});
+    })
+    .catch(err => {
+        //----> 'HERE WE CAPTURE THE ERROR'
+        console.log(err);
+    })
+})
 
 app.listen(3000);
