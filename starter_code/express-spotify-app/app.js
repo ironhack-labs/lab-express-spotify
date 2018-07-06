640b93dad5888ed308db57d52c3a7e31f07e8890
Paula <paulagarizcun@gmail.com>
2018-07-06 09:19:40 +0200

    Maquetacion home

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -10,6 +10,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
