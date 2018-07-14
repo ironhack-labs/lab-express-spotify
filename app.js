9f4025b5ca0aef490acfe95b16b5e9c6d56ac305
Javier Chacón <j.chacon.alvarez@gmail.com>
2018-07-14 20:10:34 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const bodyParse = require('body-parser');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+app.get('/artists', (req, res, next) => {
+  res.render('artists');
+});
+
+app.get('/albums', (req, res, next) => {
+  res.render('albums');
+});
+
+app.get('/tracks', (req, res, next) => {
+  res.render('tracks');
+});
+
+app.listen(3000);
