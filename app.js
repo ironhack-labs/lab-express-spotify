203c9372402890be8c37d691c94ba52b0c344491
Laura R. Ojeda <rojedalaura@gmail.com>
2018-07-23 20:19:50 +0200

    Iteration 3 Step 2

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -32,47 +33,20 @@ spotifyApi.clientCredentialsGrant()
     console.log('Something went wrong when retrieving an access token', err);
   });
 
-
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
 app.get('/artists', (req, res, next) => {
-  //--> browser me dice: cannot post /artists
-  // const artistsSearch = req.query.artists
-  // spotifyApi.searchArtists(artistsSearch)
-  spotifyApi.searchArtists('iron maiden')
-    .then(result => {
-      const resultArtist = result.body.artists.items
-      // console.log(resultArtist);
-      // console.log('imagenes', resultArtist.items[0].images)
-      res.render('artists', {resultArtist});
-  }).catch(err => {
-      console.log('err', err)
-    })
+  const resultArtist = req.query.artists
+  spotifyApi.searchArtists(resultArtist)
+  .then((data) => {
+    artistData = data.body.artists.items;
+    res.render('artists', { artistData });
+  })
+  .catch((err) => {
+    console.log('err', err)
+  })
 });
-
-// app.get('/album/:artistId', (req, res, next) => {
-//   const albumSearch = req.params.artistID
-//   .then(result => {
-
-//     res.render(albumSearch);
-//   })
-//   .catch(err => {
-//     console.log('err', err)
-//   })
-// });
-
-// app.get('/tracks/:albumID', (req, res, next) => {
-//   const trackSearch = req.params.albumID;
-//   spotifyApi.getAlbumTracks()
-//   .then(result => {
-
-//     res.render('tracks');
-//   })
-//   .catch(err => {
-//     console.log('err', err)
-//   })
-// });
 
 app.listen(3000, () => console.log('Port 3000 running'));
