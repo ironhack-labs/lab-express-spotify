2536fb498fdfcee65f155a8322d6572290805528
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-07 08:16:57 -0400

    Done

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,3 +1,24 @@
+const express = require('express');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require("path");
+
+const app = express();
+app.use(morgan('combined'))
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname,"public")));
+
+hbs.registerPartials(__dirname + '/views/partials');
+
 var SpotifyWebApi = require('spotify-web-api-node');
 
 // Remember to paste here your credentials
@@ -16,3 +37,53 @@ spotifyApi.clientCredentialsGrant()
     }, function(err) {
         console.log('Something went wrong when retrieving an access token', err);
     });
+
+
+app.get("/", function(req, res){
+
+    res.status(200)
+        .render("index");
+});
+
+app.get("/artists", async function(req, res){
+
+    let response = await spotifyApi.searchArtists(req.query.artist);
+
+    if(response.statusCode === 200) {
+
+        let artists = response.body.artists.items;
+
+        res.status(200)
+            .render("artists", { artists: artists});
+    }
+
+});
+
+app.get('/albums/:artistId', async function(req, res)  {
+    let response = await spotifyApi.getArtistAlbums(req.params.artistId);
+
+    if(response.statusCode === 200) {
+
+        let albums = response.body.items;
+
+        res.status(200)
+            .render("albums", { albums: albums});
+    }
+});
+
+app.get('/tracks/:albumId', async function(req, res) {
+    let response = await spotifyApi.getAlbumTracks(req.params.albumId);
+
+    if(response.statusCode === 200) {
+
+        let tracks = response.body.items;
+
+        res.status(200)
+            .render("tracks", { tracks: tracks});
+    }
+});
+
+
+app.listen(3000, function(){
+    console.log("Connected to server!");
+});
