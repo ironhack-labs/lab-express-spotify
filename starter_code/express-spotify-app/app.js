cbfeb22fc48a6b092008361ffcdeab7ca8ff4aa0
Elvair <elvair@mountain>
2018-09-13 19:11:15 +0200

    hecha iteraccion 2

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,4 +1,28 @@
 var SpotifyWebApi = require('spotify-web-api-node');
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const path = require('path');
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+    res.render('index');
+})
+app.get('/artist', function (req, res) {
+    const artist = req.query.artist;
+    spotifyApi.searchArtists(artist)
+    .then(data => {
+      const songs = {song:data.body.artists.items};
+        res.render('artist', {songs})
+        // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    })
+    .catch(err => { console.log('An error happened:', err) });
+      // ----> 'HERE WE CAPTURE THE ERROR'
+    
+  })
+
 
 
 var clientId = 'd4cfe9a7536d4ba8b9dae6f18014a3db',
@@ -9,6 +33,10 @@ var spotifyApi = new SpotifyWebApi({
   clientSecret : clientSecret
 });
 
+hbs.registerHelper('json', function(context) {
+    return JSON.stringify(context);
+});
+
 // Retrieve an access token.
 spotifyApi.clientCredentialsGrant()
   .then(function(data) {
@@ -16,3 +44,7 @@ spotifyApi.clientCredentialsGrant()
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
 });
+
+app.listen(3000, () => {
+    console.log('Por si es necesario');
+  });
