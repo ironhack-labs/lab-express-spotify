e7a530790d2e310d5042cfc79ae49711042b43e5
David Sirit <davidsirit@yahoo.com>
2018-09-13 21:06:16 +0200

    iteration 2

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,17 +1,26 @@
-var SpotifyWebApi = require('spotify-web-api-node');
-
+const SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
+const path =require('path'); // ojo
 
 // Remember to paste here your credentials
-var clientId = '9cf4f5c16fe74e5a9eafdbfbc329850c',
+const clientId = '9cf4f5c16fe74e5a9eafdbfbc329850c',
     clientSecret = 'b3c42c37c54440afaea0abe5d5e6c71e';
 
-var spotifyApi = new SpotifyWebApi({
+const spotifyApi = new SpotifyWebApi({
   clientId : clientId,
   clientSecret : clientSecret
 });
+
+app.set('vie engine', 'hbs'); // ojo to set up
+app.set('views', __dirname + '/views'); // para render
+app.use(express.static(path.join(__dirname, 'public'))); // ojo static folder containing img, js, css
+hbs.registerPartials(__dirname + '/views/partials'); // ojo parial
+
+const bodyParser = require('body-parser'); // http path
+app.use(bodyParser.urlencoded({ extended: true}));
+
 
 // Retrieve an access token.
 spotifyApi.clientCredentialsGrant()
@@ -19,4 +28,12 @@ spotifyApi.clientCredentialsGrant()
     spotifyApi.setAccessToken(data.body['access_token']);
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
+});
+
+app.get('/',(req,res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('Port 3000')
 });
