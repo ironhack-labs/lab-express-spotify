9e7445cafe60bd2dc65a67023de22d41fffd82e7
Francis Wardley <fwardley90@gmail.com>
2018-09-13 20:30:13 +0200

    artists done

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,4 +1,20 @@
+const express = require('express');
+const app = express();
+const path    = require('path');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
 var SpotifyWebApi = require('spotify-web-api-node');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+hbs.registerHelper('json', function(context) {
+  return JSON.stringify(context);
+});
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
+
 
 // Remember to paste here your credentials
 var clientId = '0e5120d1e41c4cfba15184f7b57cb07b',
@@ -15,4 +31,33 @@ spotifyApi.clientCredentialsGrant()
     spotifyApi.setAccessToken(data.body['access_token']);
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
+});
+
+
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+app.get('/artists',(req,res)=>{
+  spotifyApi.searchArtists(req.query.artist)
+    .then(data => {
+      res.render('artists',{data})
+    })
+    .catch(err => {
+      // ----> 'HERE WE CAPTURE THE ERROR'
+    })
+})
+
+app.get('/albums/:artistId', (req, res) => {
+  spotifyApi.searchArtists(req.query.artist)
+    .then(data => {
+      res.render('albums',{data})
+    })
+    .catch(err => {
+      // ----> 'HERE WE CAPTURE THE ERROR'
+    })
+});
+
+app.listen(3000, () => {
+  console.log("listening 3000")
 });
