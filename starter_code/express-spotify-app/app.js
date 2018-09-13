40b9ed73f13660c7acb17fc845c3dc9ef1c271de
David Sirit <davidsirit@yahoo.com>
2018-09-13 19:35:33 +0200

    Iteration 2 Express Setup

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,8 +1,12 @@
 var SpotifyWebApi = require('spotify-web-api-node');
 
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
 // Remember to paste here your credentials
-var clientId = '1c30624cba6742dcb792991caecae571',
-    clientSecret = '746977b1e77240faa9d0d2411c3e0efe';
+var clientId = '9cf4f5c16fe74e5a9eafdbfbc329850c',
+    clientSecret = 'b3c42c37c54440afaea0abe5d5e6c71e';
 
 var spotifyApi = new SpotifyWebApi({
   clientId : clientId,
