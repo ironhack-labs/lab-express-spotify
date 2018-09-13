ee2ab7219a2b955ce3d85469bed0e84002a8def2
Mapno <mariorvinas@gmail.com>
2018-09-13 22:45:00 +0200

    homepage

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -2,9 +2,9 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
+const path = require('path');
+const bodyParser = require('body-parser');
 
-
-// Remember to paste here your credentials
 const clientId = '02d11ea571ac4574a9b78134e6135685',
     clientSecret = 'e1eab7ee07994192986c1c6d7c7d2217';
 
@@ -13,10 +13,24 @@ const spotifyApi = new SpotifyWebApi({
   clientSecret : clientSecret
 });
 
-// Retrieve an access token.
-spotifyApi.clientCredentialsGrant()
-  .then(function(data) {
-    spotifyApi.setAccessToken(data.body['access_token']);
-  }, function(err) {
-    console.log('Something went wrong when retrieving an access token', err);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view options', { layout: '/layouts/layout' });
+
+
+// // Retrieve an access token.
+// spotifyApi.clientCredentialsGrant()
+//   .then(function(data) {
+//     spotifyApi.setAccessToken(data.body['access_token']);
+//   }, function(err) {
+//     console.log('Something went wrong when retrieving an access token', err);
+// });
+
+app.get('/', (req, res) => {
+    res.render(`home`);
 });
+
+
+app.listen(3000);
