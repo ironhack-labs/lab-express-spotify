60c90d157bc91d0bb3d641a2482a3cdc96b10541
Gonzalo Quiroz <kiyegoo@gmail.com>
2018-10-01 22:16:09 -0500

    Primer commit

---
+++ starter_code/app.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var app = express();
+var hbs = require("hbs");
+var path = require("path");
+var bodyParser = require("body-parser");
+var SpotifyWebApi = require("spotify-web-api-node");
+
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Remember to paste here your credentials
+var clientId = "bd3bdc03a2d74dac8e922124aaf48035",
+  clientSecret = "9fe64272f17b469ebb4a519f27e099b9";
+
+var spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret
+});
+
+// Retrieve an access token.
+spotifyApi.clientCredentialsGrant().then(
+  function(data) {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  },
+  function(err) {
+    console.log("Something went wrong when retrieving an access token", err);
+  }
+);
+
+app.get("/", (req, res) => {
+  res.render("search");
+});
+
+app.get("/artists", (req, res) => {
+  res.json(req);
+});
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en puerto 3000");
+});
