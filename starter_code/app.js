21952c02ece032fc33ea984c65395f897152d35f
Gonzalo Quiroz <kiyegoo@gmail.com>
2018-10-05 22:39:09 -0500

    Ejercicio Finalizado

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,6 +10,8 @@ app.set("views", __dirname + "/views");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+hbs.registerPartials(`${__dirname}/views/partials`);
+
 // Remember to paste here your credentials
 var clientId = "bd3bdc03a2d74dac8e922124aaf48035",
   clientSecret = "9fe64272f17b469ebb4a519f27e099b9";
@@ -34,7 +36,42 @@ app.get("/", (req, res) => {
 });
 
 app.get("/artists", (req, res) => {
-  res.json(req);
+  spotifyApi
+    .searchArtists(req.query.artist)
+    .then(data => {
+      let artists = data.body.artists.items;
+
+      res.render("artists", { artists });
+    })
+    .catch(err => {
+      throw err;
+    });
+});
+
+app.get("/albums/:id", (req, res) => {
+  spotifyApi
+    .getArtistAlbums(req.params.id)
+    .then(data => {
+      let albums = data.body.items;
+
+      res.render("albums", { albums });
+    })
+    .catch(err => {
+      throw err;
+    });
+});
+
+app.get("/tracks/:id", (req, res) => {
+  spotifyApi
+    .getAlbumTracks(req.params.id)
+    .then(data => {
+      let tracks = data.body.items;
+
+      res.render("tracks", { tracks });
+    })
+    .catch(err => {
+      throw err;
+    });
 });
 
 app.listen(3000, () => {
