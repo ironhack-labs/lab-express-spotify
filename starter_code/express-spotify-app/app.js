cf39a438be1697a89ecb6e44002721e56ccd860b
Gabriel Cueva <gabrielcueva_10@hotmail.com>
2018-11-13 23:05:43 +0100

    algo mas

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,31 +1,32 @@
-var SpotifyWebApi = require('spotify-web-api-node');
-const express = require('express');
-const hbs = require('hbs');
+var SpotifyWebApi = require("spotify-web-api-node");
+const express = require("express");
+const hbs = require("hbs");
 const path = require("path");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const app = express();
 
-
 // Remember to paste your credentials here
-var clientId = 'd38ec50650f4406e9fab5321ca7fd23e',
-    clientSecret = '69b5d1200fde4c5c9cdd6d5d3166d214';
+var clientId = "d38ec50650f4406e9fab5321ca7fd23e",
+  clientSecret = "69b5d1200fde4c5c9cdd6d5d3166d214";
 
 var spotifyApi = new SpotifyWebApi({
-  clientId : clientId,
-  clientSecret : clientSecret
+  clientId: clientId,
+  clientSecret: clientSecret
 });
 
 // Retrieve an access token.
-spotifyApi.clientCredentialsGrant()
-  .then(function(data) {
-    spotifyApi.setAccessToken(data.body['access_token']);
-  }, function(err) {
-    console.log('Something went wrong when retrieving an access token', err);
-});
+spotifyApi.clientCredentialsGrant().then(
+  function(data) {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  },
+  function(err) {
+    console.log("Something went wrong when retrieving an access token", err);
+  }
+);
 
 app.set("view engine", "hbs");
-app.set("views",__dirname+"/views");
-app.use(express.static(path.join(__dirname,"public")));
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, "public")));
 
 hbs.registerPartials(__dirname + "/views/partials");
 
@@ -33,24 +34,36 @@ app.get("/", (req, res, next) => {
   res.render("index");
 });
 
-app.post('/artists', (req,res,next) => {
-  console.log("Entra aqui!");
-  let artist = req.body.artist;
-  console.log(artist);
-  spotifyApi.searchArtists(artist)
-    .then( data => {
-      res.render("artists");
-      console.log("Esto es lo que hay: ", data);
+app.get("/artists", (req, res, next) => {
+  let { artist } = req.query;
+  spotifyApi
+    .searchArtists(artist)
+    .then(data => {
+      const data1 = data.body.artists.items;
+      //console.log(data1);
+      res.render("artists", { data1 });
     })
-    .catch( err => {
-      console.log("Eeeerroooooor", err)
-    })
-})
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get("/:albums", (req, res, next) => {
+  let album  = req.params.artist;
+  console.log(album);
+  spotifyApi.getArtistAlbums(
+    album,
+    { limit: 10, offset: 20 },
+    function(err, data) {
+      if (err) {
+        console.error("Something went wrong!");
+      } else {
+        console.log(data.body);
+      }
+    }
+  );
+});
 
 const port = 3000;
 app.listen(port);
 console.log(`Listen to port ${port}`);
-
-
-
-
