d45e79ea529e7a4b69689dca370cb09d41368cd2
nadal084 <nadalriera84@gmail.com>
2018-11-14 19:05:31 +0100

    segunda iteracion

---
+++ starter_code/express-spotify-app/app.js
@@ -0,0 +1,57 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const path = require('path');
+var clientId = 'fb51551663fc475c86b74bc8489cd02d',
+   clientSecret = "7458cec514ca49fda735f136502ad462";
+
+var spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view options', { layout: '/layouts/layout' });
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Retrieve an access token.
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
+});
+
+app.get('/', (req, res) => {
+  res.render(`home`);
+});
+
+app.get('/artists', (req, res) => {
+  spotifyApi.searchArtists(req.query.artist)
+    .then(data => {
+      let artistsArray = [];
+      data.body.artists.items.forEach(e => {
+        artistsArray.push(e);
+      });
+      res.render('artists', {artistsArray});
+    })
+    .catch(e => {
+     console.log(e);
+
+     app.get('/albums/:artistId', (req, res) => {
+      spotifyApi.getArtistAlbums(req.params.artistId)
+      .then((data) => {
+        // console.log(data)
+        let albumsArray = [];
+        data.body.items.forEach(e => {
+          albumsArray.push(e);
+        });
+        res.render('albums', {albumsArray});
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    });
