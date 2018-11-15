3cff95892c8d05dca338f1081b457bb5d2a370a2
Zeswen <zeswentyrael@gmail.com>
2018-11-15 15:44:45 +0100

    Iteration 1

---
+++ starter_code/express-spotify-app/app.js
@@ -0,0 +1,16 @@
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const clientId = '46b54da592cb457e81cd31f44841e72c';
+const clientSecret = 'bae1796f914e49efa54122868216ea4c';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId,
+  clientSecret,
+});
+
+spotifyApi.clientCredentialsGrant()
+  .then((data) => {
+    spotifyApi.setAccessToken(data.body.access_token);
+  }, (err) => {
+    console.log('Something went wrong when retrieving an access token', err);
+  });
