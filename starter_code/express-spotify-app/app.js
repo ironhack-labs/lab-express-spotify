a0f7011d9616fbe7c816614c94141ed393c9f430
Zeswen <zeswentyrael@gmail.com>
2018-11-15 15:47:22 +0100

    Iteration 2

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,5 +1,12 @@
 const SpotifyWebApi = require('spotify-web-api-node');
 
+const express = require('express');
+
+const hbs = require('hbs');
+
+const app = express();
+
+
 const clientId = '46b54da592cb457e81cd31f44841e72c';
 const clientSecret = 'bae1796f914e49efa54122868216ea4c';
 
