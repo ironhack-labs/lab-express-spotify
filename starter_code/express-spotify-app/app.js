df80893d5fc00ddaae4beaaecfa4771ba38fe1b7
cocobilbao <cocobilbao@gmail.com>
2018-11-15 19:08:57 +0100

    iteration 3

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,9 +1,13 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 const hbs = require('hbs');
 var SpotifyWebApi = require('spotify-web-api-node');
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname,'public')));
 
 // Remember to paste your credentials here
 var clientId = '15ba046ee1564552893a8f02b1cd20a6',
@@ -21,3 +25,25 @@ spotifyApi.clientCredentialsGrant()
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
 });
+
+
+app.get('/', function (req, res) {
+  res.render('search')
+})
+
+app.get('/artists', function (req, res) {
+    spotifyApi.searchArtists(req.query.artist)
+    .then(data => {
+      res.render('artists', data.body.artists.items)
+    })
+    .catch(err => {
+      console.log("Ha ocurrido un error", err)
+    })
+
+  })
+
+
+
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
