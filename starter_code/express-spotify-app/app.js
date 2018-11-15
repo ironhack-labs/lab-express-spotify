d3626adf1cd1ada7a120e750af3ad91dcfb712c5
Sandra Luengo <luchosandra@gmail.com>
2018-11-15 18:23:59 +0100

    Iterarion 3

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,6 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+hbs.registerPartials(__dirname + '/views/partials');
+
 const SpotifyWebApi = require('spotify-web-api-node');
 
-// Remember to paste your credentials here
 const clientId = 'da9aa0e9ff6e4218b4da71958c55161f',
     clientSecret = '32d76dd61b624e938152360c256e7095';
 
@@ -9,10 +21,32 @@ const spotifyApi = new SpotifyWebApi({
     clientSecret: clientSecret
 });
 
-// Retrieve an access token.
+
 spotifyApi.clientCredentialsGrant()
     .then(function (data) {
         spotifyApi.setAccessToken(data.body['access_token']);
     }, function (err) {
         console.log('Something went wrong when retrieving an access token', err);
     });
+
+
+app.get('/', (req, res, next) => {
+    res.render('index');
+});
+
+app.post('/artists', (req, res, next) => {
+    let artist = req.body.artist;
+    spotifyApi.searchArtists(artist)
+        .then(data => {
+           
+            const artists = data.body.artists.items;
+            console.log(artists)
+            res.render('artists', { artists })
+        })
+        .catch(err => {
+            console.log(`Error en el Seachr a la api ${err}`);
+        })
+})
+
+
+app.listen(3000);
