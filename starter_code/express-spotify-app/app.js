c388681941f493ab425d993a048ce42616852e15
Omar Avelar <omar.avelar.f@gmail.com>
2018-11-22 12:00:33 -0600

    3 done

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,4 +1,17 @@
 var SpotifyWebApi = require('spotify-web-api-node');
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+//static
+app.use(express.static('public'))
+
+//views
+app.set('views',__dirname + '/views')
+app.set('view engine', 'hbs')
+
+const spotifyRoutes = require('./routes/routes')
+app.use('/', spotifyRoutes)
 
 // Remember to paste your credentials here
 var clientId = 'b527e8cea1eb44479b510b93de75d167',
@@ -16,3 +29,13 @@ spotifyApi.clientCredentialsGrant()
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
 });
+
+//route
+
+app.get('/', (req,res)=>{
+  res.send('yuhiuu')
+})
+
+app.listen(3000, ()=>{
+  console.log('Listening on port 3000')
+})
