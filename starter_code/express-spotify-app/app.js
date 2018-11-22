10c0fe7ef50b2d45b2f5c9b2ab60fdccf19c90b7
Omar Avelar <omar.avelar.f@gmail.com>
2018-11-22 12:52:38 -0600

    4 almost done

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -32,9 +32,9 @@ spotifyApi.clientCredentialsGrant()
 
 //route
 
-app.get('/', (req,res)=>{
-  res.send('yuhiuu')
-})
+//app.get('/', (req,res)=>{
+  //res.send('yuhiuu')
+//})
 
 app.listen(3000, ()=>{
   console.log('Listening on port 3000')
--- starter_code/express-spotify-app/routes/routes.js
+++ starter_code/express-spotify-app/routes/routes.js
@@ -1,8 +1,44 @@
+var SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express')
 const router = express.Router()
 
+// Remember to paste your credentials here
+var clientId = 'b527e8cea1eb44479b510b93de75d167',
+    clientSecret = '8f169e89dccb4c15952c3fe405d391ec';
+
+var spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+// Retrieve an access token.
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
+});
+
+
+
 router.get('/', (req,res)=>{
-  res.render('home')
+  const {search} = req.query
+  //busqueda = {
+    //name:search
+  //}
+  if(search){
+    spotifyApi.searchArtists(search, {limit:2})
+      .then(data=>{
+        console.log(data)
+      }).catch(error =>{
+          console.log(error)
+      })
+      
+    //res.render('artists',busqueda)
+  }else{
+    res.render('home')
+  }
+ 
 })
 
 module.exports = router
