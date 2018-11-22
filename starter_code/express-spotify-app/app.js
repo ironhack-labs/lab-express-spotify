6d5f582bcc86a1cf78dbf1033a85ba9c9998f354
MontseOrtiz <montse_2908@hotmail.com>
2018-11-22 11:44:55 -0600

    Iteracion1

---
+++ starter_code/express-spotify-app/app.js
@@ -0,0 +1,18 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+
+// Remember to paste your credentials here
+var clientId = '3197ad10c1284cd1bcf921cf280c12ee',
+    clientSecret = 'febf08b7702d400f86df39a54b5dbd41';
+
+var spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+// Retrieve an access token.
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
+});
