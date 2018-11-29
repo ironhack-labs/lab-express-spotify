e53eae6ae6221bf3837227008c0cf78ac405ea03
Rita Mernissi <mernissi.rita@gmail.com>
2018-11-29 21:28:55 +0100

    view artists, view albums DONE - Next step : create route view tracks

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,52 +1,70 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const hbs = require('hbs');
-var SpotifyWebApi = require('spotify-web-api-node');
-const path = require('path');
+const hbs = require("hbs");
+var SpotifyWebApi = require("spotify-web-api-node");
+const path = require("path");
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static("public"));
 
 // Remember to paste your credentials here
-var clientId = '8378bd5b4d934462980eb5f23d57271d',
-    clientSecret = 'f89ddb6a06a946e18e60f065e751ece3';
+var clientId = "8378bd5b4d934462980eb5f23d57271d",
+  clientSecret = "f89ddb6a06a946e18e60f065e751ece3";
 
 var spotifyApi = new SpotifyWebApi({
-  clientId : clientId,
-  clientSecret : clientSecret
+  clientId: clientId,
+  clientSecret: clientSecret
 });
 
 // Retrieve an access token.
-spotifyApi.clientCredentialsGrant()
-  .then(function(data) {
-    spotifyApi.setAccessToken(data.body['access_token']);
-  }, function(err) {
-    console.log('Something went wrong when retrieving an access token', err);
-});
+spotifyApi.clientCredentialsGrant().then(
+  function(data) {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  },
+  function(err) {
+    console.log("Something went wrong when retrieving an access token", err);
+  }
+);
 
 //___________________________ROUTES_________________________________________________________
 
-hbs.registerPartials(__dirname + "/views/partials")
+hbs.registerPartials(__dirname + "/views/partials");
 
 // Route Home
 app.get("/", (req, res, next) => {
-  res.render('index')
-})
+  res.render("index");
+});
 
-// Route Artists
-app.get('/artists', function (req, res) {
+// Route Artists => pour afficher les artites avec un certain nom (ex: parov)
+app.get("/artists", function(req, res) {
   // récupère la data liée à un artist
-  spotifyApi.searchArtists(req.query.artist)
-  .then(data => {    
-    console.log("super") 
-    res.render("artists", {artists:data.body.artists.items});
-  })
-  .catch(err => {
-    console.log("erreur") 
-  })
-})
+  spotifyApi
+    .searchArtists(req.query.artist)
+    .then(data => {
+      // res.send(data.body.artists);
+      res.render("artists", { artists: data.body.artists.items });
+
+    })
+    .catch(err => {
+      console.log("erreur");
+    });
+});
 
 
+
+// Route Albums => pour voir les albums d'un artiste
+app.get('/albums/:artistId', (req, res) => {
+  spotifyApi
+    .getArtistAlbums(req.params.artistId)
+    .then(result => {
+      // res.send(result.body.items);
+      res.render("albums", {albums:result.body.items});
+    })
+    .catch(err => {
+      console.log("erreur");
+    })
+});
 
 //______________________________________________________________________________________________________
 
