5fc9fab7060c6a7dee38a0f8d6dd8002010c27ed
nicolas prieur <nicolas.prieurdelacomble@gmaile.com>
2018-11-29 21:32:05 +0100

    getArtistAlbums

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const hbs = require('hbs');
 const path = require('path');
-const spotifyApi = 
 
 var SpotifyWebApi = require('spotify-web-api-node');
+
+app.use(express.static('public'))
 
 
 app.set('views', path.join(__dirname, 'views'));
@@ -37,16 +38,34 @@ app.get('/', function (req, res) {
   res.render('index')
 })
 
-app.get('/artists', function (req, res) {
-  console.log(req.query)
-  res.render('index')
+
+app.get('/artists', (req, res) => {
   spotifyApi.searchArtists(req.query.artist)
-})
+    .then( data => {
+      console.log(data.body.artists.items)
+      res.render('artists', {artists: data.body.artists.items})
+    })
+    .catch(err => {
+      console.log(err)
+    })
+ });
 
 
- app.get('/artists', function (req, res, next) {
+ app.get('/albums/:artistId', (req, res) => {
+  spotifyApi.getArtistAlbums(req.params.artistId)
+    .then(data => {
+      console.log(data.body.items)
+      res.render('albums', {albums: data.body.items})
+    })
+    .catch(err => {
+      console.log(err)
+    })
+ });
 
-})
+
+
+
+
 
 
 
