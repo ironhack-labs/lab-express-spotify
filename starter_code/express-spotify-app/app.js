2e4d00d66fef1e3c86439168e70972678221ff99
JulieCCe <julie.chane@gmail.com>
2018-12-02 13:11:24 +0100

    done

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,45 +1,80 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const hbs = require('hbs');
-const path = require('path');
+const hbs = require("hbs");
+const path = require("path");
 hbs.registerPartials(__dirname + "/views/partials");
 
-var SpotifyWebApi = require('spotify-web-api-node');
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "hbs");
+
+// SPOTIFY
+var SpotifyWebApi = require("spotify-web-api-node");
 
 // Remember to paste your credentials here
-var clientId = '53596dcf73f44f81a482e00c18abee3a',
-    clientSecret = '5a2084913d5a4220a5c541e77ba75003';
+var clientId = "53596dcf73f44f81a482e00c18abee3a",
+  clientSecret = "5a2084913d5a4220a5c541e77ba75003";
 
 var spotifyApi = new SpotifyWebApi({
-    clientId : clientId,
-    clientSecret : clientSecret
+  clientId: clientId,
+  clientSecret: clientSecret
 });
 
 // Retrieve an access token.
-spotifyApi.clientCredentialsGrant()
-  .then(function(data) {
-    spotifyApi.setAccessToken(data.body['access_token']);
-  }, function(err) {
-    console.log('Something went wrong when retrieving an access token', err);
+spotifyApi.clientCredentialsGrant().then(
+  function(data) {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  },
+  function(err) {
+    console.log("Something went wrong when retrieving an access token", err);
+  }
+);
+
+//ROUTES
+
+app.get("/", function(req, res) {
+  res.render("index");
 });
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.get('/', function (req, res) {
-  res.render('index')
-})
-
-app.get('/artists', function (req, res) {
-    spotifyApi.searchArtists(req.query.artist)
+app.get("/artists", function(req, res) {
+  spotifyApi
+    .searchArtists(req.query.artist)
     .then(data => {
-        console.log(data.body.artists.items)
-        var searchedList = { artistList : data.body.artists.items }
-        res.render('artist', {searchedList})
+      var searchedList = data.body.artists.items;
+      res.render("artist", {list: searchedList , title: req.query.artist + " results"});
     })
     .catch(err => {
-      console.log("Erreur, déso")
-    })
-  })
+      console.log("Erreur, déso");
+    });
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.get("/albums/:artistID", function(req, res, next) {
+  spotifyApi
+    .getArtistAlbums(req.params.artistID)
+    .then(data => {
+      var artistalbum = data.body.items;
+      res.render("albums", { albumslist: artistalbum });
+    })
+    .catch(err => {
+      console.log("Erreur n°2, déso", err);
+      next();
+    });
+});
+
+// get tracks in an album
+app.get("/tracks/:albumID", function(req, res, next) {
+  spotifyApi
+  .getAlbumTracks(req.params.albumID)
+    .then(data => {
+      var albumtracks = data.body.items;
+      res.render("tracks", {trackslist: albumtracks });
+    })
+    .catch(err => {
+      console.log("Erreur n°3, déso", err);
+      next();
+    });
+});
+
+
+// SERVEUR
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
