10fb026355ad6e14ecea610aa4733a55717bffd7
Luise Schneider <luise.schneider@berlin.de>
2018-12-15 18:10:57 +0100

    iteration 1 and 2 done, not pretty

---
+++ starter_code/express-spotify-app/app.js
@@ -0,0 +1,53 @@
+const SpotifyWebApi = require("spotify-web-api-node");
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views/layout");
+app.use(express.static(__dirname + "/public"));
+
+const clientId = "b97f371b01864066b33b0df9a243e401",
+  clientSecret = "d779c60a24274903b956eb6fab9f82f4";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret
+});
+
+// Retrieve an access token.
+spotifyApi.clientCredentialsGrant().then(
+  function(data) {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  },
+  function(err) {
+    console.log("Something went wrong when retrieving an access token", err);
+  }
+);
+
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+app.post("/artists", function(req, res) {
+  // res.send(req.body.artist);
+  spotifyApi
+    .searchArtists(req.body.artist)
+    .then(data => {
+      // res.send(data);
+      let artists = data.body.artists.items.map(artist => {
+        return artist;
+      });
+      res.render("artists", { artists });
+    })
+
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.listen(3000, () => console.log("Spotify app listening on port 3000!"));
