c65d4253f5061220fa301cedcc7970035c49b5e3
Roberto <roberto.m.aragon@gmail.com>
2018-12-29 14:56:12 +0100

    almost done

---
+++ starter_code/app.js
@@ -0,0 +1,85 @@
+const SpotifyWebApi = require('spotify-web-api-node');
+const express = require('express');
+const hbs = require('hbs');
+const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const app = express();
+
+
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/',(req, res, next) => {
+  res.render('index')
+})
+
+app.get('/artists', (req, res, next) => {
+  // const term = req.query.term;
+  const { term } = req.query
+  spotifyApi.searchArtists(term)
+    .then(data => {
+      // res.send(data.body)
+      console.log(data.body.artists.items);
+      res.render('artists', {
+        artists: data.body.artists.items
+      });
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    })
+    .catch(err => {
+      // ----> 'HERE WE CAPTURE THE ERROR'
+    })
+});
+
+app.get('/albums/:artistId', (req, res, next) => {
+  spotifyApi.getArtistAlbums(req.params.artistId)
+    
+    .then(data => {console.log('Artist albums', data.body.items[0])
+      res.render('albums', { 
+      albums: data.body.items
+    })  
+    })
+    .catch(err => {console.log(err)
+    });
+})
+
+app.get('/playlist/:albumId', (req, res, next) => {
+spotifyApi.getAlbumTracks(req.params.albumId)
+  .then(data => {
+    res.render('audio', { 
+      audios: data.body.items
+  })
+  .catch(err => {
+    console.log('Something went wrong!', err);
+  });
+
+})
+});
+
+// Remember to paste your credentials here
+const clientId = 'xxxxxxx',
+    clientSecret = 'xxxxxxx';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId,
+  clientSecret
+});
+
+// Retrieve an access token.
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
+});
+
+app.listen(PORT, () => {
+  console.info(`App listen at ${PORT} port`);
+});
