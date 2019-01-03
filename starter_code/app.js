161eaf96e1e0da39f5d2165c742a787d29ebe77d
Rodrigo Otero <roterom82@gmail.com>
2019-01-03 17:28:12 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
-const PORT = 3000;
-var SpotifyWebApi = require('spotify-web-api-node');
+const PORT = process.env.PORT || 3000;
+const SpotifyWebApi = require('spotify-web-api-node');
+
+require('dotenv').config();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
 
 // Remember to paste your credentials here
 
-var spotifyApi = new SpotifyWebApi({
-  clientId : clientId,
-  clientSecret : clientSecret
+const spotifyApi = new SpotifyWebApi({
+  clientId : process.env.CLIENT_ID,
+  clientSecret : process.env.CLIENT_SECRET
 });
 
 // Retrieve an access token.
@@ -22,27 +25,24 @@ spotifyApi.clientCredentialsGrant()
     console.log('Something went wrong when retrieving an access token', err);
 });
 
-app.get('/',(req, res,next)=>{
+app.get('/',(req, res, next)=>{
   res.render('index');
 })
 
-
-app.get("/artist", (req, res, next) => {
+app.get("/artists", (req, res, next) => {
   const {artist} = req.query;
 
   spotifyApi.searchArtists(artist)
-    .then(data => {
-        // res.send(data);      
+    .then(data => {    
       res.render("artists", {artists: data.body.artists.items});
     })
 })
 
-app.get("/artist/:artistId", (req, res, next) => {
+app.get("/artists/:artistId", (req, res, next) => {
   const artist = req.params.artistId;
 
   spotifyApi.getArtistAlbums(artist)
-    .then(data => {
-        //res.send(data.body.items);      
+    .then(data => {    
       res.render("albums", {albums: data.body.items});
     })
 })
@@ -57,10 +57,6 @@ app.get("/albums/:albumId", (req, res, next) => {
     })
 })
 
-
-
 app.listen(PORT, () =>  {
 console.info(`APP listen at ${PORT} port`);
 })
-
-
