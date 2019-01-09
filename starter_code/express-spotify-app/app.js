69deae44314ee92d5067546d34a4216348ecf385
JZunigaV <j_zuniga4@hotmail.com>
2019-01-09 15:39:24 -0600

    Prueba-done

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,58 +1,107 @@
-const SpotifyWebApi = require('spotify-web-api-node');
-const express = require('express');
-const bodyParser = require('body-parser');
-const hbs = require('hbs');
-const path = require('path');
+//Using Es6
+
+//Packages
+const SpotifyWebApi = require("spotify-web-api-node");
+const express = require("express");
 const app = express();
+const hbs = require("hbs");
+const path = require("path");
+const bodyParser = require("body-parser");
 
-// Remember to paste your credentials here
-// let clientId = process.env.CLIENTID;
-// let clientSecret = process.env.CLIENTSECRET;
+//Views config
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, "public")));
 
-let clientId = '04f3b20f3d064bfea97e62570d43938b';
-let clientSecret = 'c24c2e09731543ed886bcfb385dcffa6';
+//Spotify Api credentials
+const clientId = "6c4c1b57e7db440cb3737ea1eb4a401a";
+const clientSecret = "8a7b26ca70dd4bc0a726ca459d465828";
 
- let spotifyApi = new SpotifyWebApi({
-   clientId : clientId,
-   clientSecret : clientSecret
- });
-
- // Retrieve an access token.
- spotifyApi.clientCredentialsGrant()
-   .then(function(data) {
-     spotifyApi.setAccessToken(data.body['access_token']);
-   }, function(err) {
-     console.log('Something went wrong when retrieving an access token', err);
- });
-
-// Specify everything
-app.set("views", path.join(__dirname, 'views'));
-app.set("view engine", 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: true}));
-
-// View for home route
-app.get('/', (req, res) => {
-  res.render('index');
+let spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret
 });
 
-// Artists route
-app.post('/artists', (req, res) => {
-  //Find the artist on the webpage
+//Retrieve an access token in es6
+spotifyApi.clientCredentialsGrant().then(
+  data => {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+    console.log("logged to spotify");
+  },
+  err =>
+  console.log("Something went wrong when retrievinf an access token", err)
+);
+
+//Routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+//Artists route
+app.post("/artists", (req, res) => {
   let artist = req.body.artist;
-  //Call Spotify API
-  spotifyApi.searchArtists(artist)
+  spotifyApi
+    .searchArtists(artist)
     .then(data => {
-      res.render('artists', data.body);
-      //console.log(data);
+      res.render("artists", {
+        artists: data.body.artists.items
+      });
+
     })
     .catch(err => {
-      console.log("Error ", err);
-    })
+      console.log("There was an error returning artist", err);
+    });
 });
 
-// Albums route
+//Get albums
+app.get("/albums/:artistId", (req, res) => {
+  let id = req.params;
+  let lol = id.artistId;
 
-// Tracks
+  spotifyApi
+    .getArtistAlbums(lol, {
+      limit: 40,
+      offset: 1
+    })
+    .then(data => {
+      res.render("Albums", {
+        albums: data.body.items
+      });
 
-app.listen(3000, () => console.log("Listo"));
+    })
+    .catch(err => {
+      console.log("An error has ocurred", err);
+    });
+});
+
+// //Get album tracks
+// // Get tracks in an album
+app.get("/tracks/:trackId", (req, res) => {
+  let track = req.params;
+  let trackId = track.trackId;
+
+
+  //Api call
+  // Get tracks in an album
+  spotifyApi
+    .getAlbumTracks(trackId, {
+      limit: 30,
+      offset: 0
+    })
+    .then(data => {
+      res.render("tracks", {
+        tracks: data.body.items
+      });
+
+    })
+    .catch(err => {
+      console.log("An error has ocurred", err);
+    });
+});
+
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
