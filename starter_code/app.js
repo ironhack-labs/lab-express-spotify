3fd6d389481e8b4b78257aa2ac5ed8205823a671
Pedro Arantes <pedro.hga90@gmail.com>
2019-01-30 22:40:59 +0100

    same bad request

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,7 +8,7 @@ const SpotifyWebApi = require("spotify-web-api-node");
 const app = express();
 
 app.set("view engine", "hbs");
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/public"));
 
 // setting the spotify-api goes here:
