39b570abe1c28dfe4816de22929d419fa59260b3
tomdreamer <thomas.lesage@hotmail.com>
2019-02-05 09:28:12 +0100

    Paris - Delphine & Thomas

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const hbs = require('hbs');
+hbs.registerPartials(__dirname + "/views/partials");
 
+
+//Spotify 
+const SpotifyWebApi = require('spotify-web-api-node');
+
+// Remember to insert your credentials here
+const clientId = '68066f567475414abf720987c8d38af1',
+    clientSecret = '1570cb230276449095f1976c60a616c3';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then( data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  })
 // require spotify-web-api-node package here:
 
 
@@ -24,3 +46,48 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
+
+//Routes
+//----------------------------------------
+//########################################
+
+app.get("/", (request, response, next)=>{
+    response.render("index");
+});
+
+app.get("/artists", (request, response, next)=>{
+    const {search_query} = request.query;
+
+    spotifyApi.searchArtists(search_query)
+    .then(data => {
+  
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+      response.locals.reponseOfSearch = data.body.artists.items;
+    //   response.json(data);
+      response.render("artists.hbs");
+    })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    });
+});
+
+app.get("/albums/:artistId", (request, response, next)=>{
+    // requestt.params is how you acces information in the PETH/URL of you route
+    //  request.params = {"netflixId": "80018191"}
+    // response.send(request.params);
+
+
+    const {artistId} = request.params;
+
+        spotifyApi.getArtistAlbums(artistId)
+        .then(data => {
+            // response.json(data);
+            console.log('Artist albums', data.body);
+            //if we had a database we would do it like this
+            response.locals.artistId = data.body.items;
+            response.render("albums.hbs");
+        }).catch(err => {
+
+            console.error(err);
+        });
+});
