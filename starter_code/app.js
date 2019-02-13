fad89ad142ade0bd477ca7014229d38bb6dea187
Victoria Hickam <vhickam@gmail.com>
2019-02-13 15:31:49 -0500

    done except styling

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
-
+const SpotifyWebApi = require('spotify-web-api-node');
 
 
 const app = express();
@@ -13,13 +13,76 @@ app.use(express.static(__dirname + '/public'));
 
 
 // setting the spotify-api goes here:
+// Remember to insert your credentials here
+const clientId = '0354173528464fa0a696ba04e24f1cda',
+    clientSecret = 'f1a8b05914964bc4b3e11ff545b2a039';
 
+const spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then( data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  })
 
 
 
 
 
 // the routes go here:
+app.get('/', (req, res, next) => {
+  res.render('home');
+});
+
+
+app.get('/artists', (req, res, next) => {
+  spotifyApi.searchArtists(req.query.artist)
+    
+  .then(data =>{
+      console.log("The received data from the API: ", data.body.artists.items);
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+       //res.send(data.body);
+       
+        res.render('artists', {myArtists: data.body.artists.items});
+    
+ 
+    })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    })
+});
+
+app.get('/albums/:artistId', (req, res, next) => {
+  spotifyApi.getArtistAlbums(req.params.artistId)
+  .then(function(data) {
+    console.log('Artist albums', data);
+   //res.send(data.body);
+   res.render('albums', {artistAlbums: data.body.items});
+  },
+  
+  function(err) {
+    console.error(err);
+  });
+});
+
+app.get('/tracks/:albumId', (req, res, next) => {
+  spotifyApi.getAlbumTracks(req.params.albumId)
+  .then(function(data) {
+    console.log('Album tracks', data);
+   //res.send(data.body);
+   res.render('tracks', {albumTracks: data.body.items});
+  },
+  
+  function(err) {
+    console.error(err);
+  });
+});
 
 
 
