abdbbb811d9eaefe9c3274084fe93ce140aa2dd6
Jack Fischer <jrfisch95@gmail.com>
2019-02-13 11:16:02 -0500

    Reorganized file structure

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const SpotifyWebApi = require('spotify-web-api-node');
 
 // require spotify-web-api-node package here:
 
@@ -15,8 +16,23 @@ app.use(express.static(__dirname + '/public'));
 // setting the spotify-api goes here:
 
 
+//Inserting credentials
+const clientId = '665bc553b6bf45428e46d261854c3969',
+  clientSecret = 'bfe565f44c854c218b1ffd7f8dcb1cf5';
 
+const spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
 
+//Retrieving access token
+spotifyApi.clientCredentialsGrant()
+  .then( data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch( error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  });
 
 
 // the routes go here:
