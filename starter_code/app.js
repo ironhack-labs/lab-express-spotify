d3ea14307606c57c7f1ddb52cd132877a86d19ef
Roberto Konanz <robkonanz@gmail.com>
2019-02-13 12:08:02 -0500

    Send data to artists page but just all data and no styling.

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,26 +1,49 @@
 const express = require('express');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
-
-
-
+const SpotifyWebApi = require('spotify-web-api-node');
+const bodyParser = require('body-parser'); //needed for req.body
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true })); //needed for req.body
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-
 // setting the spotify-api goes here:
+const clientId = '13284d31ccb34468827e7799a2f6fddd',
+    clientSecret = 'a99d8b952bb94bdd8c5068bcfb176adf';
 
+const spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
 
+app.get('/', (req, res, next) => {
+    res.render('layout');
+});
 
+app.get('/artists', (req, res, next) => {
+    console.log(req.body, req.params, req.query);
+    spotifyApi.searchArtists(req.query.artist)
+    .then(data => {
+      console.log("The received data from the API: ", data.body);
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+      res.send(data.body);
+    })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    })
+});
 
-
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then( data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  })
 
 // the routes go here:
-
-
-
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
