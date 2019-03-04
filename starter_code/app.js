def0217a0e2a7e10c999717c67855044dc3e9d67
Abel Colmenares Napolitano <abelpronet@gmail.com>
2019-03-04 09:17:02 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
-
 // require spotify-web-api-node package here:
 const SpotifyWebApi = require('spotify-web-api-node');
 
@@ -36,7 +35,77 @@ spotifyApi.clientCredentialsGrant()
 
 
 
-// the routes go here:
+// The routes go here:
+
+// Index Route
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+// Artist Route
+app.get('/artist', (req, res) => {
+
+    console.log(req.query.requestArtist)
+
+    spotifyApi.searchArtists(req.query.requestArtist)
+
+    .then(data => {
+            console.log("The received data from the API: ", data.body.artists.items);
+            // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+            res.render('artist', { artist: data.body.artists.items });
+        })
+        .catch(err => {
+            console.log("The error while searching artists occurred: ", err);
+        })
+
+});
+
+// Albums Route
+
+
+app.get('/albums/:id', (req, res, next) => {
+
+    console.log(req.params.id)
+    spotifyApi.getArtistAlbums(req.params.id)
+
+    .then(data => {
+
+            console.log('Artist albums', data.body.items);
+            res.render('albums', { albums: data.body.items })
+        })
+        .catch(err => {
+            console.log("The error while searching albums occurred: ", err);
+        })
+
+});
+
+// Tracks Route
+
+app.get('/tracks/:id', (req, res, next) => {
+
+    console.log(req.params.id)
+    spotifyApi.getAlbumTracks(req.params.id, { limit: 5, offset: 1 })
+
+    .then(data => {
+
+            console.log('Artist albums tracks', data.body.items);
+            res.render('tracks', { tracks: data.body.items })
+        })
+        .catch(err => {
+            console.log("The error while searching tracks occurred: ", err);
+        })
+
+});
+
+// app.get('/tracks/:id', (req, res) => {
+//     spotifyAPI.getAlbumTracks(req.params.id)
+//         .then(tracks => {
+//             console.log(tracks.body.items)
+//             res.render('tracks', { tracks: tracks.body.items })
+//         })
+// })
+
+
 
 
 
