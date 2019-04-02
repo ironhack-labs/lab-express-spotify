e4af934f4a068df6903bcf0f5fea40a1f8acfdc4
Josue Valencia <josue.valencia@domo.ec>
2019-04-02 21:56:36 +0200

    index ready

--- starter_code/app.js
+++ starter_code/app.js
@@ -38,6 +38,8 @@ spotifyApi.clientCredentialsGrant()
 
 // the routes go here:
 
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
 
-
-app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
+app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧  🥁  🎸  🔊"));
