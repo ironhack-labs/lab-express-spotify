d44b7bad9742ef700efbca1466019a8aff2cdb21
gui <gbrein@gmail.com>
2019-04-02 21:07:51 -0300

    public e views criadas

--- starter_code/app.js
+++ starter_code/app.js
@@ -21,25 +21,23 @@ const clientId = 'fb9dc24cfc014e4b9b0842ea98275de4',
     clientSecret = '2f44b0604e604c5eaad6e3372e3f853b';
 
 const spotifyApi = new SpotifyWebApi({
-  clientId : clientId,
-  clientSecret : clientSecret
+    clientId: clientId,
+    clientSecret: clientSecret
 });
 
 // Retrieve an access token
 spotifyApi.clientCredentialsGrant()
-  .then( data => {
-    spotifyApi.setAccessToken(data.body['access_token']);
-  })
-  .catch(error => {
-    console.log('Something went wrong when retrieving an access token', error);
-  })
-
-
-
-
-
+    .then(data => {
+        spotifyApi.setAccessToken(data.body['access_token']);
+    })
+    .catch(error => {
+        console.log('Something went wrong when retrieving an access token', error);
+    })
 // the routes go here:
-
+app.get('/', (req, res, next) => {
+    res.render('index');
+  });
+  
 
 
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
