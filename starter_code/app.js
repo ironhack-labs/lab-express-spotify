29a67359d49e63b2131ba7e67bdad0bf2a4025f7
gui <gbrein@gmail.com>
2019-04-02 21:06:04 -0300

    app.js criado link com api

--- starter_code/app.js
+++ starter_code/app.js
@@ -3,6 +3,9 @@ const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
 
+var Spotify = require('spotify-web-api-js');
+var s = new Spotify();
+var spotifyApi = new SpotifyWebApi();
 
 
 const app = express();
@@ -13,7 +16,23 @@ app.use(express.static(__dirname + '/public'));
 
 
 // setting the spotify-api goes here:
+// Remember to insert your credentials here
+const clientId = 'fb9dc24cfc014e4b9b0842ea98275de4',
+    clientSecret = '2f44b0604e604c5eaad6e3372e3f853b';
 
+const spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then( data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  })
 
 
 
