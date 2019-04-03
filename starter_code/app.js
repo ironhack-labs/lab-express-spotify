9fcd9218086204b6d5a4354798d66ee548b13d11
gui <gbrein@gmail.com>
2019-04-03 10:04:25 -0300

    configurando metodo get /artist

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,43 +1,52 @@
 const express = require('express');
 const hbs = require('hbs');
+const SpotifyWebApi = require('spotify-web-api-node');
 
-// require spotify-web-api-node package here:
+hbs.registerPartials(`${__dirname  }/views/partials`);
 
-var Spotify = require('spotify-web-api-js');
-var s = new Spotify();
-var spotifyApi = new SpotifyWebApi();
-
+const bodyParser = require('body-parser');
 
 const app = express();
-
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set('views', `${__dirname}/views`);
+app.use(express.static(`${__dirname}/public`));
 
 
 // setting the spotify-api goes here:
 // Remember to insert your credentials here
-const clientId = 'fb9dc24cfc014e4b9b0842ea98275de4',
-    clientSecret = '2f44b0604e604c5eaad6e3372e3f853b';
+const clientId = 'fb9dc24cfc014e4b9b0842ea98275de4';
+const clientSecret = '2f44b0604e604c5eaad6e3372e3f853b';
 
 const spotifyApi = new SpotifyWebApi({
-    clientId: clientId,
-    clientSecret: clientSecret
+  clientId:clientId,
+  clientSecret:clientSecret
 });
 
 // Retrieve an access token
 spotifyApi.clientCredentialsGrant()
-    .then(data => {
-        spotifyApi.setAccessToken(data.body['access_token']);
-    })
-    .catch(error => {
-        console.log('Something went wrong when retrieving an access token', error);
-    })
+  .then((data) => {
+    spotifyApi.setAccessToken(data.body.access_token);
+  })
+  .catch((error) => {
+    console.log('Something went wrong when retrieving an access token', error);
+  });
 // the routes go here:
 app.get('/', (req, res, next) => {
-    res.render('index');
-  });
-  
+  res.render('index');
+});
 
 
-app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
+app.get('/artist', (req, res, next) => {
+  spotifyApi.getArtistAlbums(req.query.search)
+    .then((data) => {
+      console.log('The received data from the API: ', data.body.artists.items);
+      res.render('artists', { data });
+    }, (err) => {
+      console.error(err);
+    });
+});
+
+app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
