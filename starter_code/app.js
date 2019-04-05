873fd14e39193575d8b0339a51884a627bc9be01
gui <gbrein@gmail.com>
2019-04-05 19:25:56 -0300

    rota album encontrada

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,49 +1,71 @@
-const express = require('express');
-const hbs = require('hbs');
-const SpotifyWebApi = require('spotify-web-api-node');
+const express = require("express");
+const hbs = require("hbs");
+const SpotifyWebApi = require("spotify-web-api-node");
 
-hbs.registerPartials(`${__dirname  }/views/partials`);
-const bodyParser = require('body-parser');
+hbs.registerPartials(`${__dirname}/views/partials`);
+const bodyParser = require("body-parser");
+
 const app = express();
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
-app.set('view engine', 'hbs');
-app.set('views', `${__dirname}/views`);
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", `${__dirname}/views`);
 app.use(express.static(`${__dirname}/public`));
 
-//spotify settings
-const clientId = 'fb9dc24cfc014e4b9b0842ea98275de4';
-const clientSecret = '2f44b0604e604c5eaad6e3372e3f853b';
+// spotify settings
 
 const spotifyApi = new SpotifyWebApi({
-  clientId:clientId,
-  clientSecret:clientSecret
+  clientId: "57c4285787524ec8be118f03d3a36260",
+  clientSecret: "605563e401d546768a69d763cc83e281"
 });
 
-spotifyApi.clientCredentialsGrant()
-  .then((data) => {
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => {
     spotifyApi.setAccessToken(data.body.access_token);
   })
-  .catch((error) => {
-    console.log('Something went wrong when retrieving an access token', error);
+  .catch(error => {
+    console.log("Something went wrong when retrieving an access token", error);
   });
 
-
 // the routes go here:
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-
-app.get('/artist', (req, res, next) => {
-  spotifyApi.getArtistAlbums(req.query.name)
-    .then((data) => {
-      console.log('The received data from the API: ', data.body.artists.items);
-      res.render('artists', { data });
-    }, (err) => {
+app.get("/artist", (req, res, next) => {
+  console.log(req.query);
+  const artistName = req.query.name;
+  spotifyApi.searchArtists(artistName).then(
+    data => {
+      const dataArtist = data.body.artists.items;
+      // console.log(dataArtist);
+      res.render("artist", { dataArtist });
+    },
+    err => {
       console.error(err);
-    });
+    }
+  );
 });
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.get("/albums/:albumsId", (req, res) => {
+  const albums = req.params.albumsId;
+  // console.log(albums);
+  spotifyApi.getArtistAlbums(albums)
+    .then(
+    data => {
+      const albumInfo = data.body.items;
+      console.log (albumInfo);
+    },
+    err => {
+      console.error(err);
+    }
+  );
+});
+
+app.listen(3000, () =>
+  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+);
