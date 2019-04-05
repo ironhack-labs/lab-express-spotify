fd89b7521852cd24839ad78375d1ef67bbf39dda
Josue Valencia <josue.valencia@domo.ec>
2019-04-05 18:56:52 +0200

    showing albuyms

--- starter_code/controller/album.controller.js
+++ starter_code/controller/album.controller.js
@@ -2,12 +2,13 @@ const spotifyApi = require('../api')
 
 module.exports.details = (req, res, next) => {
   const id = req.params.id;
-  // spotifyApi.searchArtists(criteria)
-  //   .then(data => {
-  //     res.render('artist', {artists: data.body.artists.items})
-  //   })
-  //   .catch(err => {
-  //     console.log("The error while searching artists occurred: ", err);
-  //   })
-  res.render('album', {id})
+  spotifyApi.getArtistAlbums(id)
+    .then(data => {
+      console.log(data.body.items)
+      res.render('album', {albums : data.body.items})
+    })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    })
+
 }
