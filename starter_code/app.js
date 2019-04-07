9b6ea0f92d67b1f7413016a3e08f9b3d9204b360
NuriaTired <nuriatired@gmail.com>
2019-04-07 09:44:05 +0200

    Comprendido, pero tengo Dudas

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const hbs = require('hbs');
-
 // require spotify-web-api-node package here:
 const SpotifyWebApi = require("spotify-web-api-node");
 
 
+const app = express();
+
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
+
 
 // setting the spotify-api goes here:
-const clientId = "79c6763d65e44ce8a7805afe8d104179",
-    clientSecret = "1fffe8486cdc46e5895366697cabbd3a";
+const clientId = "79c6763d65e44ce8a7805afe8d104179";
+const clientSecret = "1fffe8486cdc46e5895366697cabbd3a";
 
 const spotifyApi = new SpotifyWebApi({
     clientId: clientId,
     clientSecret: clientSecret
 });
-
 
 
 // Retrieve an access token
@@ -25,42 +29,37 @@ spotifyApi.clientCredentialsGrant()
     .catch(error => {
         console.log('Something went wrong when retrieving an access token', error);
     })
-
-
-
-const app = express();
-
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
-
-
-// setting the spotify-api goes here:
-const clientId = "79c6763d65e44ce8a7805afe8d104179",
-    clientSecret = "1fffe8486cdc46e5895366697cabbd3a";
-
-const spotifyApi = new SpotifyWebApi({
-    clientId: clientId,
-    clientSecret: clientSecret
-});
-
-
-
 
 
 
 // the routes go here:
+app.get('/', (req, res, next) => { res.render('index', { title: 'Home' }); });
 
-// Retrieve an access token
-spotifyApi.clientCredentialsGrant()
-    .then(data => {
-        spotifyApi.setAccessToken(data.body['access_token']);
-    })
-    .catch(error => {
-        console.log('Something went wrong when retrieving an access token', error);
-    })
+app.get('/artists', (req, res, next) => {
+    spotifyApi.searchArtists(JSON.stringify(req.query.artist))
+        .then(data => {
+            //console.log("The received data from the API: ", data.body;
+            res.render('artists', { title: 'Search', data })
+        })
+        .catch(err => { console.log("The error while searching artists occurred: ", err); })
+})
 
+app.get('/albums/:id', (req, res, next) => {
+    spotifyApi.getArtistAlbums(req.params.id)
+        .then(data => {
+            //console.log("The received data from the API: ", data.body);
+            res.render('albums', { title: 'Albums', data });
+        })
+        .catch(err => { console.log("The error while searching albums occurred: ", err); })
+})
 
-
+app.get('/tracks/:id', (req, res, next) => {
+    spotifyApi.getAlbumTracks(req.params.id)
+        .then(data => {
+            console.log("The received data from the API: ", data.body.items[0]);
+            res.render('tracks', { title: 'Tracks', data });
+        })
+        .catch(err => { console.log("The error while searching tracks occurred: ", err); })
+})
 
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
