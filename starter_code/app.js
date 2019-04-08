bcdb494e5aa691d6ba4a0c5c62cd2368b024bdd5
carlos <carlos.gsm3@gmail.com>
2019-04-08 14:21:38 +0200

    final

--- starter_code/app.js
+++ starter_code/app.js
@@ -34,20 +34,58 @@ app.get('/', (req, res, next) => {
     res.render('index');
   });
 
-  app.get('/artists', (request, response) => {
-    spotifyApi.searchArtists(request.query.artistSearch)
+app.get('/artists', (req, res,next) => {
+  const {artist} = req.query;// por que como array
+
+  spotifyApi.searchArtists(artist)
       .then(data => {
-        // console.log("The received data from the API: ", data.body.artists.items);
-        const artistsArray = data.body.artists.items;
-        response.render('artists',
-          { artistsArray }
-        );
+        const items = data.body.artists.items;
+        res.render('artists',{items});// sin el items no pinta nada, el segundo parametro es lo que estara disponible desde la vista
+        console.log("The received data from the API: ", data.body);
       })
       .catch(err => {
         console.log("The error while searching artists occurred: ", err);
       })
+  
+  
   });
   
+
+
+
+
+  app.get('/artists/:artistId', (req, res) => {
+  const artistId = req.params.artistId;
+  
+  spotifyApi.getArtistAlbums(artistId).then(//este metodo solo acpta el id.. no el name? cierto?
+    function(data) {
+      const albums = data.body.items;
+      res.render('albums', {albums});
+    },
+    function(err) {
+      console.error(err);
+    }
+    );
+
+  });
+
+  app.get('/albums/:albumId', (req, res) => {
+    const albumId = req.params.albumId;
+    
+    spotifyApi.getAlbumTracks(albumId).then(//este metodo solo acpta el id.. no el name? cierto?
+      function(data) {
+        const songs = data.body.items;
+        res.render('songs', {songs});
+      },
+        function(err) {
+          console.error(err);
+        }
+        );
+    
+      });
+    
+  
+    
 
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
   
