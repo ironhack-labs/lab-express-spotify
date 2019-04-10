a3ac726cab454ed48873d1f478f0d6dbe9db1ce1
Victor Garcia <v.garcia.mestre@gmail.com>
2019-04-10 17:55:16 +0200

    search artist process half done

--- starter_code/config/spoti.config.js
+++ starter_code/config/spoti.config.js
@@ -1,3 +1,8 @@
+const SpotifyWebApi = require('spotify-web-api-node')
+
+// setting the spotify-api goes here:
+
+
 // Remember to insert your credentials here
 const clientId = '25ab3895b3b64d5ab12a89dc682ca9da',
     clientSecret = '2fa0744635ce4ab58eac0fd66da1f403';
@@ -11,7 +16,10 @@ const spotifyApi = new SpotifyWebApi({
 spotifyApi.clientCredentialsGrant()
   .then( data => {
     spotifyApi.setAccessToken(data.body['access_token']);
+    console.log('conectado')
   })
   .catch(error => {
     console.log('Something went wrong when retrieving an access token', error);
   })
+
+  module.exports = spotifyApi
--- starter_code/routes/artist.js
+++ starter_code/routes/artist.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const spotifyApi= require('../config/spoti.config')
+const app = express();
+ 
 
 router.get('/', (req, res, next) => {
-    res.render('artist');
+
+  spotifyApi.searchArtists(req.query.artist)
+  .then(function(data) {
+    console.log('Search artists by param:');
+  }, function(err) {
+    console.error(err);
   });
+});
+
+
+
 
 module.exports = router;
-
