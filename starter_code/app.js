e574be59cbc4e255d38771aee745e3e444cc7d7d
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-04-10 12:51:44 +0200

    showing name of input artist

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,7 +5,6 @@ const hbs = require('./node_modules/hbs/lib/hbs');
 const SpotifyWebApi = require('./node_modules/spotify-web-api-node/src/server');
 
 
-
 const app = express();
 
 app.set('view engine', 'hbs');
@@ -16,16 +15,16 @@ app.use(express.static(__dirname + '/public'));
 // setting the spotify-api goes here:
 // Remember to insert your credentials here
 const clientId = '9d95380e468b4001a15050c906d3d90d',
-    clientSecret = '566268d5a8ff433ea54f27df7f4fb44e';
+  clientSecret = '566268d5a8ff433ea54f27df7f4fb44e';
 
 const spotifyApi = new SpotifyWebApi({
-  clientId : clientId,
-  clientSecret : clientSecret
+  clientId: clientId,
+  clientSecret: clientSecret
 });
 
 // Retrieve an access token
 spotifyApi.clientCredentialsGrant()
-  .then( data => {
+  .then(data => {
     spotifyApi.setAccessToken(data.body['access_token']);
   })
   .catch(error => {
@@ -33,18 +32,28 @@ spotifyApi.clientCredentialsGrant()
   })
 
 
-
-
-
 // the routes go here:
-
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
   res.render("home.hbs");
 });
 
-app.get("/artists", (req, res)=>{
-  res.render("artists.hbs");
+app.get("/artists", (req, res) => {
+  // return console.log("here", req.query)
+  spotifyApi.searchArtists(req.query.artistname)
+    .then(data => {
+      // res.json(data)
+      // console.log("The received data from the API: ", data.body);
+      res.render("artists.hbs", {
+        data
+      });
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    })
 });
+
+
 
 
 
