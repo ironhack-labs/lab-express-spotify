8eaef2c3650ab301b869e73b2ce95c2347c302b6
AnthonySBR <asbr1811@gmail.com>
2019-04-11 21:44:43 +0200

    Everything done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const hbs = require('hbs');
+require('dotenv').config();
+var bodyParser = require('body-parser')
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 
 
@@ -9,17 +12,85 @@ const app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.use(express.static(__dirname + '/public'));
 
 
-// setting the spotify-api goes here:
+
+// Remember to insert your credentials here
+
+const clientId = process.env.IdCliente;
+   const  clientSecret = process.env.SecretId;
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: clientId,
+    clientSecret: clientSecret
+});
+
+//****************************/
 
 
 
 
 
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+    .then(data => {
+        spotifyApi.setAccessToken(data.body['access_token']);
+    })
+    .catch(error => {
+        console.log('Something went wrong when retrieving an access token', error);
+    })
 
-// the routes go here:
+// ***************** Index *****************/
+
+
+app.get("/", (req, res) =>{
+      res.render("index"); });
+
+
+//************** Artists ******************/
+
+
+app.post("/artists", (req, res) =>{
+var data = req.body.search
+spotifyApi.searchArtists(data)
+
+.then(respuesta => {
+   console.log(respuesta.body.artists.items[0].images);
+    res.render("artists", {artists: respuesta.body.artists.items})
+})     .catch(err => {
+    console.log("The error while searching artists occurred: ", err);
+     })
+               });
+
+//************ Songs **************/
+app.get("/songs/:idAlbum", (req, res) => {
+    spotifyApi
+        .getAlbumTracks(req.params.idAlbum)
+        .then(data => {
+            res.render("songs", { songs: data.body.items });
+        })
+        .catch(err => {
+            console.log('Song not found Error:', err);
+        })
+});
+
+
+//************ ALBUMS **************/
+app.get("/albums/:idArtist", (req, res) => {
+    spotifyApi
+    .getArtistAlbums(req.params.idArtist)
+    .then(data=>{
+        // res.json(data)
+        res.render("albums",{ albums:data.body.items })
+    })
+    .catch(err=>{
+        console.log('Artist not found Error', err);
+    
+    })
+});
 
 
 
