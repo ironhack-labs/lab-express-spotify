ae7abb1675043639ffa875203a34e2880a9759bf
Aledia Ortega <alediaortega@gmail.com>
2019-04-11 15:29:15 +0200

    iteracion1

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
-
+const SpotifyWebApi = require('spotify-web-api-node');
 
 
 const app = express();
@@ -15,7 +15,22 @@ app.use(express.static(__dirname + '/public'));
 // setting the spotify-api goes here:
 
 
+const clientId = '928f759b24c244b08283035e708de5c9',
+    clientSecret = '0aea948de7504a53b4628a17f93c3949';
 
+const spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then( data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  })
 
 
 
