008438244540e75e1fb1af003a7a5c01d1c0c229
pipepico <fpr29.10@gmail.com>
2019-04-20 23:02:58 -0500

    init

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,26 +1,57 @@
 const express = require('express');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
-
-
+const SpotifyWebApi = require('spotify-web-api-node');
+const bodyParser = require('body-parser')
 
 const app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
-
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // setting the spotify-api goes here:
+const clientId = 'c8cfaabda26d41c19a78c8ca6f80101b',
+    clientSecret = '18c19c07781c471481fd40b15e109be1';
 
+const spotifyApi = new SpotifyWebApi({
+  clientId,
+  clientSecret
+});
 
-
-
-
-
-// the routes go here:
-
-
-
-app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  })
+  
+  
+  
+  
+  // the routes go here:
+app.get('/', (req, res) => {
+  res.render('index')
+})
+app.post('/artists', (req, res, next) => {
+  const { artist } = req.body
+  spotifyApi.searchArtists(artist)
+  .then(data => {
+    const { items } = data.body.artists
+    console.log('name'+{name}); 
+    // res.send({name})
+    res.send(data.body.artists.items)
+    console.log("The received data from the API: ", data.body);
+   })
+  .catch(err => {
+    console.log("The error while searching artists occurred: ", err);
+  })
+})
+  
+  
+  
+  
+  app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
+  
