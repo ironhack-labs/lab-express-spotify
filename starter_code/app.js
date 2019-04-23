e7cbc54df03e46ec995edd356047924f81f23ce7
Chris Cardelle <chris.cardelle@gmail.com>
2019-04-23 15:44:05 -0400

    Initial commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const hbs = require('hbs');
-
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 
+
+
+// Remember to insert your credentials here
+const clientId = '12445debe99b40fb995feea938fcd4c8',
+    clientSecret = '660f85e6e35f496da2bff90d80c574ed';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then( data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  })
+
+  // Express Constants
 
 const app = express();
 
@@ -20,7 +41,9 @@ app.use(express.static(__dirname + '/public'));
 
 
 // the routes go here:
-
+app.get('/', (req, res, next) => {
+  res.render('layout');
+});
 
 
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
