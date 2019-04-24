a77e945eee446539194b8e3b8bc94fba3a059f3c
Samuel Yamamoto <yamamoto.samuel@gmail.com>
2019-04-24 16:59:36 -0400

    Done

--- express-spotify/app.js
+++ express-spotify/app.js
@@ -1,45 +1,75 @@
 const express = require('express');
 const hbs = require('hbs');
-const SpotifyWebApi = require('spotify-web-api-node');
 const app = express();
-//const spotApi = spotify();
-
-// require spotify-web-api-node package here: (DONE)
+require('dotenv').config()
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-// Remember to insert your credentials here(DONE)
-const clientId = 'e444fad7ca40424989ec62fab7af2ca6',
-    clientSecret = 'edc9d1b76faf494f86aeeb52ea3ef144';
+console.log(process.env.clientId)
+
+const SpotifyWebApi = require('spotify-web-api-node');
+
+// Remember to insert your credentials here
+
 
 const spotifyApi = new SpotifyWebApi({
-  clientId : clientId,
-  clientSecret : clientSecret
+    clientId: 'e444fad7ca40424989ec62fab7af2ca6',
+    clientSecret: 'f3dc836a6fc34fae8a713d61a19c840b'
 });
 
 // Retrieve an access token
-
 spotifyApi.clientCredentialsGrant()
-  .then( data => {
-    spotifyApi.setAccessToken(data.body['access_token']);
-  })
-  .catch(error => {
-    console.log('Something went wrong when retrieving an access token', error);
-  })
+    .then(data => {
+        console.log('success')
+        spotifyApi.setAccessToken(data.body['access_token']);
+    })
+    .catch(error => {
+        console.log('Something went wrong when retrieving an access token', error);
+    })
+
+
+app.get('/', (elephants, bunnies, _)=>{
+    bunnies.render('index')
+})
 
 
 
-// setting the spotify-api goes here: ---------------------------
 
 
-// credentials are optional
-// var spotifyApi = new SpotifyWebApi({
-//   clientId: 'fcecfc72172e4cd267473117a17cbd4d',
-//   clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
-//   redirectUri: 'http://www.example.com/callback'
-// });
+//http://localhost:3000/searchArtist?nameOfArtist=madonna
+app.get('/searchArtist', (req,res,next)=>{
+    spotifyApi.searchArtists(req.query.nameOfArtist)
+    .then(data=> {
+        console.log('dta from spotify', data.body.artists.items)
+        res.render('artists', {items: data.body.artists.items} )
+        //res.json(data.body.artists.items) //makes looking at data easier sometimes
+    }).catch(err=>console.error(err))
+})
+
+
+
+
+
+//http://localhost:3000/albums/2QsynagSdAqZj3U9HgDzjD
+app.get('/albums/:identification', (req,res,next)=>{
+    spotifyApi.getArtistAlbums(req.params.identification).then(data=>{
+        let items = data.body.items
+        res.render('albums', { items })
+        //res.json(data)
+    })
+
+})
+
+//http://localhost:3000/tracks/2qmmPaaWXRM1kZujpUKKsO
+app.get('/tracks/:theAlbumsIdPlease', (req, res, next)=> {
+    spotifyApi.getAlbumTracks(req.params.theAlbumsIdPlease).then(data=>{
+        //res.json(data.body.items)
+        res.render('tracks', { items:data.body.items })
+    })
+})
+
 
 
 
