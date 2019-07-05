3172f6f74b65782b3b16fc8dbaac3fa8b7ccd266
Andre Cabrera <cabrerandre@gmail.com>
2019-07-05 15:41:27 -0400

    first commit, able to search for artist and display info

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const hbs = require('hbs');
+const SpotifyWebApi = require('spotify-web-api-node');
 
-// require spotify-web-api-node package here:
-
+const clientId = '94a50576dd9a4808b744f40c41d1536d',
+  clientSecret = '04b8780d38f6431a89bd57a874f786e4';
 
 
 const app = express();
@@ -14,12 +15,52 @@ app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
 
+const spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
 
-
+spotifyApi.clientCredentialsGrant()
+  .then( data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  })
 
 
 
 // the routes go here:
+
+
+  app.get('/', (req, res, next) => {
+    console.log("helloo")
+    res.render('index');
+  });
+
+
+  app.get('/artists', (req, res, next)=>{
+
+    spotifyApi.searchArtists(req.query.artists)
+    .then(data => {
+
+      console.log("The received data from the API: ", data.body.artists.items);
+      res.render('artists', {data})
+  
+    })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    })
+
+     
+  });
+
+
+
+
+
+
+
 
 
 
