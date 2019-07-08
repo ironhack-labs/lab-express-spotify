31e8cb97ff7167f991294381cce4b17cf575632a
Sergio Velikopoljski <sergiovelo17@gmail.com>
2019-07-08 09:29:56 -0400

    added new tracks button and a search for playlists

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,7 +11,9 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 hbs.registerPartials(__dirname + '/views/partials');
-
+let allArtists = ["Eminem","Drake","Kid Cudi","Lil uzi vert","Lady Gaga", "Miley Cyrus", "Kanye West", 
+"Aerosmith","Grateful Dead", "Rolling Stones", "Elvis","Tupac","Rihanna","Snoop Dogg","Michael Jackson","Cool and the Gang","Jay Z","Rocksteady Crew","2 Live Crew","Jurrasic 5",
+"Del the funky homosapien","Gorillaz","Red hot chili peppers","Smashing pumpkins","U2","DJ Laz"]
 // setting the spotify-api goes here:
 
 // Remember to insert your credentials here
@@ -50,10 +52,34 @@ app.get("/artists",(req,res,next)=>{
       console.log("The error while searching artists occurred: ", err);
     })
 })
+app.get("/random-artist",(req,res,next)=>{
+  let random = Math.floor(Math.random() * allArtists.length);
+  let randomArtist = allArtists[random];
+  spotifyApi.searchArtists(randomArtist)
+    .then(data => {
+      res.render("artists",data.body.artists)
+      console.log("The received data from the API: ", data.body);
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    })
+})
 app.get("/albums/:artistId",(req,res,next)=>{
   spotifyApi.getArtistAlbums(req.params.artistId)
     .then(data => {
       res.render("albums",data.body)
+      console.log("The received data from the API: ", data.body);
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    })
+})
+app.get("/playlist-tracks/:playlistId",(req,res,next)=>{
+  spotifyApi.getPlaylistTracks(req.params.playlistId)
+    .then(data => {
+      res.render("playlist-tracks",data.body)
       console.log("The received data from the API: ", data.body);
       // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
     })
@@ -72,5 +98,26 @@ app.get("/tracks/:albumId",(req,res,next)=>{
       console.log("The error while searching artists occurred: ", err);
     })
 })
+app.get("/new-releases",(req,res,next)=>{
+  spotifyApi.getNewReleases()
+  .then(data=>{
+    res.render("new-releases",data.body.albums);
+    console.log("The received data from the API: ", data.body);
 
+  })
+  .catch(err=>{
+    console.log(err);
+  })
+})
+app.get('/playlists',(req,res,next)=>{
+  console.log(req.query.playlist);
+  spotifyApi.searchPlaylists(req.query.playlist)
+  .then((data)=>{
+    res.render('playlist',data.body.playlists)
+    console.log("The received data from the API: ", data.body.playlists)
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+})
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
