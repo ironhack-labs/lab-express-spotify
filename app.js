02d2af24d4ed01c630c4cdfb798ac5975ff667d4
victorpaolo <vpreategui@gmail.com>
2019-07-22 01:06:32 +0200

    finished - but not working

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const hbs = require('hbs');
+const SpotifyWebApi = require('spotify-web-api-node');
+const Spotify = require('spotify-web-api-js');
+
 
 // require spotify-web-api-node package here:
 
+const s = new Spotify();
 const indexRouter = require('./routes/index');
 
  const app = express();
@@ -38,5 +42,69 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
+
+
+app.get('/artists', (req, res, next) => {
+  const {artist} = req.query;
+
+  spotifyApi.searchArtists(artist)
+      .then(data => {
+        const items = data.body.artists.items;
+
+        res.render('artists',{items});
+      })
+      .catch(err => {
+        console.log("The error while searching artists occurred: ", err);
+      })
+})
+
+
+app.get('/artists/:artistId', (req, res) => {
+  const artistId = req.params.artistId;
+
+  spotifyApi.getArtistAlbums(artistId).then(
+    function(data) {
+      const albums = data.body.items;
+      res.render('albums', {albums});
+    },
+    function(err) {
+      console.error(err);
+    }
+    );
+
+  });
+app.get('/albums/:albumId', (req, res) => {
+const trackId = req.params.albumId;
+
+  spotifyApi.getAlbumTracks(trackId, { limit : 5, offset : 1 })
+  .then(function(data) {
+    const tracks = data.body.items;
+
+    res.render('tracks',{tracks} )
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+})
+
+// Remember to insert your credentials here
+const clientId = '5c79a8bef56c46b8bbc401d78dd2156b',
+    clientSecret = '3825e830cb6f40f281bdbade7b96a960';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then( data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  })
 
  module.exports = app;
