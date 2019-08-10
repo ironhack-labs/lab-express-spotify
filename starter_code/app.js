5e2ec670810fc0dcb478a319c3bd7dc6dcdf7890
= <jacex17@gmail.com>
2019-08-09 22:37:00 -0400

    Add style to artists page

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const hbs = require('hbs');
 
@@ -35,13 +36,13 @@ app.get('/', (req, res, next) =>{
 })
 
 app.get('/artists', (req, res, next) => {
-    console.log("The Artist is: ", req.query)
+  //  console.log("The Artist is: ", req.query)
     spotifyApi.searchArtists(req.query.artistName)
     .then(data => {
            // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
       res.render('artists', { theArtist: data.body.artists.items })
    //  console.log("The received data from the API: ", data.body.artists.items);
-     console.log("The Artist ID is ", data.body.artists.items[0].id);
+   //  console.log("The Artist ID is ", data.body.artists.items[0].id);
       //console.log("Name of artists: ", data.body.artists.items[0].name); //GET THE ARTIST NAME
     //  console.log("Image url of artists: ", data.body.artists.items[0].images[0].url); //GET THE ARTIST IMAGE URL
     })
@@ -55,8 +56,8 @@ app.get('/albums/:artistId', (req, res, next) => {
   spotifyApi.getArtistAlbums(req.params.artistId)
   .then(data => {
     res.render('albums', { theAlbum: data.body.items} )
-   console.log("The info received was: ", data.body.items[0]);
-   console.log("The Album ID is: ", data.body.items[0].id);
+  // console.log("The info received was: ", data.body.items[0]);
+  // console.log("The Album ID is: ", data.body.items[0].id);
   })
   .catch(err => console.log("The error while searching albums occurred: ", err))
 })
@@ -65,7 +66,7 @@ app.get('/tracks/:albumId', (req, res, next) => {
   spotifyApi.getAlbumTracks(req.params.albumId)
   .then(data => {
     res.render('tracks', { theTracks: data.body.items} )
-   console.log("The info received was: ", data.body.items[0]);
+ //  console.log("The info received was: ", data.body.items[0]);
  //  console.log("The Album ID is: ", data.body.items[0].id);
   })
   .catch(err => console.log("The error while searching albums occurred: ", err))
