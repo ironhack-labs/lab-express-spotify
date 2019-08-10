99e3fce771dbcb6b1db5630bb187428cd8e4d28c
Anabel Perez <anabelpdv@gmail.com>
2019-08-10 10:45:58 -0400

    Initial commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 
 
@@ -13,13 +14,65 @@ app.use(express.static(__dirname + '/public'));
 
 
 // setting the spotify-api goes here:
+const clientId = 'bf4653f2845a48439f183bde80d3751b',
+    clientSecret = '9e2d88cf381a42168ba80b4d72868286';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then( data => {
+    console
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  })
 
 
+app.get('/',(req,res) => {
+  res.render('home');
+});
 
+app.get('/artists',(req,res) => {
+  let artists = req.query.artist;
+    spotifyApi.searchArtists(artists)
+      .then(data => {
+          let items = data.body.artists.items;
+          res.render('artists',{items});
+      })
+      .catch(err => {
+          console.log("The error while searching artists occurred: ", err);
+  });
+});
 
+app.get('/albums/:artistId',(req,res) => {
+  let artist = req.params.artistId;
+  spotifyApi.getArtistAlbums(artist)
+    .then(data => {
+      //res.send(data.body.items)
+      let items = data.body.items;
+      res.render('albums',{items});
+      console.log('Artist albums', data.body);
+    })
+    .catch(err => { console.error("The error while searching artists occurred: ",err);
+  });
+});
 
+app.get('/tracks/:trackId',(req,res) => {
+  let album = req.params.trackId; 
 
-// the routes go here:
+  spotifyApi.getAlbumTracks(album, { limit : 5, offset : 1 })
+  .then(function(data) {
+   // res.send(data)
+    console.log(data.body);
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+});
 
 
 
