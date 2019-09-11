4e74e41e41a9da175797c889c2582e5a5e2f171d
Lauren Laskerr <llask@me.com>
2019-09-11 14:25:33 -0400

    it's the most beautiful thing i've ever done.... ;)

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 const express = require('express');
 const hbs = require('hbs');
-
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require("spotify-web-api-node");
 
 
 
@@ -15,13 +15,63 @@ app.use(express.static(__dirname + '/public'));
 
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
 
-
-
-
-
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  })
+  .catch(error => {
+    console.log("Something went wrong when retrieving an access token", error);
+  });
 
 // the routes go here:
+
+
+app.get("/", (req, res, next) => {
+  res.render("home");
+});
+
+
+app.get("/artists", (req, res, next) => {
+  spotifyApi.searchArtists(req.query.artist).then(
+    artist => {
+      console.log(artist)
+      res.render("artists", {artists:artist.body.artists.items})
+    }
+  ).catch(error => {
+    console.log(error)
+  })
+});
+
+
+app.get("/albums/:artistId", (req, res, next) => {
+  spotifyApi.getArtistAlbums(req.params.artistId).then(
+    albums => {
+      console.log(albums)
+    res.render("albums", {albums: albums.body.items})
+  }).catch(error => {
+    console.log(error)
+  })
+})
+
+
+app.get("/songs/:songId", (req, res, next) => {
+  spotifyApi.getAlbumTracks(req.params.songId).then(
+    songs => {
+      console.log(songs)
+    res.render("songs", {songs: songs.body.items})
+  }).catch(error => {
+    console.log(error)
+  })
+})
+
+
 
 
 
