303b0ce4d2623c5eeed7af1fd57bfcf567b76b69
Christian Roque <croqu008@fiu.edu>
2019-09-11 17:24:19 -0400

    missing tracks

--- starter_code/app.js
+++ starter_code/app.js
@@ -16,7 +16,7 @@ spotifyApi
 		spotifyApi.setAccessToken(data.body['access_token']);
 	})
 	.catch((error) => {
-		console.log('Something went wrong when retrieving an access token', error);
+		// console.log('Something went wrong when retrieving an access token', error);
 	});
 
 const app = express();
@@ -29,11 +29,11 @@ app.use(express.static(__dirname + '/public'));
 spotifyApi
 	.searchArtists(/*'HERE GOES THE QUERY ARTIST'*/)
 	.then((data) => {
-		console.log('The received data from the API: ', data.body);
+		// console.log('The received data from the API: ', data.body);
 		// ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
 	})
 	.catch((err) => {
-		console.log('The error while searching artists occurred: ', err);
+		// console.log('The error while searching artists occurred: ', err);
 	});
 
 // the routes go here:
@@ -47,13 +47,32 @@ app.get('/artists', (req, res, next) => {
 	spotifyApi
 		.searchArtists(req.query.artistName)
 		.then((data) => {
-			console.log('The received data from the API: ', data.body.artists.items);
-			let results = data.body;
-			res.render('index', { results });
+			// console.log('The received data from the API: ', data.body.artists.items);
+			let results = data.body.artists.items;
+			res.render('artists', { results });
 		})
 		.catch((err) => {
-			console.log('The error while searching artists occurred: ', err);
+			// console.log('The error while searching artists occurred: ', err);
 		});
+});
+
+app.get('/albums/:artistId', (req, res, next) => {
+	spotifyApi.getArtistAlbums(req.params.artistId).then((data) => {
+		// console.log('Artist albums', data);
+		let results = data;
+		// console.log(results.body.items, 'helloo');
+		res.render('albums', { results });
+	}, function(err) {
+		// console.error(err);
+	});
+});
+
+app.get('/tracks/:trackId', (req, res, next) => {
+	spotifyApi.getAlbum(req.params.trackId).then((data) => {
+		let results = data.body.tracks.items;
+		console.log(results);
+		res.render('tracks', { results });
+	});
 });
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
