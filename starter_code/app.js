b7f274e3043bd614968fa340444140cf86ebc2ee
Bobby Wang <bobby.p.wang@gmail.com>
2019-09-11 11:11:53 -0400

    Completed connecting to the Spotify API and setting up dependencies.

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,7 +1,23 @@
 require('dotenv').config()
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const express = require('express');
 const hbs = require('hbs');
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => {
+      spotifyApi.setAccessToken(data.body["access_token"]);
+    })
+    .catch(error => {
+      console.log("Something went wrong when retrieving an access token", error);
+    });
 
 // require spotify-web-api-node package here:
 
