6cdd15ed53edbae13a6edf07764420c5bfb59e73
Qiwei Lin <Qiwei.l.ql@gmail.com>
2019-09-11 18:06:45 -0400

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
@@ -32,12 +32,52 @@ spotifyApi
   });
 
 
-
-
 // the routes go here:
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/artists', (req, res, next) => {
+  let artist = req.query.artist;
+  // console.log(artist);
+  
+  spotifyApi
+    .searchArtists(artist)
+    .then(data => {
+      console.log(data.body.artists.items);
+      res.render('artists', {artists: data.body.artists.items});
+    })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    });
+});
+
+app.get('/albums/:id', (req, res, next) => {
+  artistId = req.params.id;
+
+  spotifyApi.getArtistAlbums(artistId)
+    .then(function (data) {
+      console.log(data.body.items);
+      
+      res.render('albums', { albums: data.body.items });
+    }, function (err) {
+      console.error(err);
+    });
+});
+
+app.get('/tracks/:id', (req, res, next) => {
+  albumId = req.params.id;
+
+  spotifyApi.getAlbumTracks(albumId)
+    .then(function (data) {
+      console.log(data.body.items);
+      // res.send(data.body.items);
+      res.render('tracks', { tracks: data.body.items });
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
+});
 
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
