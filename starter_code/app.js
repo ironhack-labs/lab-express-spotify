b2fba56c9a93ce54b0ff724c464a0c2988e08c1b
Brais Vidal <brais.vidal.rodriguez@gmail.com>
2019-09-12 18:52:22 +0200

    iteration 4 done

--- starter_code/app.js
+++ starter_code/app.js
@@ -52,9 +52,23 @@ app.get('/artists', (req, res, next) => {
   spotifyApi
   .searchArtists(req.query.artist)
   .then(data => {
-    console.log("The received data from the API: ", data.body.artists.items);
+    // console.log("The received data from the API: ", data.body.artists);
     let result = data.body.artists.items;
     res.render('artists', {result});
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+  })
+  .catch(err => {
+    console.log("The error while searching artists occurred: ", err);
+  });
+});
+
+app.get("/albums/:artistId", (req, res, next) => {
+  spotifyApi
+  .getArtistAlbums(req.params.artistId)
+  .then(data => {
+    console.log("The received data from the API: ", data.body.items);
+    let result = data.body.items;
+    res.render('albums', {result});
     // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
   })
   .catch(err => {
