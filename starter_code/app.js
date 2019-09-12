f3990b4261d3abeaa468e8c2eb664b365f77b8aa
Antonio Llamas <antoniollamasln@gmail.com>
2019-09-12 21:30:12 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -12,13 +12,12 @@ app.use(express.static(__dirname + '/public'));
 
 hbs.registerPartials(__dirname + "/views/partials");
 
-// console.log(process.env);
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET
 });
 
-// Retrieve an access token
+
 spotifyApi
   .clientCredentialsGrant()
   .then(data => {
@@ -45,17 +44,13 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 app.get('/artists', (req, res, next) => {
-  // console.log(req.query.query);
 
   spotifyApi
     .searchArtists(req.query.query)
     .then(data => {
 
       const artists = data.body.artists.items
-      // console.log(albums);
       res.render('artists', { artists });
-      // console.log("The received data from the API: ", data.body.artists.items);
-      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
     })
     .catch(err => {
       console.log("The error while searching artists occurred: ", err);
@@ -79,21 +74,11 @@ app.get('/albums', (req, res, next) => {
 app.get('/tracks', (req, res, next) => {
   console.log(req.params.id);
   spotifyApi.getAlbumTracks(req.query.id).then(function (data) {
-      console.log(data.body);
       const tracks = data.body.items;
       res.render('tracks', { tracks });
     }, function (err) {
       console.log(`Something went wrong!`, err);
     })
   })
-
-
-
-
-
-
-// the routes go here:
-
-
 
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
