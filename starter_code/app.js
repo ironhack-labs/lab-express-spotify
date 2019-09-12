d65972bd69256cb2f0b2fecc86f7f8b3afa6f37c
Brais Vidal <brais.vidal.rodriguez@gmail.com>
2019-09-12 18:23:23 +0200

    iteration 3 done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 
 
+const bodyParser = require('body-parser');
 const express = require('express');
 const hbs = require('hbs');
+const path = require('path');
 
 // require spotify-web-api-node package here:
 const SpotifyWebApi = require("spotify-web-api-node");
@@ -13,6 +15,12 @@ const app = express();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(`${__dirname}/views/partials`)
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 
 // setting the spotify-api goes here:
@@ -36,6 +44,23 @@ spotifyApi
 
 
 // the routes go here:
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/artists', (req, res, next) => {
+  spotifyApi
+  .searchArtists(req.query.artist)
+  .then(data => {
+    console.log("The received data from the API: ", data.body.artists.items);
+    let result = data.body.artists.items;
+    res.render('artists', {result});
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+  })
+  .catch(err => {
+    console.log("The error while searching artists occurred: ", err);
+  });
+});
 
 
 
