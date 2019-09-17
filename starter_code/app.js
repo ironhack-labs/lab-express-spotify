f308aa59b89c7ee8956a20bce934402db6f99e50
josueacev2 <josueacev2@gmail.com>
2019-09-17 01:50:05 -0400

    commit1

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 
 const express = require('express');
 const hbs = require('hbs');
+const SpotifyWebApi = require("spotify-web-api-node");
+const path    = require('path');
 
 // require spotify-web-api-node package here:
 
@@ -11,18 +13,51 @@ const app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public')); when this one worked??? huhhh
+console.log(__dirname);
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
 
-
-
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  })
+  .catch(error => {
+    console.log("Something went wrong when retrieving an access token", error);
+  });
 
 
 
 // the routes go here:
+app.get('/', (req, res, next) => {
+  res.render('index');
+})
 
-
+app.get('/artists', (req, res, next) => {
+  
+  console.log('get artists:'+req.query.search);
+  
+  spotifyApi
+  .searchArtists(req.query.search)
+  .then(data => {
+	  
+	  res.render('artists',data);
+    console.log("The received data from the API: ", data.body.artists.items[3].images[0].url	);
+	 console.log("The received data from the API: ", data.body.artists.items[4].name	);
+	
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+  })
+  .catch(err => {
+    console.log("The error while searching artists occurred: ", err);
+  });
+  
+})
 
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
---
+++ starter_code/public/javascript/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $("#submit").click(function(){
+   
+   $("#myform").submit();
+     
+  });
+});
