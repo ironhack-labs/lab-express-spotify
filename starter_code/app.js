88ff6dfc8fc015443d038f7dc07f93a55e6c2a93
elwiiman <emmanuel.sanro@gmail.com>
2019-09-21 19:21:27 -0500

    completada la consulta a artistas

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,28 +1,84 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
+const express = require("express");
+const app = express();
+//body parser
+const bodyParser = require("body-parser");
+
+app.set("view engine", "pug");
+app.set("views", `${__dirname}/views`);
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // require spotify-web-api-node package here:
-
-
-
-const app = express();
-
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
-
+const SpotifyWebApi = require("spotify-web-api-node");
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
 
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  })
+  .catch(error => {
+    console.log("Something went wrong when retrieving an access token", error);
+  });
 
+// the routes go here:s
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
+app.get("/artists", (req, res) => {
+  console.log("que es", req.query);
+  let { search } = req.query;
+  // res.send("This is the artists page!");
+  spotifyApi
+    .searchArtists(search)
+    .then(data => {
+      // console.log("Este es el body", data);
+      console.log("The received data from the API: ", data.body.artists);
+      let { items } = data.body.artists;
+      items.search = search;
+      items.forEach(item => {
+        if (item.images.length !== 0) {
+          item.images = item.images[1];
+        } else {
+          item.images = {
+            url:
+              "https://vinylboutique.co.uk/wp-content/uploads/2017/05/default-release-cd.png"
+          };
+        }
+      });
+      // res.send(items);
+      // // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+      res.render("artists", { items });
+    })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    });
+});
 
+// app.get("/albums/:artistId", (req, res) => {
+//   let { artistId } = req.params;
+//   spotifyApi.getArtistAlbums(artistId).then(
+//     data ={
+//       console.log('Artist albums', data.body);
+//     },
+//     function(err) {
+//       console.error(err);
+//     }
+//   );
 
-// the routes go here:
+//   res.render("albums");
+// });
 
-
-
-app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
+app.listen(3000, () =>
+  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+);
