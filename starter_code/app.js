85550daf5b04c326b355ca2d87980b4f0273718b
Mônica Damasceno <monica.damasceno@gitlab.com>
2019-09-27 09:39:21 -0300

    get input

--- starter_code/app.js
+++ starter_code/app.js
@@ -12,12 +12,39 @@ app.use(express.static(__dirname + '/public'));
 
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
 
-
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  })
+  .catch(error => {
+    console.log("Something went wrong when retrieving an access token", error);
+  });
 
 // the routes go here:
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/artists', (req, res, next) => {
+  let search = req.query;
+  spotifyApi
+    .searchArtists(
+      search.artist
+    )
+    .then(function (data) {
+      console.log("The received data from the API: ", data.body);
+      res.render('artists', data.body);
+    })
+    .catch(function (err) {
+      console.log("The error while searching artists occurred: ", err);
+    });
 });
 
 app.listen(3001, () => console.log("My Spotify project running on port 3001 🎧 🥁 🎸 🔊"));
