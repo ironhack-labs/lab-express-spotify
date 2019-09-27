c8a583d7ed7b619f5ffa88d1be5ba580321cfd2d
LsM Lucas Martins <lucas.smar@gmail.com>
2019-09-27 09:43:11 -0300

    wip: just start

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,28 +1,41 @@
-require('dotenv').config()
-
-const express = require('express');
-const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
-
-
+require("dotenv").config();
+const express = require("express");
+const hbs = require("hbs");
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
-
-
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
 
-
-
-
-
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  })
+  .catch(error => {
+    console.log("Something went wrong when retrieving an access token", error);
+  });
 
 // the routes go here:
+app.get("/", (req, res, next) => res.render("layout.hbs"));
 
-
+app.get("/test", (req, res, next) => {
+  spotifyApi.searchArtists("Love").then(
+    data => {
+      console.log('Search artists by "Love"', data);
+    },
+    err => {
+      console.error(err);
+    }
+  );
+});
 
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
