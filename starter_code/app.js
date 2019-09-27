bb4bf4038bf4e24daee0b6f19d981a96827492d3
Gabriel Escame <gabriel.escame@gmail.com>
2019-09-27 10:01:08 -0300

    in process

--- starter_code/app.js
+++ starter_code/app.js
@@ -3,26 +3,42 @@ require('dotenv').config()
 const express = require('express');
 const hbs = require('hbs');
 
-// require spotify-web-api-node package here:
-
-
-
 const app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
+// require spotify-web-api-node package here:
+const SpotifyWebApi = require("spotify-web-api-node");
+
 
 // setting the spotify-api goes here:
 
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
 
-
-
-
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  })
+  .catch(error => {
+    console.log("Something went wrong when retrieving an access token", error);
+  });
 
 // the routes go here:
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
+app.get('/artists', (req, res) => {
+  const { artists } = req.query;
+  res.render('artists');
+});
 
 
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
