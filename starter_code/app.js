8f83d8db95d28f062d227a67d29f8b3ee6183cdb
Marina Cid Reyes <ymaryna@MacBook-Pro-de-Marina.local>
2019-10-31 22:01:49 +0100

    artistas implementados

--- starter_code/app.js
+++ starter_code/app.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express');
 const hbs = require('hbs');
 
-// require spotify-web-api-node package here:
+const SpotifyWebApi = require("spotify-web-api-node")
 
 
 
@@ -14,14 +14,42 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 
-// setting the spotify-api goes here:
-
-
-
-
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => {
+      spotifyApi.setAccessToken(data.body["access_token"]);
+    })
+    .catch(error => {
+      console.log("Something went wrong when retrieving an access token", error);
+    });
 
 
 // the routes go here:
+
+app.get('/artists',(req, res) => {
+    spotifyApi
+    .searchArtists(req.query.artist_name)
+    .then(data => {
+        //return res.send(data)
+        //console.log("The received data from the API: ", data.body);
+        res.render('artists', data.body.artists)
+    })
+    .catch(err => {
+        console.log("The error while searching artists occurred: ", err);
+    });
+})
+
+
+app.get('/',(req, res) => {
+    res.render('home')
+})
+
 
 
 
