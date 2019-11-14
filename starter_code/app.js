34be3abd8d6fe97ea3a4cde4b6a94c6eda770a11
Manuel Álvarez Rosado <manuelalvarezrosado@MacBook-Air-de-Manuel.local>
2019-11-14 20:24:08 +0100

    Quiero vaya lio me he hecho con tracks y albums

--- starter_code/app.js
+++ starter_code/app.js
@@ -33,24 +33,56 @@ spotifyApi
 
 // the routes go here:
 
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/artists", (req, res, next) => {
-    console.log(req.body)
-    
+app.post("/artists", (req, res) => {    
+    let name = req.body.artist;
   spotifyApi
-    .searchArtists(req.body.searchArtists)
+    .searchArtists(name)
     .then(data => {
-      console.log("The received data from the API: ", data.body);
-      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
-      res.render("artists");
+        let searchArtists = data.body.artists.items;
+      console.log("The received data from the API: ", data.body.artists.items);
+      res.render("artists", {searchArtists});
     })
     .catch(err => {
       console.log("The error while searching artists occurred: ", err);
     });
 });
+
+
+app.get("/albums/:artistId", (req, res, next) => { 
+    let name = req.params.artistId; 
+    spotifyApi.getArtistAlbums(name)
+    .then(data => {
+        let albumsArtist = data.body.items;
+
+      console.log('Artist albums', data.body.items);
+      res.render("albums", {albumsArtist});
+    })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    });
+});
+
+
+app.get("/tracks/:albumId", (req, res, next) => { 
+    let name = req.params.albumId; 
+
+    spotifyApi.getAlbumTracks(name)
+    .then(data => {
+        let songArtist = data.body.items;
+        console.log(songArtist)
+      res.render("tracks", {songArtist});
+    })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    });
+});
+
+
+
 
 app.listen(3000, () =>
   console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
