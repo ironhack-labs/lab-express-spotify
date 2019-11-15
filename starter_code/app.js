b0cb81325eaf0e81b689667c2467b8c41036d460
caprosset <capucinerosset@gmail.com>
2019-11-15 15:18:17 +0100

    Iteration 2

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,8 +2,6 @@ require('dotenv').config()
 
 const express = require('express');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
 const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
@@ -14,13 +12,13 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 
-// setting the spotify-api goes here:
+// Setting the spotify-api
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
 });
 
-// Retrieve an access token
+// Retrieving an access token
 spotifyApi
 .clientCredentialsGrant()
 .then(data => {
