d2b718cb9a841b436255dbbb202121bc1382f59b
Rafael Bosch <rbllado@outlook.com>
2019-11-15 17:57:34 +0100

    update version until iteration 2

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,26 +2,76 @@ require('dotenv').config()
 
 const express = require('express');
 const hbs = require('hbs');
+const bodyParser = require("body-parser")
 
 // require spotify-web-api-node package here:
-
+const SpotifyWebApi = require("spotify-web-api-node");
 
 
 const app = express();
 
 app.set('view engine', 'hbs');
+app.set('partials', __dirname + '/views/partials');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 
+
+
+hbs.registerPartials(__dirname + "/views/partials");
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// parse application application/json
+app.use(bodyParser.json());
+
 // setting the spotify-api goes here:
 
-
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => {
+      spotifyApi.setAccessToken(data.body["access_token"]);
+    })
+    .catch(error => {
+      console.log("Something went wrong when retrieving an access token", error);
+    });
 
 
 
 
 // the routes go here:
+
+app.get("/artists/", (req, res, next) =>{
+    // it is a query because it is from a form with submit
+    // console.log(req.query.artist);
+    // res.send(req.query);
+
+    spotifyApi
+    .searchArtists(req.query.artist)
+    .then(data => {
+      console.log("The received data from the API: ", data.body.artists.items[0].images[0].url);
+      res.render("artists", {arrayArtist: data.body.artists.items});
+    })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    });
+
+
+
+});
+
+
+
+app.get("/", (req, res, next) =>{
+    res.render("index");
+});
 
 
 
