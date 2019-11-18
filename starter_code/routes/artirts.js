94e59c529c4478ee09850685ac6ba42438273d13
barbara-carnieri <barbaracarnieri@yahoo.com>
2019-11-18 10:33:34 +0100

    dotenv deleted

---
+++ starter_code/routes/artirts.js
@@ -0,0 +1,19 @@
+// const express = require('express');
+// const router = express.Router();
+
+
+// router.get('/artists', (req, res, next) => {
+//   spotifyApi
+//   .searchArtists(req.query.searchartist)
+//   .then(data => {
+//     console.log("The received data from the API: ", data.body.artists.items[0]);
+    
+//     res.render('artists', {artists: data.body.artists.items});
+//   })
+//   .catch(err => {
+//     console.log("The error while searching artists occurred: ", err);
+//   });
+// })
+
+
+// module.exports = router;
