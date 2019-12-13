91570b45bb7752506d2ce2d2e78902f36bf15e30
Strapak, Sergej <strapak@mediamarktsaturn.com>
2019-12-13 15:12:27 +0100

    Basic Iteration 3 finished

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,22 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
+const SpotifyWebApi = require('spotify-web-api-node');
 
-// require spotify-web-api-node package here:
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET
 });
 
-// Retrieve an access token
-spotifyApi
-  .clientCredentialsGrant()
-  .then(data => {
-    spotifyApi.setAccessToken(data.body['access_token']);
-  })
-  .catch(error => {
-    console.log('Something went wrong when retrieving an access token', error);
-  });
 
 const app = express();
 
@@ -25,9 +16,36 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-// setting the spotify-api goes here:
 
-// the routes go here:
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+    console.log("successfully retreived Access Token at " + (new Date()));
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  });
+
+  
+  app.get('/artists', function (req, res){
+    spotifyApi.searchArtists(req.query.artist)
+    .then(data => {
+      res.render('artists', data.body);
+    })
+    .catch(err => {
+      console.log('The error while searching artists occurred: ', err);
+    });
+    
+  });
+  
+
+
 
 app.listen(3000, () =>
   console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')
