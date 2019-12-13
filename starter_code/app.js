dae29ee62b50aca47ce5fcf92cfee89d8410ccd7
Juliane Trapp <trappj@media-saturn.com>
2019-12-13 14:42:40 +0100

    Iteration 1,2,3 done

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
+const SpotifyWebApi = require('spotify-web-api-node');
 
 // require spotify-web-api-node package here:
 
@@ -13,7 +14,51 @@ app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
 
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  });
+
 // the routes go here:
+
+app.get("/", (req, res) => {
+  res.render('index');
+});
+
+app.get("/artists", (req, res, next) => {
+  spotifyApi
+  .searchArtists(req.query.artist)
+  .then(data => {
+    console.log('The received data from the API: ', data.body.artists.items[1]);
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    //let artistResult = {
+    //  id: data.body.artists.items[0].id,
+    //  genres: data.body.artists.items[0].genres,
+    //  
+    // 
+    //}
+
+    let artistResult =  data;
+
+    res.render('artists', {artistResult} );
+    
+  })
+  .catch(err => {
+    console.log('The error while searching artists occurred: ', err);
+  });
+});
+
+
 
 app.listen(3000, () =>
   console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')
