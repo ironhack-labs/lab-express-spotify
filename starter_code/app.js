e3464d25317c34194ce221963a34082b3441bece
Strapak, Sergej <strapak@mediamarktsaturn.com>
2019-12-13 10:16:02 +0100

    Iteration 1-2 done

--- starter_code/app.js
+++ starter_code/app.js
@@ -4,6 +4,20 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  });
 
 const app = express();
 
