5b20f2881ffcec5ec705c57cc343a108c5bed10b
pyrrhos-msits <menzelf@mediamarktsaturn.com>
2019-12-13 13:25:58 +0100

    Some Return

--- starter_code/app.js
+++ starter_code/app.js
@@ -19,16 +19,47 @@ const spotifyApi = new SpotifyWebApi({
   clientSecret: process.env.CLIENT_SECRET
 });
 
-// Retrieve an access token
 spotifyApi
-  .clientCredentialsGrant()
-  .then(data => {
-    spotifyApi.setAccessToken(data.body['access_token']);
-  })
-  .catch(error => {
-    console.log('Something went wrong when retrieving an access token', error);
-  });
+.clientCredentialsGrant()
+.then(data => {
+  spotifyApi.setAccessToken(data.body['access_token']);
+  //console.log(data)
+})
+.catch(error => {
+  console.log('Something went wrong when retrieving an access token', error);
+});
+
+
 // the routes go here:
+
+app.get('/',(request , response) => {
+  response.render('index');
+});
+
+app.get('/artists',(request , response) => {
+  console.log(request.query);
+  response.render('artists', request.query);
+});
+
+app.get('/albums', (request,response) => {
+    spotifyApi.searchArtists(request.query.artist)
+    .then((data) => {
+      console.log('The received data from the API: ', data.body);
+      response.send(data.body);
+    })
+    .catch(error => {
+      console.log('Something went wrong when retrieving an access token', error);
+    });
+});
+
+
+
+// Spotify Artist search
+
+
+
+
+
 
 app.listen(3000, () =>
   console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')
