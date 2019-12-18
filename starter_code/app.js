3933ba209dd7c720b990f22c243e3f4777662f46
Edgar Mora <edgar@MacBook-Air-de-Esteban.local>
2019-12-18 13:49:35 -0600

    Connected two screens

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
 
 // require spotify-web-api-node package here:
 var SpotifyWebApi = require('spotify-web-api-node');
@@ -11,6 +12,7 @@ const app = express();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
@@ -37,17 +39,5 @@ app.get('/artists', (req, res, next) => {
 	console.log('hello')
 	res.render('artists');
 })
-
-// app.get("/artists", (req, res) => {
-// 	spotifyApi
-// 		.searchArtists(req.query.artist)
-// 		.then(searchResponse => {
-// 			const { artists } = searchResponse.body;
-// 			res.render("artists", artists);
-// 		})
-// 		.catch(err => {
-// 			console.log("The error while searching artists occurred: ", err);
-// 		});
-// });
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
