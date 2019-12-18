dccc3611e1a38488661fa4ce157ec35cda862e99
Analin Flores <foglzerika@gmail.com>
2019-12-17 23:18:19 -0600

    Creting homepage

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,8 +2,22 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
-
+const SpotifyWebApi = require('spotify-web-api-node');
 // require spotify-web-api-node package here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  });
 
 const app = express();
 
@@ -14,6 +28,9 @@ app.use(express.static(__dirname + '/public'));
 // setting the spotify-api goes here:
 
 // the routes go here:
+app.get('/', (req, res, next) => {
+  res.render('form');
+});
 
 app.listen(3000, () =>
   console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')
