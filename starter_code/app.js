8772c37be7813a368c0ce53e0161073ccb27d29a
Edgar Mora <edgar@MacBook-Air-de-Esteban.local>
2019-12-18 12:54:57 -0600

    First screen design and functionality done

--- starter_code/app.js
+++ starter_code/app.js
@@ -14,9 +14,8 @@ app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
-	clientId     : process.env.CLIENT_ID,
-	clientSecret : process.env.CLIENT_SECRET,
-	redirectUri  : 'http://www.example.com/callback'
+	clientId: process.env.CLIENT_ID,
+	clientSecret: process.env.CLIENT_SECRET
 });
 
 // Retrieve an access token
@@ -31,7 +30,24 @@ spotifyApi
 
 // the routes go here:
 app.get('/', (req, res, next) => {
-	res.render('layout');
+	res.render('index');
 });
+
+app.get('/artists', (req, res, next) => {
+	console.log('hello')
+	res.render('artists');
+})
+
+// app.get("/artists", (req, res) => {
+// 	spotifyApi
+// 		.searchArtists(req.query.artist)
+// 		.then(searchResponse => {
+// 			const { artists } = searchResponse.body;
+// 			res.render("artists", artists);
+// 		})
+// 		.catch(err => {
+// 			console.log("The error while searching artists occurred: ", err);
+// 		});
+// });
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
