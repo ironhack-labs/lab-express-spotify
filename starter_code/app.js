2c9e24332eb5e3c9f24f8919b99ff24ef0291f80
Juliane <juliane_ch.arruda@live.com>
2019-12-19 19:26:06 -0300

    third interaction: first step completed

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,8 +9,8 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const app = express();
 
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set('views', `${__dirname  }/views`);
+app.use(express.static(`${__dirname  }/public`));
 
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
@@ -29,11 +29,14 @@ spotifyApi
   });
 
 // the routes go here:
-const spotifyApi = new SpotifyWebApi({
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET
+
+app.get('/', (req, res) => {
+  res.render('home');
 });
 
-app.listen(3000, () =>
-  console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')
-);
+app.get('/artists', (req, res) => {
+  const { artists } = req.query;
+  res.render('artists', { artists });
+});
+
+app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'),);
