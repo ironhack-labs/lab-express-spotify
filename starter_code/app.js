bc6dc8f3a1de9be87270af4dbf14550ee752ddac
Edgar Mora <edgar@MacBook-Air-de-Esteban.local>
2019-12-18 18:05:02 -0600

    Functionality done

--- starter_code/app.js
+++ starter_code/app.js
@@ -25,6 +25,7 @@ spotifyApi
 	.clientCredentialsGrant()
 	.then((data) => {
 		spotifyApi.setAccessToken(data.body['access_token']);
+		console.log("successfully retreived Access Token at " + (new Date()));
 	})
 	.catch((error) => {
 		console.log('Something went wrong when retrieving an access token', error);
@@ -36,8 +37,46 @@ app.get('/', (req, res, next) => {
 });
 
 app.get('/artists', (req, res, next) => {
-	console.log('hello')
-	res.render('artists');
+	spotifyApi
+		.searchArtists(req.query.artist)
+		.then(data => {
+			let artistResult = data;
+			res.render('artists', { artistResult });
+		})
+		.catch(err => {
+			console.error(err);
+			res.render('index');
+		});
+});
+
+app.get("/albums/:artistId", (req, res, next) => {
+
+	spotifyApi
+		.getArtistAlbums(req.params.artistId)
+		.then(data => {
+			let albumResult = data;
+			res.render('albums', { albumResult });
+		})
+		.catch(err => {
+			console.log('The error while searching albums occurred: ', err);
+			res.render('index');
+		});
+
+})
+
+app.get("/tracks/:albumId", (req, res, next) => {
+
+	spotifyApi
+		.getAlbumTracks(req.params.albumId)
+		.then(data => {
+			let tracksResult = data;
+			res.render('tracks', { tracksResult });
+		})
+		.catch(err => {
+			console.log('The error while searching albums occurred: ', err);
+			res.render('index');
+		});
+
 })
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
