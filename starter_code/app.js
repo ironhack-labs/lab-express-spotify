d4a71188873a791f1e11f16cb28c50878ac31fb2
Leonardo Polon <lpolon@gmail.com>
2019-12-19 23:26:48 -0300

    git commit -m " WIP. I will get back to it. get response from spotify API OK. Cool!"

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,18 +6,16 @@ const hbs = require('hbs');
 // require spotify-web-api-node package here:
 const SpotifyWebApi = require('spotify-web-api-node');
 
-const spotifyApi = new SpotifyWebApi(
-  {
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-  },
-);
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
 
 // retrieve an access token
 const getAccessToken = async () => {
   try {
     const data = await spotifyApi.clientCredentialsGrant();
-    console.log(`The access token is: ${data.body.access_token}`)
+    // console.log(`The access token is: ${data.body.access_token}`)
     spotifyApi.setAccessToken(data.body.access_token);
   } catch (error) {
     console.log(error);
@@ -27,13 +25,27 @@ getAccessToken();
 
 const app = express();
 
+// hbs setup
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set('views', `${__dirname}/views`);
+
+hbs.registerPartials(`${__dirname}/views/partials`);
 
 // setting the spotify-api goes here:
 
 // the routes go here:
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+  res.render('index.hbs');
+});
+
+app.get('/artists/:name?', async (req, res) => {
+  console.dir(req.query);
+  const data = await spotifyApi.searchArtists(req.query.name);
+  console.log('query response from spotify: ', data);
+  res.send(data); // send obj to this pace?;
+});
 
 app.listen(3000, () =>
   console.log('My Spotify project running on port 3000 🎧')
