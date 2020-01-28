b6e2ecf3daf649f836cf73a0be29c5cf8ae8dfd6
Evgeny Galitskiy <gha665@gmail.com>
2020-01-27 22:38:40 -0500

    accomplished iterationa 1 through 5 without CSS

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,20 +1,85 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
-
+const express = require("express");
+const hbs = require("hbs");
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+//-------------Require spotify-web-api-node package:
+const SpotifyWebApi = require("spotify-web-api-node");
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
 
-// setting the spotify-api goes here:
+//--------------Setting the spotify-api:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
 
-// the routes go here:
+//--------------Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  })
+  .catch(error => {
+    console.log("Something went wrong when retrieving an access token", error);
+  });
+
+//--------------The routes go here:
+//-----GET Home Page
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+app.get("/artists", (req, res, next) => {
+  //-----Display results for artist search
+  // console.log(req.query.artist);
+  spotifyApi
+    .searchArtists( /*'HERE GOES THE QUERY ARTIST'*/ req.query.artist)
+    .then(data => {
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+      let {
+        items
+      } = data.body.artists;
+      // console.log("The received data from the API: ", items);
+      res.render("artists", {
+        items
+      });
+    })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    });
+});
+
+////-----Display results for album search
+app.get("/albums/:artistId", (req, res, next) => {
+  // console.log(req.params.artistId);
+  const id = req.params.artistId;
+  spotifyApi
+    .getArtistAlbums(id)
+    .then(data => {
+      // console.log(data.body.items);
+      res.render("albums", {
+        albums: data.body.items
+      });
+    })
+    .catch(err => console.log(err));
+});
+
+////-----Display results for tracks search
+app.get("/tracks/:albumId", (req, res, next) => {
+  const idOfTheAlbum = req.params.albumId;
+  console.log(idOfTheAlbum);
+  spotifyApi
+    .getAlbumTracks(idOfTheAlbum)
+    .then(trackData => {
+      // console.log(trackData.body.items);
+      res.render("tracks",{tracks:trackData.body.items});
+    })
+    .catch(err => console.log(err));
+});
 
 app.listen(3000, () =>
-  console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')
+  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
 );
