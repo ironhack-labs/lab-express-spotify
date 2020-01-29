763c283ac6c794fc813aa66733004cd18ceb6988
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-01-29 17:12:58 -0500

    index page not rendering on localhose:3000. Left off on hbs part of iteration #3

--- starter_code/app.js
+++ starter_code/app.js
@@ -12,8 +12,47 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+  //redirect URL needed here?
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  });
 
 // the routes go here:
+//#1: Render index.hbs
+app.get('/', (req, res, next) => {
+  // console.log("hello")
+  res.render('index');
+  //^ this is rendering the hbs file listed in the ()
+});
+
+//#2: Render artist.hbs
+app.get('/artists', (req, res, next)=>{
+
+  spotifyApi.searchArtists(req.query.artists)
+  .then(data => {
+    // console.log("The received data from the API: ", data.body.artists.items);
+    res.render('artists', {artist: data.body.artists.items})
+  })
+  .catch(err => {
+    console.log("The error while searching artists occurred: ", err);
+  })
+
+});
+
+
 
 app.listen(3000, () =>
   console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')
