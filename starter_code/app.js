d05006cabe0d7a7a1b05df5d87094eea91bef13e
Jerlissa Rivas <jerlissarivas@Jerlissas-MacBook-Air.local>
2020-01-29 15:06:48 -0500

    Lab not completed

--- starter_code/app.js
+++ starter_code/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -13,7 +14,54 @@ app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
 
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
+
 // the routes go here:
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+// app.get('/artist', (req, res) => {
+//   spotifyApi
+//   .searchArtists(req.query.artists)
+//   .then(responseFromApi =>
+//     res.render('artist.hbs', {artist: responseFromApi})
+//   .catch(error => console.log(error))
+// )});
+
+app.get('/artist', (req, res) => {
+  spotifyApi
+    .searchArtists(req.query.artist)
+    .then(responseFromApi => 
+      // console.log('Beers from the database: ', responseFromApi))
+    res.render('artist', { artist: responseFromApi })
+    // .catch(error => console.log(error))
+// )
+});
+
+app.get('/album/:artistId', (req, res, next) => {
+  spotifyApi.getArtistAlbums('req.params.artistId')
+    .then(
+      function (data) {
+        console.log('Artist albums', data.body);
+      })
+    .catch(err => {
+      console.log('The error while searching artists occurred: ', err);
+    });
+});
+
+app.get('/track/:albumId', (req, res, next) => {
+spotifyApi.getAlbumTracks('req.params.albumId', { limit : 5, offset : 1 })
+  .then(function(data) {
+    console.log(data.body);
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+});
 
 app.listen(3000, () =>
   console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')
