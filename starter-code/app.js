d92bc41dcaab831781b0bce5aa1206b0fce3cd5d
annkay108 <annkhachhibhoya@gmail.com>
2020-02-08 12:32:14 +0100

    access albums from particular artist

--- starter-code/app.js
+++ starter-code/app.js
@@ -36,10 +36,23 @@ app.get('/artist-search',(req,res)=>{
     let objectArtist = {
       searchedArtist: data.body.artists.items
     };
-    res.render('artist-search', objectArtist);
+    res.render('artist-search-results', objectArtist);
   })
   .catch(err => console.log('The error while searching artists occurred: ', err));
   
 })
 
+app.get('/albums/:artistId',(req, res)=>{
+  spotifyApi.getArtistAlbums(req.params.artistId,{limit: 20, offset: 20},(err,data)=>{
+    if(err){
+      console.error("something went wrong");
+    }
+    else{
+      let objectAlbums = {
+        artistAlbums: data.body.items
+      };
+      res.render("albums",objectAlbums);
+    }
+  });
+})
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
