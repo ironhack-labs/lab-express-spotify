39826705782d0474fadbcf4ce67f2442b9b97326
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-02-10 10:38:36 +0100

    Better names for everything. clearer for me

--- starter-code/app.js
+++ starter-code/app.js
@@ -7,15 +7,21 @@ const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 
-app.set("view engine", "hbs");
-app.set("views", __dirname + "/views");
-app.use(express.static(__dirname + "/public"));
 
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
 	clientId: process.env.CLIENT_ID,
 	clientSecret: process.env.CLIENT_SECRET
 });
+
+//HBS SETUP
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+
+
+//MIDDLEWARE
+//Lets us pass all of the functions and stuff through express
+app.use(express.static(__dirname + "/public"));
 
 
 // Retrieve an access token
@@ -32,16 +38,20 @@ app.get("/", (req, res, next) => {
 	res.render("index");
 });
 
-app.get("/artist-search", (req, res) => {	
-	const artista = req.query.artist;
+app.get("/artist-search", (req, res) => {
 	console.log("artista", req.query.artist);
+	const artistaObj = req.query.artist;
 
+	//Getting info with SpotifyAPI
 	spotifyApi
-		.searchArtists(artista)
-		.then(data => {
-				console.log("Found artist", data.body.artists.items);
-				const artistsList = data.body.artists.items;
-				res.render("artist-search-results", {artistsList});
+		.searchArtists(artistaObj)
+		.then(responseFromAPI => {
+				// console.log("Data received from API about artist", responseFromAPI.body.artists.items);
+				const artistsList = responseFromAPI.body.artists.items;
+
+				res.render("artist-search-results", {
+					artistsList
+				});
 			},
 			function (err) {
 				console.error(err);
@@ -51,15 +61,17 @@ app.get("/artist-search", (req, res) => {
 
 
 app.get("/albums/:artistId", (req, res, next) => {
-	console.log("artist", req.query.artistId); //=> find albums adress
+	// console.log("artist", req.query.artistId); //=> find albums adress
 	const lastPartOfUrlakaArtistId = req.params.artistId;
 
 	spotifyApi
 		.getArtistAlbums(lastPartOfUrlakaArtistId)
-		.then(data => {
-				const albumsList = data.body.items;
+		.then(responseFromAPI => {
+				const albumsList = responseFromAPI.body.items;
 				// console.log("Found album", data.body);			 
-				res.render("albums.hbs", {albumsList});
+				res.render("albums.hbs", {
+					albumsList
+				});
 			},
 			function (err) {
 				console.error(err);
@@ -67,22 +79,25 @@ app.get("/albums/:artistId", (req, res, next) => {
 		);
 });
 
+
 app.get("/tracks/:albumId", (req, res, next) => {
-	// console.log("ALBUM", req.params.albumId);
+	// console.log("ALBUM", req.query.albumId);
 	const albumId = req.params.albumId;
 
 	spotifyApi
 		.getAlbumTracks(albumId)
-		.then(data => {
-			// console.log("Album tracks", data.body.items);
-			const tracksList = data.body.items;
-			//Song preview URL : 
-			// console.log("preview_url":, data.body.items[0].preview_url);
-			res.render("tracks", { tracksList});
+		.then(response => {
+				// console.log("Album tracks", response.body.items);
+				const tracksList = response.body.items;
+				//Song preview URL : 
+				// console.log("preview_url":, response.body.items[0].preview_url);
+				res.render("tracks", {
+					tracksList
+				});
 			},
 			function (err) {
-			console.log('Something went wrong!', err);
-		});
+				console.log('Something went wrong!', err);
+			});
 });
 
 
