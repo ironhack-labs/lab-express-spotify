7018ad8837a0c26e335c1ac34553d6a1e6dae39c
RaqueCoelho <57578500+RaqueCoelho@users.noreply.github.com>
2020-02-13 15:31:01 +0000

    done without css

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,7 +12,53 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+app.get('/', (request, response) => {
+  response.render('index');
+});
+
+app.get('/artist-search', (request, response) => {
+  // credentials are optional
+  spotifyApi
+    .searchArtists(request.query.artist)
+    .then(data => {
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+      response.render('artist-search-results.hbs', { artists: data.body.artists.items });
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
+});
+
+app.get('/albums/:artistId', (req, res, next) => {
+  // .getArtistAlbums() code goes here
+  spotifyApi
+    .getArtistAlbums(req.params.artistId)
+    .then(data => {
+      console.log('Received from the api:', data.body);
+      res.render('albums.hbs', { albums: data.body.items });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/tracks/:albumId', (req, res) => {
+  spotifyApi
+    .getAlbumTracks(req.params.albumId)
+    .then(data => {
+      res.render('tracks.hbs', { tracks: data.body.items });
+    })
+    .catch(error => console.log(error));
+});
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
