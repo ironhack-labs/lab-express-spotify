36bea57cae5b5710ece34cef684bc6a77ce25a3f
alinepasetti <pasettialine@gmail.com>
2020-02-13 15:37:47 +0000

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,8 +2,16 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
+
+// Retrieve an access token
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
 
 const app = express();
 
@@ -12,7 +20,54 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/artist-search', (req, res) => {
+  const term = req.query.term;
+  spotifyApi
+    .searchArtists(term)
+    .then(data => {
+      const artists = {
+        artist: data.body.artists.items
+      };
+      res.render('artist-search-results', artists);
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
+});
+
+app.get('/albums/:artistId', (req, res) => {
+  const artistId = req.params.artistId;
+  spotifyApi
+    .getArtistAlbums(artistId)
+    .then(data => {
+      const artist = {
+        albums: data.body.items
+      };
+      res.render('albums', artist);
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
+});
+
+app.get('/album/:albumId', (req, res) => {
+  const albumId = req.params.albumId;
+  spotifyApi
+    .getAlbumTracks(albumId)
+    .then(data => {
+      const albums = {
+        tracks: data.body.items
+      };
+      console.log('this albums', albums.tracks[0].name);
+      res.render('tracks', albums);
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
+});
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
