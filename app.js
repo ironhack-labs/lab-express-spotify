959cac5f8c2bce0e67b6f197bb5fd41edc11d89f
Thibault-d <thibaultdelfaud@gmail.com>
2020-02-22 14:01:33 +0100

    search bar and query string ok

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
+
+// Spotify connection
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
 });
 
+// Retrieve an access token
 spotifyApi
     .clientCredentialsGrant()
     .then(data => spotifyApi.setAccessToken(data.body['access_token']))
@@ -21,5 +24,9 @@ spotifyApi
 
 // Our routes go here:
 app.get("/", (req, res, next) => res.render("index"));
+app.get("/artists", (req, res, next) => {
+    console.log(req.query);
+    res.render("artists");
+});
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
