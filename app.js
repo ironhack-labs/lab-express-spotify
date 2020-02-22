761e5167422f0d7d223fa47b10ac05f4d8846a7a
Thibault-d <thibaultdelfaud@gmail.com>
2020-02-23 00:17:10 +0100

    Great progress. Took me forever to figure the audio controls. More CSS to do

--- app.js
+++ app.js
@@ -27,16 +27,39 @@ spotifyApi
 app.get("/", (req, res, next) => res.render("index"));
 
 app.get("/artists", (req, res, next) => {
-    console.log(req.query.artist);
     spotifyApi.searchArtists(req.query.artist)
         .then(datafromAPI => {
-            res.render("artists", {
-                artistsresults: datafromAPI.body
+            res.render("artist-search-results", {
+                artistsresults: datafromAPI.body.artists.items
             });
-            console.log(datafromAPI.body.artists.items);
         }, function (err) {
             console.error(err);
         });
 });
+
+app.get("/albums/:artistIdentifier", (req, res, next) => {
+    spotifyApi.getArtistAlbums(req.params.artistIdentifier)
+        .then(datafromAPI => {
+            res.render("albums", {
+                albums: datafromAPI.body.items
+            })
+        }, function (err) {
+            console.error(err);
+        });
+});
+
+app.get("/tracks/:albumIdentifier", (req, res, next) => {
+
+    spotifyApi.getAlbumTracks(req.params.albumIdentifier)
+        .then(datafromAPI => {
+            res.render("tracks", {
+                tracks: datafromAPI.body.items
+                
+            })
+        }, function (err) {
+            console.error(err);
+        });
+});
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
