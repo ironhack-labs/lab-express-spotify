ff62612eb054ed98d46d5fe988f7a5110140c679
Unai Gonzalez <unaigoe91@gmail.com>
2020-03-12 21:35:01 +0100

    BCN-FT-Unai Gonzalez. Todo ok menos Css por este maldito error que al final he podido arreglar Refused to apply style from 'http://localhost:3000/public/styles/style.css' because its MIME type ('text/html') is not a supported stylesheet MIME type, and strict MIME checking is enabled.

--- app.js
+++ app.js
@@ -2,17 +2,83 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
-
+const SpotifyWebApi = require('spotify-web-api-node');
+const path = require('path')
 // require spotify-web-api-node package here:
 
 const app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/'));
 
-// setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
 
-// Our routes go here:
+  spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
+  
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
+
+app.get('/', (req,res,next) => res.render('index'));
+
+app.get('/artist-search', (req, res, next) => {
+    console.log(req.query.artist)
+    spotifyApi
+  .searchArtists(req.query.artist)
+  .then(data => {
+
+    let spotiArtist = data.body.artists.items
+   //console.log('The received data from the API: ', {spotiArtist});
+    //console.log(spotiArtist[0].artistid--undefined)
+    res.render('artist-search-results', {spotiArtist})
+    
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));    
+
+});
+
+app.get('/albums/:id', (req, res, next) => {
+   
+    spotifyApi
+    .getArtistAlbums(req.params.id) 
+    .then(data => {
+       // console.log(data)
+        let spotiAlbum = data.body.items
+        
+       // console.log('The received data from the API: ', {spotiAlbum});
+       //console.log(spotiAlbum[0].images[0].url)
+        res.render('albums', {spotiAlbum} )
+
+    })
+    .catch(err => console.log(`You made a mistake ${err} `))
+
+  });
+
+
+
+  app.get('/tracks/:id', (req, res, next) => { 
+  
+  spotifyApi
+  .getAlbumTracks(req.params.id)
+  .then (data => { 
+    console.log(data)
+
+    let spotiTrack = data.body.items
+    console.log({spotiTrack})
+    res.render('tracks', {spotiTrack})
+  
+  })
+
+  .catch(err => console.log(`you made a mistake ${err}`))
+
+})
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
