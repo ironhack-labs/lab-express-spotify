c954485c4eb5f91c344a137a3f2109e3e788169b
Stefano Santonocito <oddouu@gmail.com>
2020-04-15 21:57:44 +0100

    on iteration 3

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 
+
 const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
+
 
 const app = express();
 
@@ -12,7 +15,42 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+app.get("/", (req, res) => {
+    res.render("layout");
+    //console.log(req);
+});
+
+app.get("/artist-search", (req, res) => {
+    let artistFromForm = req.query.artist;
+    let searchResults;
+
+    console.log(`logging query`, req.query);
+
+
+    spotifyApi
+        .searchArtists( artistFromForm )
+        .then(data => {
+            searchResults = data.body;
+            console.log('The received data from the API: ', data.body);
+            // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+            res.render("artist-search-results", 
+                {searchResults});
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+
+    
+});
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
