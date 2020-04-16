64a81d7cad3f41db98135f4d00a7557b15fc1109
DayanRojas <dayan18_13@hotmail.com>
2020-04-16 18:18:34 +0200

    done

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -13,6 +12,46 @@ app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
 
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
 // Our routes go here:
+app.get('/', (req, res, next) => res.render('index'))
+
+app.get('/artist-search', (req, res, next) => {
+    spotifyApi
+        .searchArtists(req.query.artist)
+        .then(data => res.render('artist-search-results', data.body))
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
+app.get('/albums/:artistId', (req, res, next) => {
+    spotifyApi
+        .getArtistAlbums(req.params.artistId)
+        .then(albums => {
+            //console.log(albums.body)
+            res.render('albums', albums.body)
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
+app.get('/tracks/:albumId', (req, res, next) => {
+    spotifyApi
+        .getAlbumTracks(req.params.albumId)
+        .then(tracks => {
+            //console.log(tracks.body)
+            res.render('tracks', tracks.body)
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+
+})
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
