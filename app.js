8b871fecdcd03bfaebadf4be5fb9ac3cfa400c3b
Julian Bajo <jbajopinedo@gmail.com>
2020-04-16 18:52:53 +0200

    Ejercicio resuelto hasta iteracion 4 y la mitad de esta, falta que aparezca la informacion de los albumes en la vista, pero la funcion está desarollada y probada

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -13,6 +14,50 @@ app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
 
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
 // Our routes go here:
+
+app.get('/', (req,res) => res.render('index'))
+app.get('/artist-search', (req, res) => {
+
+    console.log(req.query.artist)
+    
+
+    spotifyApi
+        .searchArtists(req.query.artist)
+        .then(data => { 
+            console.log('The received data from the API: ', data.body.artists);
+        // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+        res.render('artist-search-results', data.body.artists)
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
+app.get('/albums/:id', (req, res, next) => {
+    // console.log('-------------------------------------------hola')
+    
+
+    spotifyApi
+        .getArtistAlbums(req.params.id)
+        .then(album => {
+            console.log({album})
+            return res.render('albums', {album})
+        })
+
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+
+});
+
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
