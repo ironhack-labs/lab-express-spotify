a0ebb3371817602fe31f1029942f7da8732085a6
vitor lopes <lopesvitor959@gmail.com>
2020-04-18 11:43:23 +0100

    LIS-PTWD-FEB2020-Vitor-Lopes

--- HELPfile_app.js.js
+++ HELPfile_app.js.js
@@ -35,11 +35,6 @@ app.get('/artist-search-results',(req,res)=> {
     console.log('artist-search-results route was created')
 })
 
-app.get('/albums', (req,res) => {
-  console.log('albums route was created')
-})
-
-
 app.get('/artist-search', (req, res, next) => {
     console.log(req.query.artname) // --> { artname: 'placebo' } if in the form I type "placebo" and submit the form
     spotifyApi
@@ -47,32 +42,40 @@ app.get('/artist-search', (req, res, next) => {
   .then(data => {
     console.log('The received data from the API: ', data.body);
     console.log('One of the items of the data: ', data.body.artists.items[0]);
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    // app.get('/artist-search-results', (req, res) => {
+        //! CAN WE CREATE A ROUTE INSIDE OF ANOTHER ROUTE??? , maybe not and that is why I couldn't access artist-search-results
+    //     res.send('this route is just to  ...');
+    //   })
+    // app.post('/artist-search-results', (req, res) => {
+    //      // if we want to make post requests we have to have body-parser installed
+    //     // res.render('artist-search-results', { artists: data });
+    //   //  let name = req.body.name;
+    //     console.log('The name is:', name)
+    //     // let image= req.body.images.url
+    //      // res.render('artist-search-results', { name });
+
+    //      // VER COMENTARIOS DO ANDRE NO SLACK. txt no desktop
+    //   });
     let artists = data.body.artists.items
     console.log('sending data to artist-search results')
-    res.render('artist-search-results',{ artists }  )     
+    res.render('artist-search-results',{ artists }  )
 
   })
   .catch(err => console.log('The error while searching artists occurred: ', err));
 })
 
+app.get('/albums/:artistId', (req, res, next) => {
+  // .getArtistAlbums() code goes here
+  console.log(req.params.id)
+  spotifyApi
+  .getArtistAlbums(req.params.id)
+    .then(data => {
+    console.log('The received data from the API: ', data.body);
+    // console.log('One of the items of the data: ', data.body.artists.items[0])});
+});
+})
 
- app.get('/albums/:id', (req, res, next) => {
-            // .getArtistAlbums() code goes here
-            console.log(req.params.id)
-            let id = req.params.id;
-            spotifyApi
-            .getArtistAlbums(id)
-              .then(data => {
-              console.log('The received data from the API: ', data.body);
-              // console.log('The received data from the API: ', data.body.images);
-              // console.log('One of the items of the data: ', data.body.artists.items[0])});
-              let items = data.body.items;
-              //! Why can I pass images to albums.hbs ????
-              console.log(items)
-               res.render('albums', { items} )
-            })
-           .catch(err => console.log('The error while searching albums occurred: ', err))
-          })
 
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000'));
