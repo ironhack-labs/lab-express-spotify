3bb8164a68ef69364faa63ceb4552a64047b74cf
William Hermozo <william.hermozo@protonmail.com>
2020-04-20 21:58:42 +0200

    functionality done

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const app = express();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
-app.use('/', require('./routes/index'));
 app.locals.title = 'Spotify Index';
 
 // API setup
@@ -18,12 +17,63 @@ const spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
 });
+
+// Getting access token
 const port = spotifyApi
   .clientCredentialsGrant()
   .then((data) => spotifyApi.setAccessToken(data.body['access_token']))
   .catch((error) =>
     console.log('Something went wrong when retrieving an access token', error)
   );
+
+// Routes
+app.get('/', (req, res, next) => {
+  res.render('home.hbs');
+});
+
+app.get('/artist-search', (req, res, next) => {
+  spotifyApi
+    .searchArtists(req.query.searchArtist)
+    .then((data) => {
+      console.log(data.body.artists);
+      res.render('artist-search-results.hbs', {
+        artists: data.body.artists.items,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('home.hbs');
+    });
+});
+
+app.get('/albums/:artistId', (req, res, next) => {
+  spotifyApi
+    .getArtistAlbums(req.params.artistId)
+    .then((data) => {
+      console.log(data.body.items);
+      res.render('albums.hbs', {
+        albums: data.body.items,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('home.hbs');
+    });
+});
+
+app.get('/albums/tracks/:albumId', (req, res) => {
+  spotifyApi
+    .getAlbumTracks(req.params.albumId)
+    .then((data) => {
+      res.render('tracks', {
+        tracks: data.body.items,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/');
+    });
+});
 
 // Server setup
 app.listen(process.env.PORT, () =>
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// home
-router.get('/', (req, res) => {
-  res.render('index.hbs');
-});
-
-module.exports = router;
