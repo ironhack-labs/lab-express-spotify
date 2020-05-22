5295ce15c978a84200c87a03a80e546c85c1512e
Marcela Vilas Boas <marcela.vilasboas@gmail.com>
2020-05-22 14:36:37 +0200

    adding artists  cards

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
-
 // require spotify-web-api-node package here:
 const SpotifyWebApi = require('spotify-web-api-node');
 
@@ -24,5 +23,43 @@ spotifyApi
     .then(data => spotifyApi.setAccessToken(data.body['access_token']))
     .catch(error => console.log('Something went wrong when retrieving an access token', error));
 // Our routes go here:
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.get('/artist-search', (req, res) => {
+    spotifyApi.searchArtists(req.query.artista) //string digitada no campo Artista
+        .then(data => {
+            console.log('The received data from the API: ', JSON.stringify(data.body));
+            console.log(data.body.artists.items[0].name);
+            console.log(data.body.artists.items[0].images[0].url);
+            const artistSearched = {
+                lista: [],
+            }
+            for (let i = 0; i < data.body.artists.items.length; i++) {
+                if (data.body.artists.items[i].images.length > 0) {
+                    artistSearched.lista.push({
+                        name: data.body.artists.items[i].name,
+                        img: data.body.artists.items[i].images[0].url
+                    })
+                } else {
+                    artistSearched.lista.push({
+                        name: data.body.artists.items[i].name,
+                        img: "https://data.pixiz.com/output/user/frame/preview/api/big/5/0/4/8/1968405_67bf5.jpg"
+                    })
+                }
+
+            }
+            res.render('artist-search-results', artistSearched) //parametros: para onde, o que se envia
+            // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+
+})
+
+
+
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
