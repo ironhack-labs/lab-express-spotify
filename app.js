015f19bc7eba9e6006b3536587c2281c9457abc7
Marc De Mena <mdemena@gmail.com>
2020-05-22 21:23:31 +0200

    Finished Iteration 4

--- app.js
+++ app.js
@@ -25,22 +25,96 @@ spotifyApi
 
 app.get('/', (req, res, next) => res.render('index'));
 
-app.get('/artist-search/:artistSearch', (req, res, next) => {
+app.get('/artist-search', (req, res, next) => {
 	spotifyApi
-		.searchArtists(`${req.params.artistSearch}`)
+		.searchArtists(`${req.query.search}`)
 		.then((data) => {
-			console.log('The received data from the API: ', data.body);
+			let artistSearchDTO = [];
+			data.body.artists.items.forEach((artist) => {
+				let artistDTO = {
+					id: artist.id,
+					name: artist.name,
+					image: 'http://localhost:3000/images/spotify-background.jpg',
+				};
+				let image = artist.images.filter((img) => img.width === 300);
+				if (image.length > 0) {
+					artistDTO.image = image[0].url;
+				} else {
+					image = artist.images.filter((img) => img.width === 64);
+					if (image.length > 0) {
+						artistDTO.image = image[0].url;
+					}
+				}
+				artistSearchDTO.push(artistDTO);
+			});
+			res.render('artist-search-results', { artistSearchDTO });
 		})
 		.catch((err) =>
 			console.log('The error while searching artists occurred: ', err)
 		);
 });
 
-app.get('/albums/:artistId', (req, res, next) => {
+app.get('/albums/:artistName/:artistId', (req, res, next) => {
+	let albumsDTO = {
+		artistName: req.params.artistName,
+		albums: [],
+	};
 	spotifyApi
 		.getArtistAlbums(req.params.artistId)
 		.then((data) => {
-			console.log('The received data from the API: ', data.body);
+			//console.log('The received data from the API: ', data.body);
+			data.body.items.forEach((album) => {
+				let albumDTO = {
+					id: album.id,
+					name: album.name,
+					image: 'http://localhost:3000/images/spotify-background.jpg',
+				};
+				let image = album.images.filter((img) => img.width === 300);
+				if (image.length > 0) {
+					albumDTO.image = image[0].url;
+				} else {
+					image = artist.images.filter((img) => img.width === 64);
+					if (image.length > 0) {
+						albumDTO.image = image[0].url;
+					}
+				}
+				albumsDTO.albums.push(albumDTO);
+			});
+			res.render('albums', albumsDTO);
+		})
+		.catch((err) =>
+			console.log('The error while searching artists occurred: ', err)
+		);
+});
+
+app.get('/tracks/:artistName/:albumName/:albumId', (req, res, next) => {
+	let tracksDTO = {
+		artistName: req.params.artistName,
+		albumName: req.params.albumName,
+		tracks: [],
+	};
+	spotifyApi
+		.getArtistAlbums(req.params.artistId)
+		.then((data) => {
+			//console.log('The received data from the API: ', data.body);
+			data.body.items.forEach((album) => {
+				let albumDTO = {
+					id: album.id,
+					name: album.name,
+					image: 'http://localhost:3000/images/spotify-background.jpg',
+				};
+				let image = album.images.filter((img) => img.width === 300);
+				if (image.length > 0) {
+					albumDTO.image = image[0].url;
+				} else {
+					image = artist.images.filter((img) => img.width === 64);
+					if (image.length > 0) {
+						albumDTO.image = image[0].url;
+					}
+				}
+				albumsDTO.albums.push(albumDTO);
+			});
+			res.render('tracks', albumsDTO);
 		})
 		.catch((err) =>
 			console.log('The error while searching artists occurred: ', err)
