6546382634b4250d738fc836a71979ac58416ffa
David de Lamo <d.lamo.valverde@gmail.com>
2020-05-23 17:23:15 +0200

    spotify configuration refactor

--- routes/albumRouter.js
+++ routes/albumRouter.js
@@ -1,18 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const SpotifyWebApi = require('spotify-web-api-node');
-
-// setting the spotify-api goes here:
-const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-  });
-  
-// Retrieve an access token
-spotifyApi
-    .clientCredentialsGrant()
-    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
-    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+const spotifyApi = require('./spotifyConfig');
 
 router.get('/:artistId', (req, res, next) => {
     const artistId = req.params.artistId;
