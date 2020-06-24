589a3793b26a9e44d59bf739486bee7513ba21df
fermarinsanchez <fermarinsanchez@yahoo.es>
2020-06-24 20:38:47 +0200

    nomino a Roman

--- app.js
+++ app.js
@@ -25,4 +25,20 @@ const spotifyApi = new SpotifyWebApi({
   .catch(error => console.log('Something went wrong when retrieving an access token', error));
 // Our routes go here:
 
+app.get('/', (req, res, next) => {
+  res.render('index')
+})
+
+app.get('/artist-search', (req, res, next) => {
+  const artSearch = req.query.artist_name
+
+  spotifyApi
+  .searchArtists(artSearch)
+  .then(data => {
+   res.send(data.body)
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
