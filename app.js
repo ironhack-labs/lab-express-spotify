c6dc05d6c0bce3ad592807666009b80cb541e25b
Old <onelifedesigning@gmail.com>
2020-06-24 19:49:18 +0200

    iterarion 1

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 require('dotenv').config();
 
 const express = require('express');
-const hbs = require('hbs');
+const ejs = require('ejs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
 
 const app = express();
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
