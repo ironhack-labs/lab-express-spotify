d105c49b030a6b25163a36108f745329bc3f44da
Pumpel <romanbaos@gmail.com>
2020-06-24 21:29:46 +0200

    muy bien equipo

--- app.js
+++ app.js
@@ -35,10 +35,36 @@ app.get('/artist-search', (req, res, next) => {
   spotifyApi
   .searchArtists(artSearch)
   .then(data => {
-   res.send(data.body)
+    const searchResults = data.body.artists.items;
+    console.log(searchResults[0])
+   res.render('artist-search-results',{artists: searchResults})
+
     // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
   })
   .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
+app.get('/albums/:id', (req, res, next) => {
+  spotifyApi
+  .getArtistAlbums(req.params.id)
+  .then(data => {
+    const searchResults = data.body.items;
+    res.render('albums',{albums: searchResults})
+
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+  })
+  .catch(err => console.log('The error while searching albums occurred: ', err));
+})
+
+app.get('/tracks/:id', (req, res, next) => {
+  spotifyApi
+  .getAlbumTracks(req.params.id)
+  .then(data => {
+    res.send(data.body)
+
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+  })
+  .catch(err => console.log('The error while searching albums occurred: ', err));
 })
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
