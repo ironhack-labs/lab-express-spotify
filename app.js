2c90ae7ae4e7c9ac40290fca528a8440472219ec
Laura Martinez Delgado <lauram.delgado87@gmail.com>
2020-06-25 19:16:55 +0200

    week4_day4

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
+const path = require('path')
+const SpotifyWebApi = require('spotify-web-api-node')
 
 const app = express();
 
@@ -11,8 +11,46 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-// setting the spotify-api goes here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'))
 
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+});
+
+
+
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => {
+     spotifyApi.setAccessToken(data.body["access_token"])
+        
+    })
+    
+  .catch((error) => 
+    console.log("Something went wrong when retrieving an access token", error)
+);
+  
 // Our routes go here:
+app.get('/', (req, res) => res.render('index'))
+app.get('/artist-search', (req, res) => {
+    console.log(req.query.from)
+    spotifyApi
+        .searchArtists(req.query.from)
+        .then(data => {
+            console.log('The received data from the API: ', data.body);
+            res.render('artist-search-results', {
+                artist: data.body.artists.items
+            })
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+})
+app.get('/albums/:artistId', (req, res) => {
+    spotifyApi
+    .getArtistAlbums(req.params.artistId)
+        .then(album => res.render('albums', album.body))
+        .catch(err => console.log('The error while searching artists occurred: ', err))
+})
+app.listen(3001, () => console.log('My Spotify project running on port 3001 🎧 🥁 🎸 🔊'));
+
