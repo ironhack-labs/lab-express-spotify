15f75aae171469e9a20114e1a36f42b862b7850a
Paola Garay <garay.angela@gmail.com>
2020-06-28 17:07:20 +0200

    Iteration 2

--- app.js
+++ app.js
@@ -15,10 +15,11 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api
-const spotifyApi = new SpotifyWebApi({
+  const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
   });
+
 
   // Retrieve an access token
 spotifyApi
@@ -27,5 +28,16 @@ spotifyApi
 .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 
-// Routes
+//Routes
+app.get('/', (req, res, next) => {
+    res.render('index');
+  });
+
+app.get('/artist-search', (req, res, next) => 
+    spotifyApi
+        .searchArtists(req.query.artist)
+        .then((data) => res.render("artist-search", { artists: data.body.artists.items}))
+        .catch(err => console.log('The error while searching artists occurred: ', err))
+  );
+
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
