c21aa4e96725d82cc8bfc8d150cbcddb235a3d8b
SiEr0212 <simonertel@gmx.de>
2020-06-28 21:31:45 +0200

    everything is done exept styling(problems to connect styles.css to the rest on ubuntu)

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '.'));
 
 // setting the spotify-api goes here:
 
@@ -27,3 +27,38 @@ const spotifyApi = new SpotifyWebApi({
 // Our routes go here:
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/artist-search', (req, res) => {
+  spotifyApi
+  .searchArtists(req.query.artist)
+  .then(data => {
+    console.log('The received data from the API: ', data.body);
+    res.render('artist-search-results', {artists: data.body.artists.items})
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+});
+app.get('/albums/:artistId', (req, res, next) => {
+  spotifyApi
+  .getArtistAlbums(req.params.artistId) 
+  .then((data) => {
+    console.log(data.body); 
+    const albums =data.body.items
+    res.render('albums', {albums: albums})
+})
+.catch(err => console.log('Not possible to get albums'))
+});
+
+app.get('/tracks/:albumId', (req, res, next) => {
+  spotifyApi
+  .getAlbumTracks(req.params.albumId) 
+  .then((data) => {
+    console.log(data.body); 
+    const tracks =data.body.items;
+    res.render('tracks', {tracks: tracks})
+})
+.catch(err => console.log('Not possible to get tracks'))
+});
