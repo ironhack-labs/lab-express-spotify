080fc17a409b2dc126ad1281c5682a3f36ef415a
Majd Sabik <majdsabik.ms@gmail.com>
2020-06-28 15:23:08 +0200

    Functionality implemented

--- app.js
+++ app.js
@@ -1,18 +1,51 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
+const express = require("express");
+const hbs = require("hbs");
 
 // require spotify-web-api-node package here:
-
+const SpotifyWebApi = require("spotify-web-api-node");
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
 
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+  .catch((error) => console.log("setAccessToken error", error));
 // Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.get("/", (req, res) => res.render("index"));
+
+app.get("/artists", (req, res) =>
+  spotifyApi
+    .searchArtists(req.query.name)
+    .then((data) => res.render("artists", { artists: data.body.artists.items }))
+    .catch((err) => console.log("Artist search error: ", err))
+);
+
+app.get("/albums/:id", (req, res) =>
+  spotifyApi
+    .getArtistAlbums(req.params.id)
+    .then((data) => res.render("albums", { albums: data.body.items }))
+    .catch((err) => console.log("Album retrieval error: ", err))
+);
+
+app.get("/songs/:id", (req, res) =>
+  spotifyApi
+    .getAlbumTracks(req.params.id)
+    .then((data) => res.render("songs", { songs: data.body.items }))
+    .catch((err) => console.log("Album songs retrieval error: ", err))
+);
+
+app.listen(3000, () =>
+  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+);
