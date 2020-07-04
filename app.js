74976e8e7c7c9c66dc8a2e01b502be055f87cee0
Julia Foresti <juliajforesti@gmail.com>
2020-07-04 19:18:45 -0300

    fixing heroku bug

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ const spotifyApi = new SpotifyWebApi({
       res.render("tracks", { tracks });
     });
     
-    app.listen(3000, () =>
+    app.listen((process.env.PORT || 3000), () =>
       console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
     );
 
