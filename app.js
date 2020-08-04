707213bcacb8e28bb570a7bcc0d301ee99f4590c
Giovanna Izzo <giovannapenelopeizzo@gmail.com>
2020-08-04 17:50:13 +0200

    commplete done

--- app.js
+++ app.js
@@ -2,17 +2,56 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
+const path = require('path');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
+  
+// Retrieve an access token
+spotifyApi
+.clientCredentialsGrant()
+.then(data => spotifyApi.setAccessToken(data.body['access_token']))
+.catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+app.get('/', (req, res) => {
+    res.render('layout');
+});
+
+app.get('/artist-search', (req, res) => {
+    const artist = req.query.artist;
+    spotifyApi
+  .searchArtists(artist)
+  .then(data => {
+    console.log('The received data from the API: ', data.body);
+    console.log('item', JSON.stringify(data.body.artists.items[1]))
+    res.render('artist-search-results.hbs', {artists: data.body.artists.items});
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+});
+
+app.get('/albums/:artistId', (req, res, next) => {
+    // .getArtistAlbums() code goes here
+    spotifyApi
+    .getArtistAlbums(req.params.artistId)
+    .then(data => {
+        console.log('The received albums from the API: ', data.body);
+        res.render('albums.hbs', {albums: data.body.artists.items});
+    })
+    .catch(err => console.log('The error while searching artists\' album occurred: ', err));
+  });
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
