41b13b76eef1726819dbc5ca3314ed614d1e2dbd
anna dodo <anna.dorenskaya@gmail.com>
2020-09-01 19:45:04 +0200

    1st and 2nd iterations

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+//To avoid making our API keys public, we don't want to add and commit them.
+// We'll use a package named dotenv for that.
 require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
-
+const SpotifyWebApi = require('spotify-web-api-node')
 // require spotify-web-api-node package here:
 
 const app = express();
@@ -12,6 +14,26 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
+
+//This package is imported at the very beginning of app.js. All that 
+//is left to do is to add your keys in the .env file.
+//CLIENT_ID=your clientId goes here
+//CLIENT_SECRET=your clientSecret goes here
+
+//The .env is referred to in the .gitignore file so you're safe!
+
 
 // Our routes go here:
 
