d97d0ea34c069091646a9aa74d262378c90e480d
Tolga Kara <tolgakara2.0@gmail.com>
2020-09-05 07:01:45 +0200

    Done

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express()
 
 app.set("view engine", "hbs")
 app.set("views", __dirname + "/views")
+hbs.registerPartials("/views/partials")
 app.use(express.static(__dirname + "/public"))
 
 // setting the spotify-api goes here:
@@ -47,14 +48,26 @@ app.get("/artist-search", async (req, res) => {
 	res.render("artist-search-results", { artists: getArtist })
 })
 
-app.get("/albums/:artistID", (req, res) => {
-  let getAlbum = await spotifyApi
-		.getAlbum(req.query.name)
+app.get("/albums/:id", async (req, res) => {
+	console.log()
+	await spotifyApi
+		.getArtistAlbums(req.params.id)
 		.then((val) => {
-			return val.body.artists.items
+			console.log(val.body.items)
+			res.render("albums", { albums: val.body.items })
 		})
 		.catch((err) => console.log("Something went wrong by getting the Albums: ", err))
-	res.render("index", { albums: getAlbum })
+})
+
+app.get("/album/:id", async (req, res) => {
+	console.log()
+	await spotifyApi
+		.getAlbumTracks(req.params.id)
+		.then((val) => {
+			console.log(val.body.items)
+			res.render("tracks", { tracks: val.body.items })
+		})
+		.catch((err) => console.log("Something went wrong by getting the Albums: ", err))
 })
 
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"))
