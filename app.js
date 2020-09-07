7f5f5489d48bce509d6653d5ac93911232e90d85
Your Name <daniela.kirilova@gmx.de>
2020-09-07 09:21:11 +0200

    WDFT-BER-Daniela

--- app.js
+++ app.js
@@ -42,7 +42,62 @@ app.get("/artist-search", (req, res) => {
     .catch((err) =>
       console.log("The error while searching artists occurred: ", err)
     );
+}); 
+app.get('/albums/:artistId', (req, res, next) => {
+
+  spotifyApi.getArtistAlbums(
+      req.params.artistId, {
+          limit: 10,
+          offset: 20
+      },
+      
+      function (err, data) {
+          
+          if (err) {
+              console.error('Something went wrong!', err);
+          } else {
+              const spotifyAlbumsArray = data.body.items;
+
+              const albumsArray = spotifyAlbumsArray.map(function (album) {
+                  return {
+                      name: album.name,
+                      image: album.images[0],
+                      id: album.id
+                  };
+              });
+             
+              res.render('albums', { albumsArray });
+          }
+          
+      }
+  );    
 });
-app.listen(3000, () =>
-  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
-);
+
+
+app.get('/tracks/:albumId', (req, res) => {
+
+  spotifyApi.getAlbumTracks(
+      req.params.albumId, { 
+          limit : 5, 
+          offset : 1 
+      })
+  .then(function(data) {
+    const spotifyTracksArray = data.body.items;
+ 
+    const tracksArray = spotifyTracksArray.map(function (track) {
+        return {
+            name: track.name,
+            audio: track.preview_url,
+            id: track.id
+        };
+    });
+ 
+    res.render('tracks', { tracksArray});
+
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+
+});
+
+app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
