34256cfee8ba8ce2c1c786d1b4f36fe29ea2891a
Arua Monteiro <aruamonteiro@gmail.com>
2020-09-15 08:13:23 -0300

    done

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const hbs = require("hbs");
 const router = require("./routes/index");
 
-// require spotify-web-api-node package here:
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 
@@ -12,7 +12,17 @@ app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/public"));
 
-// setting the spotify-api goes here:
+// setting the spotify-api:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+  .catch((error) => console.log("Something went wrong when retrieving an access token", error));
 
 // Our routes go here:
 
@@ -20,6 +30,4 @@ app.use("/", router);
 
 // just to force update
 
-app.listen(3000, () =>
-  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
-);
+app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 const router = require("express").Router();
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
+
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+  .catch((error) => console.log("Something went wrong when retrieving an access token", error));
 
 router.get("/", (req, res) => res.render("home"));
 
-router.get("/artist-search", (req, res) => {
-  console.log(req.query);
+router.get("/artist-search", async (req, res) => {
+  try {
+    const result = await spotifyApi.searchArtists(req.query.artistSearch);
+    res.render("artist-search-results", { artists: result.body.artists.items });
+    console.log("The received data from the API: ", result.body.artists.items[0]);
+  } catch (err) {
+    console.log("The error while searching artists occurred: ", err);
+  }
 });
 
 module.exports = router;
