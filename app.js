ed42f39943ce31b7f4cfd327473b53e29acb11a3
Jonathan Jimenez <jonathanajp@gmail.com>
2020-10-23 00:29:16 +0200

    done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -12,7 +13,55 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
-
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  // Retrieve an access token
+  spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
 // Our routes go here:
+app.get('/', (req, res) => {
+    res.render('index');
+  });
+
+app.get('/artist-search', (req, res) => {
+      console.log(req.query)
+spotifyApi
+  .searchArtists(req.query.name)
+  .then(data => {
+    console.log('The received data from the API: ', data.body.artists.items);
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    res.render('artist-search-results', {datos: data.body.artists.items})
+})
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+});
+
+
+app.get('/albums/:id', (req, res) => {
+    spotifyApi.getArtistAlbums(req.params.id)
+  .then(function(data) {
+    console.log('Artist albums', data.body.items);
+    res.render('albums', {datos: data.body.items})
+  }, function(err) {
+    console.error(err);
+  });
+});
+
+app.get('/tracks/:id', (req, res) => {
+    spotifyApi.getAlbumTracks(req.params.id)
+    .then(function(data) {
+      console.log(data.body);
+      res.render('tracks', {datos: data.body.items})
+    }, function(err) {
+      console.log('Something went wrong!', err);
+    });
+  });
+
+
+
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
