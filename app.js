c5c89928028765ba6dc0ce19645c70252b4443c4
Nina van den Braak <ninavandenbraak@gmail.com>
2020-11-06 15:08:56 +0100

    forgot some catches and semicolons

--- app.js
+++ app.js
@@ -23,10 +23,9 @@ spotifyApi
     .then(data => spotifyApi.setAccessToken(data.body['access_token']))
     .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
-
 app.get(`/`, (req, res) => {
     res.render(`index`)
-})
+});
 
 app.get(`/artist-search`, (req, res, next) => {
     spotifyApi
@@ -36,8 +35,8 @@ app.get(`/artist-search`, (req, res, next) => {
                 data
             })
         })
-        .catch(err => console.log('The error while searching artists occurred: ', err))
-})
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+});
 
 app.get('/albums/:artistID', (req, res, next) => {
     spotifyApi.getArtistAlbums((req.params).artistID)
@@ -46,28 +45,27 @@ app.get('/albums/:artistID', (req, res, next) => {
                 data
             })
         })
-})
+        .catch(err => console.log('An error occured: ', err));
+});
 
 app.get('/view-tracks/:album', (req, res, next) => {
     spotifyApi.getAlbumTracks((req.params).album)
         .then(data => {
-            res.render(`view-tracks`, {
+            res.render(`view-album-tracks`, {
                 data
             })
         })
-})
+        .catch(err => console.log('An error occured: ', err));
+});
 
 app.get('/top-tracks/:artistID', (req, res, next) => {
     spotifyApi.getArtistTopTracks((req.params).artistID, "NL")
         .then(data => {
-            console.log(data)
             res.render(`top-tracks`, {
                 data
             })
         })
-})
-
-
-
+        .catch(err => console.log('An error occured: ', err));
+});
 
 app.listen(PORT, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
