ac5874b639a3ea335e92005949a0e0c1dd09ab8e
teof95 <teof95@gmail.com>
2020-11-06 16:57:43 +0100

    not complete. complete till iteration 4. no styling added yet

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
+
 
 const app = express();
 
@@ -12,7 +14,52 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+});
+
+// Retrieve an access token
+spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+app.get('/', (req, res) => {
+    res.render("index")
+})
+
+
+app.get('/artists-search', (req, res, next) => {
+    spotifyApi
+        .searchArtists(req.query.artists)
+        .then(data => {
+            console.log('Artist information', data.body.artists.items);
+            res.render('artists-search-results', { data: data.body.artists.items });
+            console.log("The received data from the API: ", data.body.artists)
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err))
+
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+
+})
+
+app.get('/albums/:id', (req, res) => {
+    const { id } = req.params;
+    spotifyApi
+        .getArtistAlbums(id)
+        .then((data) => {
+            const { items } = data.body;
+            res.render("albums", { albums: items });
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
+
+console.log("this worked")
+
+
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
