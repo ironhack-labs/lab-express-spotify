b2e17a63e52303cc6fa8762c47c3b86091eae3a0
sarastackhouse <sarapalacios7@gmail.com>
2020-11-12 23:37:24 +0100

    home button fixedCh

--- app.js
+++ app.js
@@ -6,14 +6,11 @@ const hbs           = require('hbs');
 // require spotify-web-api-node package here:
 const SpotifyWebApi = require('spotify-web-api-node');
 
-
 const app = express();
-
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
-
 
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
@@ -21,17 +18,13 @@ const spotifyApi = new SpotifyWebApi({
     clientSecret: process.env.CLIENT_SECRET
   });
   
-  // Retrieve an access token
-  spotifyApi
-    .clientCredentialsGrant()
-    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
-    .catch(error => console.log('Something went wrong when retrieving an access token', error));
-
-
-
+// Retrieve an access token
+spotifyApi
+.clientCredentialsGrant()
+.then(data => spotifyApi.setAccessToken(data.body['access_token']))
+.catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
-
 app.get('/', (req, res, next)=>{
     res.render('home');
 });
@@ -50,12 +43,28 @@ app.get('/artist-search', (req, res, next)=>{
 })
 
 
-app.get('/albums/:artistId', (req, res, next)=>{
+app.get('/albums/:id', (req, res, next)=>{
     spotifyApi
-    .getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', { limit: 10, offset: 20 })
+    .getArtistAlbums((req.params.id))
     .then(
       function(data) {
         console.log('Album information', data.body);
+      },
+      function(err) {
+        console.error(err);
+      }
+    );
+})
+
+
+app.get('/view-tracks/:id', (req, res, next)=>{
+    spotifyApi
+    .getAlbumTracks((req.params.id))
+    .then(
+      function(data) {
+        res.render('albums', { albums: albums.body.items })
+
+        console.log('Track information', data.body);
       },
       function(err) {
         console.error(err);
