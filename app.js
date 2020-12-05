f8e45fd44bf1b25b9adc541c0d10dd8480712c22
Mina Samwail <minasamwail@MacBook-Air-de-Mina.local>
2020-12-05 19:17:31 +0100

    iteration done 1 to 3

--- app.js
+++ app.js
@@ -1,18 +1,54 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
 
 // require spotify-web-api-node package here:
-
-const app = express();
-
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+const SpotifyWebApi = require("spotify-web-api-node");
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
+
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+  .catch((error) =>
+    console.log("Something went wrong when retrieving an access token", error)
+  );
 
 // Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+//route Home
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+// route to get the artist from the Form search
+app.get("/artist-search", (req, res) => {
+  //   console.log(req.query.artist);
+  spotifyApi
+    .searchArtists(req.query.artist)
+    .then((data) => {
+      console.log("The received data from the API: ", data.body);
+
+      const database = data.body.artists.items;
+      //   console.log("The API send those info", database);
+      res.render("artist-search-results", { database });
+    })
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
+});
+
+app.listen(3000, () =>
+  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+);
