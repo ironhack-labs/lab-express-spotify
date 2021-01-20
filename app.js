3fe9ae02125038904dee1e3e46d3f94e0cadb327
Jose Luis Rodriguez <j_cerra@hotmail.com>
2021-01-20 21:36:22 +0100

    iteration 3 done

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
+const SpotifyWebApi = require('spotify-web-api-node');
 
 // require spotify-web-api-node package here:
+
+
 
 const app = express();
 
@@ -12,7 +15,15 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
 
+  
 // Our routes go here:
+app.get('/', (req, res, next) => {
+  res.render('index')
+})
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.listen(3001, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
