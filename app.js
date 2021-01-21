115d0a5746e22b8b83bc39e48bc22b18a68c514f
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-01-22 00:33:35 +0100

    2

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const hbs = require('hbs');
 const SpotifyWebApi = require('spotify-web-api-node');
-
 // require spotify-web-api-node package here:
 
 
@@ -13,6 +12,7 @@ const app = express();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials')
 
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
@@ -20,10 +20,34 @@ const spotifyApi = new SpotifyWebApi({
     clientSecret: process.env.CLIENT_SECRET
   });
 
+  spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
   
 // Our routes go here:
 app.get('/', (req, res, next) => {
   res.render('index')
+
 })
 
-app.listen(3001, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+
+app.get('/search-artist', (req, res, next) => {
+  console.log(req.query.name)
+  spotifyApi
+  .searchArtists(req.query.name)
+  .then(data => {
+    console.log(data.body);
+    if (data.body.artists.total == 0) {
+      res.render('no-results')
+  } else {
+      console.log(data.body.artists.items)
+      res.render('artist-search-results', { results: data.body.artists})
+  }
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+
+})
+
+app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
