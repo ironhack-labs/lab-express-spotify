27a225249911fc5a2fdc54d27971317b2794956e
Thiarhes <injesusservice@hotmail.com>
2021-01-24 20:36:56 -0300

    Done... CSS only aplies in the index page, don't want to apply anymore

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
+
+
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
@@ -29,14 +31,39 @@ app.get('/', (req, res, next) => {
     res.render('index');
 })
 
-app.get('/artist-search', (req, res, next) => {
+app.get('/artist', (req, res, next) => {
+    const search = req.query.artist;
     spotifyApi
-        .searchArtists(req.query.artist)
+        .searchArtists(search)
         .then(data => {
-            console.log('The received data from the API: ', data.body.artists.items);
-            res.render('artist-search-results', data.body)
+            // console.log('The received data from the API: ', data.body.artists.items);
+            const artists = data.body.artists.items;
+            res.render('artist', { artists });
         })
         .catch(error => console.log('The error while searching artists occurred: ', error));
 });
+
+app.get('/album/:artistId', (req, res, next) => {
+    const album = req.params.artistId;
+    spotifyApi
+        .getArtistAlbums(album)
+        .then(data => {
+            const albums = data.body.items;
+            res.render('album', { albums });
+        })
+        .catch(error => console.log('The error while searching albums occurred: ', error));
+})
+
+app.get('/track/:albumId', (req, res, next) => {
+    const track = req.params.albumId;
+    spotifyApi
+        .getAlbumTracks(track)
+        .then(data => {
+            const tracks = data.body.items;
+            res.render('track-information', { tracks });
+        })
+        .catch(error => console.log(error));
+
+})
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
