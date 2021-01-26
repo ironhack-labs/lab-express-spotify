6a6c272969d3a91097a39bd34d69644834f33a72
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-01-26 20:12:12 +0100

    done

--- app.js
+++ app.js
@@ -16,16 +16,16 @@ hbs.registerPartials(__dirname + '/views/partials')
 
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-  });
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
 
-  spotifyApi
+spotifyApi
   .clientCredentialsGrant()
   .then(data => spotifyApi.setAccessToken(data.body['access_token']))
   .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
-  
+
 // Our routes go here:
 app.get('/', (req, res, next) => {
   res.render('index')
@@ -35,16 +35,52 @@ app.get('/', (req, res, next) => {
 
 app.get('/search-artist', (req, res, next) => {
   spotifyApi
-  .searchArtists(req.query.name)
-  .then(data => {
-    if (data.body.artists.total == 0) {
-      res.render('no-results')
-  } else {
-      res.render('artist-search-results', { results: data.body.artists})
-  }
-  })
-  .catch(err => console.log('The error while searching artists occurred: ', err));
+    .searchArtists(req.query.name)
+    .then(data => {
+      // console.log(data.body.artists)
+      if (data.body.artists.total == 0) {
+        res.render('no-results')
+      } else {
+        res.render('artist-search-results', { results: data.body.artists })
+      }
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
 
 })
 
+app.get('/albums/:name/:artistId', (req, res, next) => {
+  let id = req.params.artistId;
+  spotifyApi
+    .getArtistAlbums(id)
+    .then(data => {
+      if (data.body.items == 0) {
+        res.render('no-results')
+      } else {        
+        res.render('albums', { results: data.body, name: req.params.name, id:id})
+      }
+    })
+    .catch(err => console.log('Search error: ', err));
+})
+
+app.get('/tracks/:id', (req, res, next) => {
+  console.log(req.params.id);
+  spotifyApi
+    .getAlbumTracks(req.params.id)
+    .then(data => {
+      console.log(data.body)
+      if (data.body.items == 0) {
+        res.render('no-results')
+      } else {        
+        res.render('tracks', {results: data.body})
+      }
+    })
+    .catch(err => console.log('Search error: ', err));
+})
+
+app.use((req, res, next) => {
+  res.status(400)
+  res.render('error')
+})
+
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+
