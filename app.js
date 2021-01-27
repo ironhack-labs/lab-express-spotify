570326b044f46e27d6502f6b86bfba85c7e6f0b0
Pedro Frias <plfriasf@gmail.com>
2021-01-27 06:19:47 -0800

    Iteration #1 done

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
-
+const  SpotifyWebApi  =  require ( 'spotify-web-api-node' ) ;
 const app = express();
 
 app.set('view engine', 'hbs');
@@ -12,6 +12,18 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: proceso.env.CLIENT_ID,
+  clientSecret: proceso.env.CLIENT_SECRET,
+});
+
+// Recupera un token de acceso
+spotifyApi
+  .clientCredentialsGrant()
+  .luego((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+  .catch((error) =>
+    console.log("Algo salió mal al recuperar un token de acceso", error)
+  );
 
 // Our routes go here:
 
