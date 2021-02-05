b408f5b36569e002ad3b13eae92c45593ba7a6e3
honghainguyen777 <n.h.hai2015@gmail.com>
2021-02-05 16:29:41 +0100

    view albums

--- app.js
+++ app.js
@@ -34,16 +34,35 @@ app.get('/artist-search', (req, res) => {
         .searchArtists(req.query.search.toLowerCase())
         .then(resData => {
             let artists = resData.body.artists.items;
-            // replace artist without image
             artists = artists.map(artist => {
-                if (artist.images.length === 0) artist.images = {url : "/images/default.png"};
+                // get artist uri code only
+                artist.uri = artist.uri.split(":")[2];
+                // replace artist without image,
+                if (artist.images.length === 0) artist.images = {url : "/images/default_artist.png"};
                 else artist.images = artist.images[0];
                 return artist;
             });
-            console.log(artists);
             res.render('artist-search-results', {artists: artists})
         })
         .catch(error => console.log(error))
+})
+
+app.get('/albums/:artistId', (req, res) => {
+    spotifyApi
+        .getArtistAlbums(req.params.artistId)
+        .then(resData => {
+            let albums = resData.body.items;
+            albums = albums.map(album => {
+                // get album uri code only
+                album.uri = album.uri.split(":")[2];
+                // replace album without image,
+                if (album.images.length === 0) album.images = {url : "/images/default_album.png"};
+                else album.images = album.images[0];
+                return album;
+            });
+            res.render('albums', {albums});
+        })
+        .catch(error => console.log(error));
 })
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
