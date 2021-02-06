4cd540c21f9629218438635cf3dccd56c566967d
mcapurri <m.capurri@gmail.com>
2021-02-06 18:43:32 +0100

    under construction

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-require('dotenv').config();
+// Load env vars
+require('dotenv').config({ path: './config/config.env' });
 
 const express = require('express');
 const hbs = require('hbs');
 
-// require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -11,8 +12,51 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-// setting the spotify-api goes here:
+// Spotify credentials
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+});
 
-// Our routes go here:
+// Retrieve an access token
+spotifyApi
+    .clientCredentialsGrant()
+    .then((data) => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch((error) =>
+        console.log(
+            'Something went wrong when retrieving an access token',
+            error
+        )
+    );
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+// ROUTES
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/artist-search', (req, res) => {
+    spotifyApi
+        .searchArtists(req.query.search)
+        .then((artists) => {
+            res.render('artist-search-results', {
+                artists: artists.body.artists.items,
+            });
+        })
+        .catch((err) => console.log(err));
+});
+
+app.get('/albums/:artistId', (req, res, next) => {
+    spotifyApi.getArtistAlbums(req.params.artistId).then((albums) => {
+        res.render('albums', { albums: albums.body.items });
+    });
+});
+
+app.get('/tracks/:albumId', (req, res, next) => {
+    spotifyApi.getAlbumTracks(req.params.albumId).then((tracks) => {
+        console.log('tracks', tracks.body);
+    });
+});
+const PORT = 3609;
+app.listen(PORT, () =>
+    console.log(`My Spotify project running on port: ${PORT}`)
+);
