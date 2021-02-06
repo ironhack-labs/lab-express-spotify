a743c576c3161ed7bbee828eac1ad3ee36fce39d
Viviana Pulido <vivianapulido_dg@outlook.com>
2021-02-06 16:00:29 -0600

    listo

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -13,6 +14,61 @@ app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
 
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
 // Our routes go here:
+
+app.get('/', (req, res, next)=>{
+    res.render('home')
+})
+
+app.get('/artists-search', async(req, res, next)=>{
+    const data= req.query.artist //en esta variable se guarda lo que el usuario pone en el buscador
+    const datosApiRespuesta= await spotifyApi.searchArtists(data) //aqui se le aplica el metodo de spotify y nos regresa la info de la api
+  const extractedInfo= await datosApiRespuesta.body.artists.items.map((elem)=>({ //como lo que hay dentro de items es un array, aplicamos un map para que nos de un nuevo
+      name: elem.name,
+      image: elem.images[0],
+      id: elem.id
+  }))
+  //console.log(datosApiRespuesta.body.artists.items[0].images[0].url)
+  res.render(('artistsearchresults'), {extractedInfo})
+    })
+
+app.get('/albums/:id', async(req, res, next)=>{
+    const idDelArtistaSeleccionado= req.params.id
+    const albums= await spotifyApi.getArtistAlbums(idDelArtistaSeleccionado)
+    //console.log("ahora empieza aqui",albumes.body.items[0])
+    const albumsInfo= albums.body.items.map((elem)=>({
+        image: elem.images[0],
+        name: elem.name,
+        id: elem.id
+    }))  
+    res.render(('albums'), {albumsInfo})
+})    
+     
+
+app.get('/tracks/:id', async(req, res, next)=>{
+    const idDelAlbumSeleccionado= req.params.id
+    const tracks= await spotifyApi.getAlbumTracks(idDelAlbumSeleccionado)
+    //console.log("desdeaqui",tracks.body.items[0])
+    const tracksInfo= tracks.body.items.map((elem)=>({
+        name: elem.name,
+        preview: elem.preview_url
+    }))
+    res.render(("tracks"), {tracksInfo})
+
+})
+
+
+ //LEVANTAMIENTO DEL SERVER   
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
