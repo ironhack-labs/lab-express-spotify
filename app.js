f2d4a7f0c611a627ece1016f8982b934a4861de2
Daniela Alonzo <monsterjunior@outlook.es>
2021-02-05 22:53:10 -0600

    working

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -13,6 +12,39 @@ app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
 
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
 // Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.get('/', (req, res, next) =>{
+
+    const search = req.query.search
+    res.render("home", {
+    })
+})
+
+app.get('/search-artist', async(req, res, next)=>{
+    const data = req.query.artist
+    const datosApi = await spotifyApi.searchArtists(data)
+    console.log(datosApi.body.artists.items)
+    res.render('artistsearchresults', datosApi.body.artists.items)
+})
+
+app.get('/search-albums/:artistId', async(req, res, next)=>{
+    const dataid = req.query.album
+    const datosApiAlbum = await spotifyApi.getArtistAlbums(dataid)
+    console.log(datosApiAlbum.body.album)
+    res.render('albumsinformation', datosApiAlbum.body.album)
+})
+
+
+app.listen(3001, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
