495b4c1b74469709b7be2f2cd1be188ac676bdcd
Hugo Copca <copcal79@gmail.com>
2021-02-06 18:22:23 -0600

    404

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 
 const express = require('express');
+const { registerPartial } = require('hbs');
 const hbs = require('hbs');
 
-// require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node')
 
 const app = express();
 
@@ -12,7 +13,38 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log(error));
 
 // Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.get('/', (req, res, next) => {
+    res.render('home')
+})
+
+app.get('/artist-search', async(req, res, next) => {
+    const data = req.query.artist
+    const datosApiRespuesta = await spotifyApi.searchArtists(data).catch(err => console.log(err))
+    console.log(datosApiRespuesta.body.artists.items)
+    res.render('artistsearchresults', datosApiRespuesta.body.artists.items)
+})
+
+app.get('/tracks/:id', async(req, res, next) => {
+    const { id } = req.params
+    const canciones = await spotifyApi.getAlbumTracks(id).catch(err => console.log(err))
+    console.log(canciones)
+})
+
+
+
+
+
+app.listen(3000, () => console.log('My Spotify project running on port 3001 🎧 🥁 🎸 🔊'));
