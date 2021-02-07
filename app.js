45ecede60fec6339ed5237448e8702d4e03b758c
Diego <diego.arronizt@gmail.com>
2021-02-07 02:31:35 -0500

    casi listo, hay que revisar porque bota con el error invalid ID 400

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
-
+const SpotifyWebApi =require('spotify-web-api-node')
 const app = express();
 
 app.set('view engine', 'hbs');
@@ -12,7 +12,45 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
-
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 // Our routes go here:
 
+app.get('/', (req,res,next)=>{
+    res.render('index')
+})
+
+app.get('/artist-search', async(req,res,next) => {
+  let   clientPageSearch = req.query.artistSearch
+  let   artistSearched =  await spotifyApi.searchArtists(clientPageSearch)
+
+    res.render('artist-search-result',{artistSearched})
+   //console.log(artistSearched.body.artists.items[0])
+})
+
+app.get("/albums/:artistId",async (req,res,next)=>{
+    let aId = String(req.params.artistId)
+    let albumCheck = await spotifyApi.getArtistAlbums(aId)
+    
+    //console.log(albumCheck.body.items[0])
+    res.render("albums",{albumCheck})
+})
+
+app.get("/tracks/:albumId", async(req,res, next)=> {
+    let album = await String(req.params.albumId)
+    let trackCheck = await spotifyApi.getAlbumTracks(album)
+    console.log(trackCheck.body.items[0])
+    res.render("tracks", {trackCheck})
+})
+/*${artistClick}*/
+//'23wEWD21D4TPYiJugoXmYb'
+//server
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
