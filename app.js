207a0fa4863e7642a9f29629063a7c8b020c667f
cintiathieme <cintiathieme@gmail.com>
2021-03-16 18:26:57 -0300

    interations 1-5

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -12,7 +13,53 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+
+app.get('/', (req,res) => {
+    res.render('home')
+});
+
+app.get('/artist-search', (req, res) => {
+    const {artistName} = req.query;
+    spotifyApi
+  .searchArtists(artistName)
+  .then(data => {
+    console.log('The received data from the API: ', data.body);
+    res.render('artist-search-results', {results: data.body.artists.items}) 
+
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
+app.get('/albums/:artistId', (req, res) => {
+    const {artistId} = req.params;
+    spotifyApi
+    .getArtistAlbums(artistId)
+    .then(data => {
+        res.render('albums', {albums: data.body.items})
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
+app.get('/tracks/:tracksId', (req,res) => {
+    const {tracksId} = req.params;
+    spotifyApi
+    .getAlbumTracks(tracksId)
+    .then(data => {
+        res.render('tracks', {track: data.body.items})
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
+})
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
