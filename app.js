30c9d6192960359f3711d0f4bbc75071eaa28b90
Bclark <brittneyclark39@gmail.com>
2021-03-17 14:00:46 +0100

    nodemone crash error? but kinda done

--- app.js
+++ app.js
@@ -1,18 +1,68 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
-
+const express = require("express");
+const hbs = require("hbs");
+const SpotifyWebAPI = require("spotify-web-api-node");
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+  .catch((error) =>
+    console.log("Something went wrong when retrieving an access token", error)
+  );
 
 // setting the spotify-api goes here:
 
 // Our routes go here:
+app.get("/", (req, res) => {
+  res.render("home-page");
+});
+app.get("/artist-search", (req, res) => {
+  spotifyApi
+    .searchArtists(req.query.artist)
+    .then((data) => {
+      console.log("The received data from the API", data.body);
+      res.render("artist-search-results", { artists: data.body.items });
+    })
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
+});
+app.get("/albums/:artistId", (req, res) => {
+  const artistId = req.params.artistId;
+  spotifyApi
+    .getArtistAlbums(artistId)
+    .then((albumsReturned) => {
+      res.render("albums", { albumsReturned: albumsReturned.body.items });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.get("/tracks/:trackId", (req, res) => {
+  const trackId = req.params.trackId;
+  spotifyApi
+    .getAlbumTracks(trackId)
+    .then((tracksReturned) => {
+      console.log(tracksReturned.body.items);
+      res.render("tracks", { tracksReturned: tracksReturned.body.items });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.listen(3000, () =>
+  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+);
