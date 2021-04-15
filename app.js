98bb6154da11589bb3338798c3f273014aa6bc67
PabloCabreraR <pablocabrera2g@gmail.com>
2021-04-15 17:42:54 +0200

    Lab completed

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node')
 
 const app = express();
 
@@ -12,7 +13,55 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+app.get('/', (req, res)=>{
+    res.render('home')
+})
+
+app.get('/artist-search', (req, res)=>{
+    spotifyApi
+        .searchArtists(req.query.artist)
+        .then(data => {
+            // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+            
+            res.render('artist-search-results', {artists: data.body.artists.items})
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
+app.get('/albums/:id/:artistName', (req, res)=>{
+    spotifyApi
+        .getArtistAlbums(req.params.id)
+        .then((result)=>{ 
+            const data =  {artistName: req.params.name, albums: result.body.items}
+            res.render('albums-search-results', data)
+        })
+        .catch(err => console.log('The error while looking for this artist albums occurred: ', err));
+})
+
+app.get('/view-tracks/:id', (req, res)=>{
+    spotifyApi
+        .getAlbumTracks(req.params.id)
+        .then((result)=>{
+            res.render('tracks', {tracks: result.body.items})
+        })
+        .catch(err => console.log('The error while looking for this album tracks occurred: ', err));
+
+})
+
+
+
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
