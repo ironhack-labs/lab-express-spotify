476f06e6e6bbaadf9f0c76f895d16b49b3ecccd1
Raquel Rodriguez <srtademaciel@gmail.com>
2021-04-15 16:10:06 +0200

    iteration 4

--- app.js
+++ app.js
@@ -16,13 +16,53 @@ app.use(express.static(__dirname + '/public'));
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
-  });
-  
-  // Retrieve an access token
-  spotifyApi
+});
+
+// Retrieve an access token
+spotifyApi
     .clientCredentialsGrant()
     .then(data => spotifyApi.setAccessToken(data.body['access_token']))
     .catch(error => console.log('Something went wrong when retrieving an access token', error));
 // Our routes go here:
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/artist-search', (req, res) => {
+    spotifyApi
+        .searchArtists(req.query.artist)
+        .then((data) => {
+            /* console.log(data.body.artists.items) */
+            const artists = {
+                artistItems: data.body.artists.items
+            }
+            res.render('artist-search-results', artists)
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
+/* app.get('/albums/:artistId', (req, res) => {
+
+    spotifyApi
+    .getArtistAlbums(req.params)
+        .then((data) => {
+            const artistAlbums = {artistsAlbums: data.body.items}
+                res.render('albums', { albums: data.body.items })
+            })
+            .catch((err) =>{ 
+                console.log(err)
+            })
+}) 
+ */
+app.get('/albums/:artistId', (req, res) => {
+     const { artistId } = req.params;
+  spotifyApi
+    .getArtistAlbums(artistId)
+    .then((data) => {
+      res.render('albums', { albums: data.body.items });
+    })
+    .catch((err) => console.log('Error rendering albums ==> ', err));
+});
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
