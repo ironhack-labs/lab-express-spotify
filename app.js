2afdc78fcb36dacd119aa7b3b1ca827a7357422f
Tigran Harutyunyan <tigrandeboris@outlook.com>
2021-04-16 17:10:33 +0200

    ADD some modifications

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get("/artist-search", (req, res) => {
     .then(data => {
         console.log('The received data from the API: ', data.body);
         // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
-        res.render('artist-search-result', {resuls: data.body.artists.items});
+        res.render('artist-search-results', {results: data.body.artists.items});
     })
     .catch(err => console.log('The error while searching artists occurred: ', err));
 
