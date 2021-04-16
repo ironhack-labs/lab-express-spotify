779e2baffba4168a817e6b1f358e9abe0bab8f5a
Esther Perez Serrajordi <estherperezserrajordi@MacBook-Pro-de-Esther.local>
2021-04-16 14:49:05 +0200

    Initial commit - Iterations 1 and 2

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ require('dotenv').config();
 const express = require('express');
 const hbs = require('hbs');
 
-// require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 const app = express();
 
