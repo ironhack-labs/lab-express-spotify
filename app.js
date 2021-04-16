150aa2cbafd004e5dac1267effa97d7ba1dbc63c
Susana Prado <susana.prado01@gmail.com>
2021-04-16 15:06:03 +0200

    iteration2

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 const express = require("express");
 const hbs = require("hbs");
-const mongoose = require("mongoose")
 const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
