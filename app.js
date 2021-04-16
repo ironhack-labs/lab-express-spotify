574e9b72fc6c2600957261c30bee4eafd8c9e5a8
Bea Avis <bea.avis.p@gmail.com>
2021-04-16 15:45:00 +0200

    Done iteration 3

--- app.js
+++ app.js
@@ -24,5 +24,18 @@ const spotifyApi = new SpotifyWebApi({
     .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.get('/artist-search', (req, res) => {
+    spotifyApi
+    .searchArtists(req.query.artist)
+    .then(data => {
+        console.log('The received data from the API: ', data.body.artists.items);
+        res.render('artist-search-results', data.body);
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
+})
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
