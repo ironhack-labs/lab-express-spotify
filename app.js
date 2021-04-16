918360c94377506c0803190e5dec8daf403ff157
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-04-16 19:37:42 +0200

    clean up app.js

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const spotifyApi = new SpotifyWebApi({
 
 
 
-// require spotify-web-api-node package here:
 
 const app = express();
 
@@ -18,12 +17,10 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-// setting the spotify-api goes here:
 spotifyApi
   .clientCredentialsGrant()
   .then(data => spotifyApi.setAccessToken(data.body['access_token']))
   .catch(error => console.log('Something went wrong when retrieving an access token', error));
-// Our routes go here:
 
 
 app.get('/', (req,res)=>{
@@ -35,35 +32,28 @@ app.get('/artist-search',(req,res)=>{
     spotifyApi
   .searchArtists(req.query.q)
   .then(data => {
-      console.log(req.query.q);
     console.log(data.body);
     const searchedMusic = data.body;
-    console.log(searchedMusic.artists.items[0])
     res.render('music', {searchedMusic});
   })
   .catch(err => console.log('error while searching artists occurred: ', err));
-    // res.render('music', {searchedMusic})
 })
 
 app.get('/albums/:artist', (req, res, next)=>{
-    console.log(req.params.artist.substr(15));
     spotifyApi
     .getArtistAlbums(req.params.artist.substr(15))
     .then(data => {
         const albumData = data.body;
-        console.log(albumData);
         res.render('albums', {albumData});
     })
     .catch(err=> console.log('error while returning albums: ', err))
 })
 
 app.get('/tracks/:album', (req,res)=>{
-    console.log(req.params.album.substr(14));
     spotifyApi
     .getAlbumTracks(req.params.album.substr(14))
     .then(data => {
         const trackData = data.body;
-        console.log(trackData);
         res.render('tracks', {trackData});
     })
     .catch(err=> console.log('error retrieving track info', err))
@@ -73,4 +63,3 @@ app.get('/tracks/:album', (req,res)=>{
 app.listen(3000, () => console.log('😠🎸 port 3000 '));
 
  
-//change
