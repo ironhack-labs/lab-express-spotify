42deb8b6dad6a70dbb299c3a8db22a37d6b9cdb8
Bea Avis <bea.avis.p@gmail.com>
2021-04-16 14:41:41 +0200

    Done iteration 2

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
-
 // require spotify-web-api-node package here:
 const SpotifyWebApi = require('spotify-web-api-node');
 
