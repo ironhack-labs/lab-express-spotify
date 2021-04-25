00c9de852d77da098a40e6acf3448aaa8d930c27
Luciano Colabraro <luciano.colabraro@gmail.com>
2021-04-25 11:30:54 +0200

    done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const { query } = require("express");
 const express = require("express");
 const hbs = require("hbs");
 
@@ -17,13 +18,7 @@ spotifyApi
   .catch((error) =>
     console.log("Something went wrong when retrieving an access token", error)
   );
-//   spotifyApi
-//   .searchArtists(/artist-search)
-//   .then(data => {
-//     console.log('The received data from the API: ', data.body);
-//     // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
-//   })
-//   .catch(err => console.log('The error while searching artists occurred: ', err));
+//
 
 const app = express();
 
@@ -35,22 +30,69 @@ app.use(express.static(__dirname + "/public"));
 
 // Our routes go here:
 
-// create the routes of the search
-// app.get("/artist-search", (req, res) => {
-//   spotifyApi
-//     .searchArtists(artist - search)
-//     .then((data) => {
-//       console.log("The received data from the API: ", data.body);
-//       // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
-//     })
-//     .catch((err) =>
-//       console.log("The error while searching artists occurred: ", err)
-//     );
-//   res.render("artist-search-results");
-// });
+// Route to render the home page
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+// Route to search and display searched artist. From artist-search URL, search for artist, move to render page where showing result.
+// Search Query has to be the same used in get form, input name.
+
+app.get("/artist-search", (req, res) => {
+  console.log("Searching artists");
+  // console.log(req.query);
+  let artistSearch = req.query.name; // add the query to a variable (name is equal to name used in the form)
+
+  spotifyApi
+    .searchArtists(artistSearch) // apply the variable artistSearch
+    .then((data) => {
+      let results = data.body.artists.items; // save the database info into a variable results
+      console.log("The received data from the API: ", data.body);
+      res.render("artist-search-results", { results });
+    })
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
+});
+
+// Crate get route for search for albums.
+
+app.get("/albums/:artistId", (req, res, next) => {
+  console.log("Searching albums");
+  const artistId = req.params.artistId; // put artistID into a variable
+
+  spotifyApi
+    .getArtistAlbums(artistId) // id variable
+    .then((data) => {
+      const albumResults = data.body.items; // save into a variable the database info
+      console.log("The received data from the API: ", data.body);
+      res.render("albums", { albums: albumResults }); // albums will be calle din the albumd.hbs to display name etc
+    })
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
+});
+
+// create get route with albumID(used in the link view track on albums page).
+// save to a variable named albumId the value of albumid using req.params
+// use spotifyApi method, save to a variable the info database
+//  use tracks to display info on tracks.hbs view
+
+app.get("/tracks/:albumId", (req, res, next) => {
+  console.log("Searching tracks");
+  const albumId = req.params.albumId;
+
+  spotifyApi
+    .getAlbumTracks(albumId)
+    .then((data) => {
+      const trackResult = data.body.items;
+      console.log("The received data from the API: ", data.body);
+      res.render("tracks", { tracks: trackResult });
+    })
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
 });
 
 app.listen(3000, () =>
