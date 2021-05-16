1768bf4b9864484c5281989864287de117ee04eb
Mohamud Issa <mohamud.issa@ymail.com>
2021-05-16 22:19:57 +0200

    small changes in the index.js file

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
-const SpotifyWebApi = require('spotify-web-api-node');
 const album = require('./routes/album');
 const artist = require('./routes/artist');
 const track = require('./routes/track');
@@ -12,23 +9,9 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
 app.use('/artist', artist);
 app.use('/album', album);
 app.use('/track', track);
-
-const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-});
-
-// Retrieve an access token
-spotifyApi
-  .clientCredentialsGrant()
-  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
-  .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 app.get("/", (req, res) => res.render("index"))
 
