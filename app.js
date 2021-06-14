edc49f2a0b5e298ad8207ec13a1e3224feaa1911
Grecia Patricia <grecia.busquets@gmail.com>
2021-06-14 21:40:25 +0200

    Stopped at Displaying the results for artist search - IT 3 step 2

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
-const SpotifyWebApi = require('spotify-web-api-node');
+const express = require("express");
+const hbs = require("hbs");
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
 
 const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
+
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+
+  .catch((error) =>
+    console.log("Something went wrong when retrieving an access token", error)
+  );
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+ 
+
+app.get("/artist-search", (req, res) => {
+    spotifyApi
+  .searchArtists("spotify:local:{artist}")
+  .then(data => {
+    console.log('The received data from the API: ', data.body);
+    res.render("artist-search-results");
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+    
   });
-  
-  // Retrieve an access token
-  spotifyApi
-    .clientCredentialsGrant()
-    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
-    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
-// Our routes go here:
-
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.listen(3000, () =>
+  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+);
