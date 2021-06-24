c02ea3fa6104745aa1d72db9bcac570be550d42f
javierceballosaltuna <javierceballosaltuna@gmail.com>
2021-06-24 18:07:41 +0200

    not finished

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 
+
+
 const express = require('express');
 const hbs = require('hbs');
 
-// require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');// require spotify-web-api-node package here:
 
 const app = express();
 
@@ -11,7 +13,62 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-// setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));// setting the spotify-api goes here:
+
+
+app.get('/', (req, res) => {
+
+    res.render('index')
+})
+
+app.get('/artist-search', (req, res) => {
+
+console.log ('heloooooooo')
+    const artistName = req.query.artistName
+    // const {artistName} = req.query
+
+    spotifyApi
+        .searchArtists(artistName)
+        .then(data => {
+            console.log(data.body)
+
+            res.render('artist-search-result', { artist: data.body.artists.items });
+            
+            // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err))
+
+})
+
+
+app.get('/albums/:artistId', (req, res, next) => {
+    
+const artistID = req.query.artistID
+
+    spotifyApi
+    
+    .getArtistAlbums(artistID)
+    .then(
+        (data) => {
+            
+            res.render('albums', {albums : data.body.artistID});
+        },
+        (err) => {         console.error(err);
+        }
+    );
+    
+    // .getArtistAlbums() code goes here
+});
+
 
 // Our routes go here:
 
