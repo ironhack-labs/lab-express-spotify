7d9c135039021136da31996c76f760385d483215
Bart Spangenberg <bart_spangenberg@protonmail.com>
2021-06-29 13:51:35 +0200

    Spotify lab, iteration 5 only partially

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 
+const SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
 
 const app = express();
 
@@ -14,5 +13,11 @@ app.use(express.static(__dirname + '/public'));
 // setting the spotify-api goes here:
 
 // Our routes go here:
+const index = require('./routes/index.js')
+app.use('/', index);
+
+const artistRoutes = require('./routes/artist.routes')
+app.use('/', artistRoutes)
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
