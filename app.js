2e703834d4bcee92d49b7ae9680b1a28c9b98080
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-09-02 00:46:44 +0200

    iteration 3.1

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 
-const express = require('express');
-const hbs = require('hbs');
-
+// Iteracion #2
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
 
@@ -11,8 +13,43 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-// setting the spotify-api goes here:
+// partials
+//hbs.registerPartials(`${__dirname}/views/partials`);
 
+// Iteración #1
+// setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+	clientId: process.env.CLIENT_ID,
+	clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+	.clientCredentialsGrant()
+	.then((data) => spotifyApi.setAccessToken(data.body['access_token']))
+	.catch((error) => console.log('Something went wrong when retrieving an access token', error));
+
+// Iteración #3
 // Our routes go here:
+// Index route
+app.get('/', (req, res) => {
+	res.render('index');
+	//console.log('Hi');
+	//console.log(__dirname);
+});
+
+// Artist route
+// 3h he estado haciendo el mongoloDB hasta que he visto que el require de la línia #1 estaba comentada...
+app.get('/artist-search', (req, res) => {
+	spotifyApi
+		.searchArtists(req.query.artist)
+		.then((data) => {
+			//console.log('The received data from the API: ', data.body.artists.items);
+			res.render('artist-search-results', { data: data.body.artists.items });
+			//res.render('artist-search-results', { data });
+			//res.send({ data: data.body.artists.items });
+		})
+		.catch((err) => console.log('The error while searching artists occurred: ', err));
+});
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
