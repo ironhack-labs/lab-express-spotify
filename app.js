6f5d7936feffa8afa44f79f6bbc5a31c867f01d3
Yoel Martin <yoelmartinfornieles@gmail.com>
2021-09-03 00:34:02 +0200

    Features: 3.2 done

--- app.js
+++ app.js
@@ -34,10 +34,40 @@ spotifyApi
 
 app.get('/', (req, res) => {
 	res.render('index');
-  });
+	});
 
 app.get('/artist-search', (req, res) => {
-	res.render('artist-search');
-});
+	//console.log ("request: " + req.query.name)
+  let artistToSearch = req.query.name
+	spotifyApi
+  .searchArtists(artistToSearch)
+  .then(data => {
+/* 	The received data from the API:  {
+		href: 'https://api.spotify.com/v1/search?query=extremoduro&type=artist&offset=0&limit=20',
+		items: [
+		  {
+			external_urls: [Object],
+			followers: [Object],
+			genres: [Array],
+			href: 'https://api.spotify.com/v1/artists/3bgsNtcf5d5h9jbQbohfBK',
+			id: '3bgsNtcf5d5h9jbQbohfBK',
+			images: [Array],
+			name: 'Extremoduro',
+			popularity: 66,
+			type: 'artist',
+			uri: 'spotify:artist:3bgsNtcf5d5h9jbQbohfBK'
+		  },
+...
+		]
+	  } */
+	let artistsArray = data.body.artists.items;
+    console.log('The received data from the API: ', artistsArray);
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+	res.render('artist-search-results', {artistsArray});
+	})
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+	});
+
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
