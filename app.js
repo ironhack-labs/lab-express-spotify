4864dc42712fe5192c71ad3cecc14b90f0a90cff
alejandro <ftvalex@hotmail.com>
2021-09-03 14:02:54 +0100

    Lab completo sin estilos
    Lab funcional a falta de los estilos

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 
+const SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express');
 const hbs = require('hbs');
+const path = require('path');
 
-// require spotify-web-api-node package here:
+
 
 const app = express();
 
@@ -11,8 +13,50 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-// setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
 
 // Our routes go here:
+app.get("/", (req, res) => {
+    res.render("index");
+  });
+
+  app.get('/artist-search', (req, res) => {
+    spotifyApi
+        .searchArtists(req.query.artist)
+        .then((data) => {
+          
+            res.render('artist-search-results', {data: data.body.artists.items});
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+
+})
+
+app.get('/albums/:albumId', (req, res) => {
+    spotifyApi
+        .getArtistAlbums(req.params.albumId)
+        .then((data) => {
+            res.render('albums',{data:data.body.items})
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
+app.get('/tracks/:albumId', (req, res) => {
+    spotifyApi
+        .getAlbumTracks(req.params.albumId)
+        .then((data) => {
+           
+            res.render('tracks',{data:data.body.items})
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
