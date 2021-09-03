7b9b4f17ed4ed25394ce78a9974049825aa40dd3
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-09-03 18:06:29 +0200

    missing styling

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ app.get('/search', (req, res) => {
     console.log(searchPhrase);
     spotifyApi.searchArtists(searchPhrase.toLowerCase())
     .then(data => {
-        console.log('The received data from the API: ', data.body);
-        res.render('artist-search-results', { artistsList: data.body.items })// ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+        console.log('The received data from the API: ', data.body.artists.items);
+        res.render('artist-search-results', { artistsList: data.body.artists.items})// ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
     })
     .catch(err => console.log('The error while searching artists occurred: ', err));
     // const filteredMovies = movies.filter(movie =>
@@ -44,5 +44,37 @@ app.get('/search', (req, res) => {
     // console.log(filteredMovies);
     // res.render('movies', { moviesList: filteredMovies, doctitle: 'Filtered Movies' });
 })
+
+app.get('/albums/:artistId', (req, res) => {
+    console.log(req.params);
+    const searchPhrase = req.params.artistId;
+    console.log(searchPhrase);
+    spotifyApi
+    .getArtistAlbums(searchPhrase)
+    .then(data => {
+        console.log('The received data from the API albums page: ', data.body.items);
+        res.render('albums', { albumsList: data.body.items})// ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    })
+    .catch(err => console.log('The error while searching albums occurred: ', err));
+    
+})
+
+app.get('/tracks/:albumId', (req, res) => {
+    console.log(req.params);
+    const searchPhrase = req.params.albumId;
+    console.log(searchPhrase);
+    spotifyApi
+    .getAlbumTracks(searchPhrase)
+    .then(data => {
+        console.log('The received data from the API tracks page: ', data.body.items);
+        res.render('tracks', { tracksList: data.body.items})// ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    })
+    .catch(err => console.log('The error while searching tracks occurred: ', err));
+    // res.send(req.params)
+    // res.render('index', {doctitle: 'Ironhack spotify thing',});
+})
+
+
+
 
 app.listen(5555, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
