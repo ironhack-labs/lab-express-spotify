0068c608e7bdbea4b7f99f47204612cc13c08899
Dominika Lipka <dominika.lipka96@gmail.com>
2021-10-17 11:33:14 +0200

    Iteration 4 done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const { json } = require('express');
 const express = require('express');
 const hbs = require('hbs');
 
@@ -42,5 +43,13 @@ app.get('/artist-search', (req, res) => {
   })
   .catch(err => console.log('The error while searching artists occurred: ', err));
 })
+
+app.get('/albums/:artistId', (req, res, next) => {
+    spotifyApi
+        .getArtistAlbums(req.params.artistId)
+        .then (data => {
+            res.render('albums', data.body)
+        }) 
+});
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
