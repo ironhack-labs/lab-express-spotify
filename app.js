a5d7f08bb8f2af22a98aea162e5fe20956e45502
Alice <alice.jost@outlook.de>
2021-10-17 23:41:19 +0200

    add CSS

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get('/artist-search', (req, res, next) => {
     spotifyApi
         .searchArtists(req.query.name)
         .then(data => {
-            //console.log('The received data from the API: ', data);
+            console.log('The received data from the API: ', data.body.artists.items);
             res.render('artist-search-results', {listOfArtists: data.body.artists.items})
         })
         .catch(err => console.log('The error while searching artists occurred: ', err));
