690c8a332561a30f0d85178ed51e77a42fbcce83
CodingEinhornDeu <88826323+CodingEinhornDeu@users.noreply.github.com>
2021-10-18 08:02:35 +0200

    iterations 1 2 3 4 5

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
+
 
 const app = express();
 
@@ -12,7 +14,58 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
 
 // Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+app.get('/artist-search', (req, res) => {
+
+  spotifyApi
+    .searchArtists(req.query.artist)
+    .then(data => {
+      console.log('The received data from the API: ', data.body);
+      res.render(
+        'artist-search-result', data.body.artists
+      );
+    })
+    .catch(err => {
+      console.log('The error occurred: ', err)
+    })
+})
+app.get('/albums/:artistId', (req, res, next) => {
+  spotifyApi
+    .getArtistAlbums(req.params.artistId)
+    .then(data => {
+      res.render('albums', data.body);
+    })
+    .catch(err => {
+      console.log('The error occurred: ', err)
+    })
+})
+
+app.get('/tracks/:albumId', (req, res, next) => {
+  spotifyApi
+    .getAlbumTracks(req.params.albumId)
+    .then(data => {
+      res.render('tracks', data.body)
+    })
+    .catch(err => {
+      console.log('The error occurred: ', err)
+    })
+})
+app.listen(3001, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
