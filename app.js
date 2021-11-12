b6da0d650052e47014f2bd4a17681c4e62c535a5
abdelmalek.saksioui <abdelmalek.saksioui@cgm.com>
2021-11-12 17:46:23 +0100

    Done

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
-
 const express = require('express');
 const hbs = require('hbs');
+
+const SpotifyWebApi = require('spotify-web-api-node');
 
 // require spotify-web-api-node package here:
 
@@ -12,7 +13,56 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
 
+// Retrieve an access token
+spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 // Our routes go here:
 
+app.get('/', (req, res) => {
+    res.render('home');
+})
+app.get('/artist-search', (req, res) => {
+    searchArtists(res, req.query.q);
+})
+app.get('/albums/:id', (req, res) => {
+    getAlbums(res, req.params.id);
+})
+app.get('/tracks/:id', (req, res) => {
+    getTracks(res, req.params.id);
+})
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+
+
+function searchArtists(res, query) {
+    spotifyApi
+        .searchArtists(query)
+        .then(data => {
+            res.render('search', {artists: data.body.artists});
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+}
+
+function getAlbums(res, id) {
+    spotifyApi
+        .getArtistAlbums(id)
+        .then(data => {
+            res.render('albums', {albums: data.body.items});
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+}
+
+function getTracks(res, id) {
+    spotifyApi
+        .getAlbumTracks(id)
+        .then(data => {
+            res.render('tracks', {tracks: data.body.items});
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+}
