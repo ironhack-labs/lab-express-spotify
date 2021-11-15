6ea4a17742e119391c50a5d8cd0e8131adac6bed
nerea pardo <nerea.pardo.cundins@gmail.com>
2021-11-15 21:02:44 +0000

    solved

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
 });
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Retrieve an access token
 spotifyApi
@@ -24,6 +26,40 @@ spotifyApi
   );
 
 // Our routes go here:
+app.get("/", (req, res) => {
+  res.render("home.hbs");
+});
+
+app.get("/artist-search", async (req, res) => {
+  const artist = req.query.artist;
+  try {
+    const artistFromDb = await spotifyApi.searchArtists(artist);
+    // console.log("The received data from the API: ", data.body);
+    res.render("artist-search-results.hbs", {
+      artist: artistFromDb.body.artists.items,
+    });
+  } catch (err) {
+    console.log("The error while searching artists occurred: ", err);
+  }
+});
+app.get("/albums/:id", async (req, res) => {
+  try {
+    const spotifyAlbums = await spotifyApi.getArtistAlbums(req.params.id);
+    res.render("albums.hbs", {
+      albums: spotifyAlbums.body.items,
+    });
+  } catch (err) {
+    console.log("The error while searching artists occurred: ", err);
+  }
+});
+app.get("/tracks/:id", async (req, res) => {
+  try {
+    const spotifyTrack = await spotifyApi.getAlbumTracks(req.params.id);
+    res.render("tracks.hbs", { tracks: spotifyTrack.body.items });
+  } catch (err) {
+    console.log("The error while searching artists occurred: ", err);
+  }
+});
 
 app.listen(3000, () =>
   console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
