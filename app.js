8ac5494ec93a78253c6f0e6e5923faf8eb638b67
Santi Jimenez <jimenez.santi@gmail.com>
2021-11-15 12:50:22 +0100

    Iteration 2&3.1

--- app.js
+++ app.js
@@ -16,14 +16,25 @@ app.use(express.static(__dirname + '/public'));
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
-  });
+});
   
-  // Retrieve an access token
-  spotifyApi
-    .clientCredentialsGrant()
-    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
-    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+// Retrieve an access token
+spotifyApi
+.clientCredentialsGrant()
+.then(data => spotifyApi.setAccessToken(data.body['access_token']))
+.catch(error => console.log('Something went wrong when retrieving an access token', error));
   
 // Our routes go here:
+app.get('/', (req, res) =>{
+    res.render('home.hbs')
+})
+
+app.get('/artist-search', (req, res) =>{
+    res.send(req.query);
+    console.log("hola")
+    console.log(req.query)
+    res.render('artistSearch.hbs')
+})
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
