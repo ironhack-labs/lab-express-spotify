2b10db43f3039cbb4266638fcefddedf60c64e41
nachbr91 <nabero91@gmail.com>
2021-11-15 19:20:14 +0100

    Iteration 5 done. Add styles to home page

--- app.js
+++ app.js
@@ -15,13 +15,15 @@ app.use(express.static(__dirname + '/public'));
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET
+  clientSecret: process.env.CLIENT_SECRET,
 });
 
 spotifyApi
   .clientCredentialsGrant()
-  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
-  .catch(error => console.log('Something went wrong when retrieving an access token', error));
+  .then((data) => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch((error) =>
+    console.log('Something went wrong when retrieving an access token', error)
+  );
 
 // Our routes go here:
 
@@ -31,36 +33,37 @@ app.get('/', (req, res) => {
 
 app.get('/artist-search', async (req, res) => {
   try {
-    const spotifyArtist = await spotifyApi.searchArtists(req.query.artist)
-    res.render('artistSearch.hbs',{
-      spotifyArtist: spotifyArtist.body.artists.items
-    })
-  } catch(err) {
-    console.log('The error while searching artists occurred: ', err)
+    const spotifyArtist = await spotifyApi.searchArtists(req.query.artist);
+    res.render('artistSearch.hbs', {
+      spotifyArtist: spotifyArtist.body.artists.items,
+    });
+  } catch (err) {
+    console.log('The error while searching artists occurred: ', err);
   }
 });
 
 app.get('/albums/:artistId', async (req, res) => {
   try {
-    const spotifyAlbums = await spotifyApi.getArtistAlbums(req.params.artistId)
-    res.render('albums.hbs',{
-      albums: spotifyAlbums.body.items
-    })
-  } catch(err) {
-    console.log('The error while searching artists occurred: ', err)
+    const spotifyAlbums = await spotifyApi.getArtistAlbums(req.params.artistId);
+    res.render('albums.hbs', {
+      albums: spotifyAlbums.body.items,
+    });
+  } catch (err) {
+    console.log('The error while searching artists occurred: ', err);
   }
 });
 
-// app.get('/albums/:artistId', async (req, res) => {
-//   try {
-//     const spotifyAlbums = await spotifyApi.getArtistAlbums(req.params.artistId)
-//     console.log('The received data from the API:', spotifyAlbums.body)
-//     res.render('albums.hbs',{
-//       albums: spotifyAlbums.body.items
-//     })
-//   } catch(err) {
-//     console.log('The error while searching artists occurred: ', err)
-//   }
-// });
+app.get('/tracks/:id', async (req, res) => {
+  try {
+    const spotifyTrack = await spotifyApi.getAlbumTracks(req.params.id);
+    res.render('tracks.hbs', {
+      tracks: spotifyTrack.body.items,
+    });
+  } catch (err) {
+    console.log('The error while searching artists occurred: ', err);
+  }
+});
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.listen(3000, () =>
+  console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')
+);
