60de70506488afd3a783f2a0bee41c365f317e5d
Aarón López <n_aaron5@hotmail.com>
2021-11-15 22:41:58 +0100

    Laboratory done

--- app.js
+++ app.js
@@ -1,73 +1,77 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const chalk = require('chalk')
-const express = require('express');
-const hbs     = require('hbs');
+const chalk = require("chalk");
+const express = require("express");
+const hbs = require("hbs");
 
 // require spotify-web-api-node package here:
-const  SpotifyWebApi  =  require ( 'spotify-web-api-node' ) ;
-const app             = express();
+const SpotifyWebApi = require("spotify-web-api-node");
+const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-  });
-  
-  // Retrieve an access token
-  spotifyApi
-    .clientCredentialsGrant()
-    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
-    .catch(error => console.log(chalk.bgRed('Something went wrong when retrieving an access token', error)))
-  
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+  .catch((error) =>
+    console.log(
+      chalk.bgRed("Something went wrong when retrieving an access token", error)
+    )
+  );
 
 // Our routes go here:
 
-app.get('/', (req, res)=>{
-    res.render('home.hbs')
-  })
+app.get("/", (req, res) => {
+  res.render("home.hbs");
+});
 
-app.get('/artist-search', async (req, res)=>{
+app.get("/artist-search", async (req, res) => {
   try {
-    const spotifyArtist = await spotifyApi.searchArtists(req.query.artist)
-    res.render('artist.hbs', {
-      artist: spotifyArtist.body.artists.items
-    })
-  } catch(err){
-    console.log(chalk.bgRed(err))
+    const spotifyArtist = await spotifyApi.searchArtists(req.query.artist);
+    res.render("artist.hbs", {
+      artist: spotifyArtist.body.artists.items,
+    });
+  } catch (err) {
+    console.log(chalk.bgRed(err));
   }
-  
-})
+});
 
-app.get('/albums/:artistId', async (req, res)=>{
+app.get("/albums/:artistId", async (req, res) => {
   try {
-    const spotifyAlbums = await spotifyApi.getArtistAlbums(req.params.artistId)
-    res.render('albums.hbs', {
-      albums: spotifyAlbums.body.items
-    })
-  } catch(err) {
-    console.log(chalk.bgRed(err))
+    const spotifyAlbums = await spotifyApi.getArtistAlbums(req.params.artistId);
+    res.render("albums.hbs", {
+      albums: spotifyAlbums.body.items,
+    });
+  } catch (err) {
+    console.log(chalk.bgRed(err));
   }
-})
+});
 
-app.get('/tracks/:albumId', async (req, res)=>{
+app.get("/tracks/:albumId", async (req, res) => {
   try {
-    const spotifyTracks = await spotifyApi.getAlbumTracks(req.params.albumId)
-    console.log(spotifyTracks.body)
-    res.render('tracks.hbs', {
-      tracks: spotifyTracks.body.items
-    })
-  } catch(err) {
-    console.log(chalk.bgRed(err))
+    const spotifyTracks = await spotifyApi.getAlbumTracks(req.params.albumId);
+    console.log(spotifyTracks.body);
+    res.render("tracks.hbs", {
+      tracks: spotifyTracks.body.items,
+    });
+  } catch (err) {
+    console.log(chalk.bgRed(err));
   }
-})
+});
 
-
-
-app.listen(3000, () => console.log(chalk.bgGreen('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')))
+app.listen(3000, () =>
+  console.log(
+    chalk.bgGreen("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+  )
+);
