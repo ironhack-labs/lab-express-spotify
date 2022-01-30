16259ad08c24dcac2211c32f6271d3cf8cb8fe1d
Sergio Villabrille <sergiovillabrille@hotmail.com>
2022-01-30 22:43:40 +0100

    Lab donete

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static(__dirname + '/public'));
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
-  });
+  });      
   
   // Retrieve an access token
   spotifyApi
@@ -24,5 +24,36 @@ const spotifyApi = new SpotifyWebApi({
     .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/artistSearchResults', (req, res, next) => {
+  const artist = req.query.artist;
+
+  spotifyApi
+.searchArtists(artist)
+.then(data => {
+  res.render('artistSearchResults', {artists:data.body.artists.items});
+})
+});
+
+app.get('/albums/:id', (req, res, next) => {
+  spotifyApi
+  .getArtistAlbums(req.params.id)
+  .then(artistAlbums => {
+      res.render('albums', {albums:artistAlbums.body.items})
+  })
+})
+
+  app.get('/tracks/:id', (req, res, next) => {
+    spotifyApi
+    .getAlbumTracks(req.params.id, { limit: 5, offset: 1 })
+    .then((albumsTracks) => {
+      res.render('tracks', {tracks: albumsTracks.body.items});
+  })
+    .catch(err => console.log(err));
+  });
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
