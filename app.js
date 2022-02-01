13e86be8c9f2bb077128ebc1f447e9d564628333
Bo Bennetsen <Bo.Bennetsen@LEGO.com>
2022-02-01 08:06:44 +0100

    Iteration 4 completed. Iteration 5 - view tracks is not working yet

--- app.js
+++ app.js
@@ -28,22 +28,53 @@ app.get('/', (req, res, next) => {
     res.render('index');
   });
 
+  //Getting artist
   app.get('/artist-search', (request, res, next) => {
-    console.log(request.query.artists);
+    //console.log(request.query.artists);
     spotifyApi
     .searchArtists(request.query.artists)
     .then(data => { 
      const apiResp = data.body.artists.items;
-     
-
-     console.log('The received data from the API: data.body.artists.items ', data.body.artists.items);
-     console.log(`Data from apiResp ${apiResp}`)
     
      res.render('artist-search-results', { artists: apiResp });
      //res.send(data.body.artists.items)
+     //console.log('The received data from the API: data.body.artists.items ', data.body.artists.items);
+     //console.log(`Data from apiResp ${apiResp}`)
 
     })
     .catch(err => console.log('The error while searching artists occurred: ', err));
   });
  
+
+//Getting albums
+  app.get('/albums/:theId', (req, res, next) => {
+    spotifyApi
+    .getArtistAlbums(req.params.theId)
+    .then((data) => {
+      //console.log('The received data from the API: ', data);
+       //console.log('The received data from the API: ', data.body);
+      //console.log('The received data from the API: ', data.body.items);
+      res.render("albums", { albums: data.body.items });
+    })
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
+});
+
+
+//Getting tracks not working yet:
+app.get("/tracks/:id", (req, res) => {
+  spotifyApi
+    .getAlbumTracks(req.params.id)
+    .then((data) => {
+      console.log("The received tracks data from the API: ", data.body.items);
+     
+      res.render("tracks", { tracks: data.body.items });
+    })
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
+});
+
+
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
