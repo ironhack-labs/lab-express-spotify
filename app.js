926b5e2344103eb977cc149791899eb51f0abd77
dorrrn <doan7tran@gmail.com>
2022-02-04 17:55:42 +0100

    display artists with 1 album image & button "view albums"

--- app.js
+++ app.js
@@ -60,14 +60,15 @@ app.get("/artist-search", (req, res, next) => {
       console.log("The received data from the API: ", data.body);
       // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
       const artistItems = data.body.artists.items;
-
-      console.log(artistItems[1]);
+    //console.log(artistItems[1]);
       res.render("artist-search-results", { artist: artistItems });
     })
     .catch((err) =>
       console.log("The error while searching artists occurred: ", err)
     );
 });
+
+// ==== create route to album view
 
 app.listen(3007, () =>
   console.log("My Spotify project running on port 3007 🎧 🥁 🎸 🔊")
