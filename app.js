f50b09a4a0edf705b35b940d8dbb25f8741dce3c
dorrrn <doan7tran@gmail.com>
2022-02-04 17:44:37 +0100

    search result page displaying results

--- app.js
+++ app.js
@@ -53,13 +53,16 @@ app.get("/", (req, res, next) => {
 });
 
 //=== create route to artist-search-result
-app.get("/artist-search/:artist", (req, res, next) => {
+app.get("/artist-search", (req, res, next) => {
   spotifyApi
-    .searchArtists(req.body)
+    .searchArtists(req.query.artist)
     .then((data) => {
       console.log("The received data from the API: ", data.body);
       // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
-      res.render("artist-search-results", req.body);
+      const artistItems = data.body.artists.items;
+
+      console.log(artistItems[1]);
+      res.render("artist-search-results", { artist: artistItems });
     })
     .catch((err) =>
       console.log("The error while searching artists occurred: ", err)
