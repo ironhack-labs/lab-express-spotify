3c0647128742ae3694f59aa0a90f678f96c4f9fc
Joana Carballo <joana16cruz@gmail.com>
2022-02-05 16:23:40 +0100

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 
-
 const express = require('express');
 const hbs = require('hbs');
 
@@ -14,6 +13,11 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
+// Register the location for handlebars partials here:
+
+hbs.registerPartials(__dirname + "/views/partials");
+
+
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
@@ -26,6 +30,28 @@ spotifyApi
     .then(data => spotifyApi.setAccessToken(data.body['access_token']))
     .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
+
 // Our routes go here:
+// index route
+app.get('/', (req, res) => {
+  res.render("index", {
+    doctitle: "Home Page",
+    });
+  });
+
+// /artist-search route
+spotifyApi
+  .searchArtists('/artist-search', (req, res, next) => {
+    res.render(req.query);
+  })
+  .then(data => {
+    console.log('The received data from the API: ', data.body);
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+
+
+
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
