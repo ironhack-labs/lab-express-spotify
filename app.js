65f30b3a74ba1d37569666ac1d3ad1e6baa4fc50
DaianyNascimento <daiany.cristina@outlook.com>
2022-03-11 18:06:20 +0000

    Solved lab

--- app.js
+++ app.js
@@ -2,17 +2,49 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
+const path = require('path');
+const SpotifyWebApi = require('spotify-web-api-node');
+const PORT = 3000;
 
 const app = express();
-
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+//app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+app.get('/artist-search', (req, res) => {
+    //console.log(req.query);
+    spotifyApi
+        .searchArtists(req.query.artist)
+        .then(data => {
+            console.log('The received data from the API: ', data.body.artists);
+            // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+            const [firstArtistFound] = data.body.artists;
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+            res.render("artist-search-results", {
+                name: firstArtistFound.name,
+                imgUrl: firstArtistFound.images,
+            });
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+
+});
+
+app.get("/", (req, res) => {
+    res.render("layout", { pageTitle: "Home - Daify" });
+});
+
+app.listen(PORT, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
