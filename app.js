4688403b182d43f5c7a5a6430fb5b8e11ae704f3
Roberto D. Ezquerro <roberto.ezquerro@gmail.com>
2022-04-14 18:38:44 +0200

    [MAD PP] Pablo Andreu Roberto Ezquerro

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 
 const express = require('express');
+const { param } = require('express/lib/request');
+const res = require('express/lib/response');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -12,7 +13,55 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/artist-search', (req, res) => {
+
+
+
+    spotifyApi
+        .searchArtists(req.query.search)
+        .then(data => {
+            let album = data.body.artists.items
+
+            console.log('The received data from the API: ', { album });
+            // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+            res.render('artist-search-results', { album })
+
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+});
+
+
+app.get('/albums/:artistId', (req, res) => {
+
+    const { artistId } = req.params
+
+    spotifyApi.getArtistAlbums(artistId)
+        .then(artistId => {
+            res.render("albums", { artistId })
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+
+})
+
+
+
+
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
