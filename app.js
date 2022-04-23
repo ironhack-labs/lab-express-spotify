306a687dfacd3204a9d6d000a721b425bb9fb409
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-04-23 18:40:34 +0200

    solve iteration 3

--- app.js
+++ app.js
@@ -15,14 +15,28 @@ app.use(express.static(__dirname + '/public'));
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
-  });
-  
-  // Retrieve an access token
-  spotifyApi
+});
+
+// Retrieve an access token
+spotifyApi
     .clientCredentialsGrant()
     .then(data => spotifyApi.setAccessToken(data.body['access_token']))
     .catch(error => console.log('Something went wrong when retrieving an access token', error));
-    
+
 // Our routes go here:
+app.get("/", (req, res, next) => {
+    res.render("home");
+});
+
+app.get("/artist-search", (req, res, next) => {
+    const artistName = req.query.artistName;
+    spotifyApi
+        .searchArtists(artistName)
+        .then(data => {
+            console.log('The received data from the API: ', data.body.artists.items[0].images);
+            res.render("artist-search-results", { results: data.body.artists.items })
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+})
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
