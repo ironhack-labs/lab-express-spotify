473dae8d5dd7d19baf575b4656f8cb1cda33ed15
Ivo Silva <silvaivosoares@gmail.com>
2022-04-24 23:02:28 +0100

    Completed extra iteration. Took me some time because while my reasoning was correct, first I couldn't find the total number of artists in the API's response, and then I took a while getting right passing the necessary variables to the hbs file.

--- app.js
+++ app.js
@@ -26,22 +26,70 @@ const spotifyApi = new SpotifyWebApi({
     .then(data => spotifyApi.setAccessToken(data.body['access_token']))
     .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
-// Our routes go here:
 
+// const pagination = () => {
+// const prevPageButton = document.getElementById("previous-page");
+// const nextPageButton = document.getElementById("next-page");
+
+// let currentUrl;
+// let currentPage;
+// let urlLength;
+
+// prevPageButton.addEventListener("click", () => {
+//     currentUrl = window.location.href;
+//     urlLength = window.location.href.length();
+//     console.log(currentUrl);
+//     console.log(urlLength);
+//     currentPage = currentUrl.lastIndexOf
+// });
+
+// nextPageButton.addEventListener("click", () => {
+//     currentUrl = window.location.href;
+// });
+// }
+
+
+
+
+// Our routes go here:
 app.get("/", (req, res, next) => {
   res.render("home");
 });
 
-app.get("/artist-search-results", (req, res, next) => {
 
-  spotifyApi.searchArtists(`${req.query.artist}`, {limit: 50})
+
+app.get("/artist-search-results", (req, res, next) => {
+  let page;
+  let nextPage;
+  let previousPage;
+  const artistName = req.query.artist;
+
+  if (req.query.page) {
+    page = parseInt(req.query.page);
+  } else {
+    page = 1;
+  }
+
+  spotifyApi.searchArtists(`${req.query.artist}`, {limit: 20, offset: (page - 1) * 20})
   .then(response => {
+    const maxArtists = parseInt(response.body.artists.total);
+
+    if (page > 0) {
+      previousPage = page - 1;
+    }
+    
+    if (page < Math.floor(maxArtists / 20)){
+      nextPage = page + 1;
+    }
+
     // console.log(response.body.artists.items);
 
-    res.render("artist-search-results",{artistsArr: response.body.artists.items})
+    res.render("artist-search-results",{artistsArr: response.body.artists.items, artistName, page, nextPage, previousPage})
   })
   .catch(error => console.log("There was an error getting the artists:", error));
 });
+
+
 
 app.get('/albums/:artistId', (req, res, next) => {
   spotifyApi.getArtistAlbums(req.params.artistId)
