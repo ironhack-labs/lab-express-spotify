49d347f931954ef19754ba048c16fb27c86bbca3
Louise <louisegabriellecc@gmail.com>
2022-04-24 17:47:17 +0100

    Removed console logs - tidied up code

--- app.js
+++ app.js
@@ -36,44 +36,44 @@ app.get("/", (req, res) => {
 
 // Search for a artist and return an array with all data matching search criteria and display
 app.get("/artist-search", (req, res) => {
-  console.log("A search was made from the home page");
   spotifyApi
-    .searchArtists(req.query.name)
-    .then(artistSearched => {
-      console.log(
-        "A artist was retrieved from the DB",
-        artistSearched.body.artists.items
-      );
-      res.render("artist-search", {artist: artistSearched.body.artists.items});
+    .searchArtists(req.query.name, { limit: 10 }) // limit results to 10
+    .then((artistSearched) => {
+      console.log("A artist was retrieved from the DB", artistSearched.body);
+      res.render("artist-search", {
+        artist: artistSearched.body.artists.items,
+      });
     })
     .catch((err) => {
       console.log("Error error error searching for artists in DB", err);
     });
 });
 
+const nextPage = async (req, res) => {};
+
 // When a 'View Albums' link is clicked search for all albums from the artist and display
 app.get("/artist-albums/:artistId", (req, res) => {
-    console.log("a request to show the artist's albums has been made for this id", req.params);
-    spotifyApi.getArtistAlbums(req.params.artistId)
-    .then(artistAlbum => {
-        console.log("the artist album details are", artistAlbum.body)
-        res.render("artist-albums", {album: artistAlbum.body.items})
+  spotifyApi
+    .getArtistAlbums(req.params.artistId)
+    .then((artistAlbum) => {
+      res.render("artist-albums", { album: artistAlbum.body.items });
     })
     .catch((err) => {
-        console.log("Error error error searching for albums in DB", err);
-      });
-})
+      console.log("Error error error searching for albums in DB", err);
+    });
+});
 
 //When a 'View Tracks' link is clicked search for all tracks from the artist and display
 app.get("/artist-tracks/:albumId", (req, res) => {
-    console.log("a request has been made to see the tracks from the alnum", req.params)
-    spotifyApi.getAlbumTracks(req.params.albumId)
-.then(albumTracks => {
-    console.log("the album tracks are", albumTracks.body)
-    res.render("album-tracks", {track: albumTracks.body.items})
-})
-.catch(err => console.log("Error errror error retrieving tracks from an album"))
-})
+  spotifyApi
+    .getAlbumTracks(req.params.albumId)
+    .then((albumTracks) => {
+      res.render("album-tracks", { track: albumTracks.body.items });
+    })
+    .catch((err) =>
+      console.log("Error errror error retrieving tracks from an album")
+    );
+});
 
 app.listen(3000, () =>
   console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
