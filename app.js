96b9acf12a1295afb63c1fd15fc725677ef96b03
EugeBr <eugebordenave@gmail.com>
2022-05-03 14:45:03 +0200

    serch form and home page

--- app.js
+++ app.js
@@ -26,4 +26,8 @@ const spotifyApi = new SpotifyWebApi({
 
 // Our routes go here:
 
+app.get("/", (req, res) => {
+	res.render("home");
+});
+
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
