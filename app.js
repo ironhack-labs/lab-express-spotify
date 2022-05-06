e1aaac784c0ac6b4b44fba781edb7da9cb0448c2
lucas-emg <lucas.gabriel3.lg@gmail.com>
2022-05-06 20:18:17 -0300

    lab started

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-// setting the spotify-api goes here:
+// setting the spotify-api goes here
 
 // Our routes go here:
 
