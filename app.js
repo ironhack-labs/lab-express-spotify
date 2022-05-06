54161c761ae09cba33d99e8fcdf4ce65ab234c3c
Matilde Costa <matildecosta@MBP-de-Matilde.lan>
2022-05-06 17:25:20 +0100

    hbs and app done, css needed

--- app.js
+++ app.js
@@ -1,18 +1,87 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
+const express = require("express");
+const hbs = require("hbs");
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+  .catch((error) =>
+    console.log("Something went wrong when retrieving an access token", error)
+  );
 
 // Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+app.get("/artist-search", (req, res, next) => {
+  spotifyApi
+    .searchArtists(req.query.artistName)
+    .then((artist) => {
+      console.log(
+        "The received data from the API: ",
+        artist.body.artists.items
+      );
+      const artistResult = artist.body.artists.items;
+
+      res.render("artistSearchResults", { artistResult });
+    })
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
+});
+
+app.get("/albums/:artistId", (req, res, next) => {
+  const { artistId } = req.params;
+  spotifyApi
+    .getArtistAlbums(artistId)
+    .then((album) => {
+      console.log("Albums accessed with success!");
+
+      const albumResult = album.body.items;
+
+      res.render("albums", { albumResult });
+      console.log(albumResult);
+    })
+    .catch((err) =>
+      console.log("The error while getting the albums occurred: ", err)
+    );
+});
+
+app.get("/tracks/:trackId", (req, res, next) => {
+  const { trackId } = req.params;
+  spotifyApi
+    .getAlbumTracks(trackId)
+    .then((tracks) => {
+      console.log("tracks accessed with success!");
+
+      const tracksResult = tracks.body.items;
+      res.render("viewTracks", { tracksResult });
+      console.log(tracksResult);
+    })
+    .catch((err) =>
+      console.log("The error while getting the albums occurred: ", err)
+    );
+});
+
+app.listen(3000, () =>
+  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+);
