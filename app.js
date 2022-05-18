9b986be05f7f9b8c82e6330e0b1fd34638292d85
markixa <laiamr77@gmail.com>
2022-05-18 13:52:22 +0200

    added routes, and views

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
+const spotifyWebApi = require('spotify-web-api-node');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -12,7 +14,63 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
-// Our routes go here:
+ // Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+}); 
+
+/**
+ * Iteration 3
+ * Search for an artist
+ * Create a route that renders a simple home page -> /artist-search
+ */
+
+app.get('/artist-search', (req, res) => { 
+  spotifyApi
+    .searchArtists(req.query.artistSearch)
+    .then(data => {
+      console.log('The received data from the API: ', data.body.artists);
+      //actions after receiving data from the API
+      res.render('artist-search-results', { artists: data.body.artists.items });
+    })
+    .catch(err => console.log(err))
+
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+app.listen(3000, () => console.log('My Spotify project running on port 3000'));
