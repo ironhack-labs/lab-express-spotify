e7f230e3c3dbc62548bb09ed62d5dc89d0543a8b
Vale Quiroga <valentinqramos@outlook.com>
2022-06-07 15:30:45 -0500

    adding partials

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerPartials(__dirname + "/views/partials");
+
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET
@@ -25,22 +27,19 @@ spotifyApi
 app.get('/', (req, res, next) => { res.render('home') });
 
 app.get('/artist-search', (req, res, next) => {
-  spotifyApi
-  .searchArtists(req.query.artist)
-    .then( data => {res.render('artist-search-results', data.body.artists)})
+  spotifyApi.searchArtists(req.query.artist)
+    .then( data => { res.render('artist-search-results', data.body.artists) })
     .catch( err => console.log('searching artist error -> ', err));
 });
 
 app.get('/albums/:artistId',(req, res, next) =>{
-  spotifyApi
-  .getArtistAlbums(req.params.artistId)
-    .then( data => {res.render('albums', data.body)})
+  spotifyApi.getArtistAlbums(req.params.artistId)
+    .then( data => { res.render('albums', data.body) })
     .catch( err => console.log('searching albums error ->', err));
 });
 
 app.get('/tracks/:albumId/', (req, res, next) => {
-  spotifyApi
-  .getAlbumTracks(req.params.albumId)
+  spotifyApi.getAlbumTracks(req.params.albumId)
     .then( data => { res.render('tracks', data.body)})
     .catch( err => console.log('searching track error ->', err))
 });
