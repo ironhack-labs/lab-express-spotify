ec81be85d20663ea916794e40bbb22aad4978726
antonio-datahack <antoniosastre@tutanota.com>
2022-06-24 18:44:37 +0200

    almost done

--- app.js
+++ app.js
@@ -1,18 +1,76 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
+const express = require("express");
+const hbs = require("hbs");
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
+hbs.registerPartials(__dirname + "/views/partials");
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
 
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+  .catch((error) =>
+    console.log("Something went wrong when retrieving an access token", error)
+  );
 // Our routes go here:
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.get("/artist-search", (req, res, next) => {
+  spotifyApi
+    .searchArtists(req.query.q)
+    .then((data) => {
+      //   console.log("The received data from the API: ", data.body.artists.items);
+      //   console.log(req.query.q);
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+      res.render("artist-search-results", {
+        artists: data.body.artists.items,
+      });
+    })
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
+});
+
+app.get("/albums/:artistId", (req, res, next) => {
+  spotifyApi.getArtistAlbums(req.params.artistId).then(
+    function (data) {
+      //   console.log(req.params.artistId);
+      //   console.log("Artist albums", data.body);
+      res.render("albums", { artistsAlbum: data.body.items });
+    },
+    function (err) {
+      console.error(err);
+    }
+  );
+});
+
+app.get("/albums/tracks/:albumId", (req, res, next) => {
+  spotifyApi.getAlbumTracks(req.params.albumId, { limit: 5, offset: 1 }).then(
+    function (data) {
+      console.log(data.body);
+      res.render("tracks", { tracks: data.body.items });
+    },
+    function (err) {
+      console.log("Something went wrong!", err);
+    }
+  );
+});
+
+app.listen(3000, () =>
+  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+);
