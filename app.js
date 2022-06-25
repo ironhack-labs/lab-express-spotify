dc31a9c988fd95da770754f8dd417a367fe5828f
WHL99 <xjv298347@gmail.com>
2022-06-25 15:22:01 +0200

    done

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ const app = express();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + "/views/partials");
+//必須註冊partial路徑！
 
 // setting the spotify-api goes here:
 
@@ -31,37 +33,52 @@ app.get('/', (req, res, next) => {
   console.log('index test')
 });
 
-
-app.get('/artist-search', (req, res) => {
+app.get("/artist-search", (req, res, next) => {
   spotifyApi.searchArtists(req.query.q)
-    .then(data => {
-  //    console.log('The received data from the API: ', data.body.artists.items);
-      res.render('artist-search-results', { artistList: data.body.artists.items })
+    .then((data) => {
+       // console.log("The received data from the API: ", data.body.artists.items[0].preview_url);
+      //   console.log(req.query.q);
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+      res.render("artist-search-results", {
+        artists: data.body.artists.items
+      });
     })
-    .catch(err => console.log('The error while searching artists occurred: ', err));
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
 });
 
-app.get('/albums/:artistId', (req, res, next) => {
-  spotifyApi.getArtistAlbums(req.params.artistId)
-    .then(data => {
-   //   console.log('Artist albums?????');
-       res.render('albums',{ albumList: req.params.artistId})
-    })
-    .catch(err => console.log('The error while albums: ', err));
+app.get("/albums/:artistId", (req, res, next) => {
+  spotifyApi.getArtistAlbums(req.params.artistId).then(
+    function (data) {
+      //   console.log(req.params.artistId);
+      //   console.log("Artist albums", data.body);
+      res.render("albums", { artistsAlbum: data.body.items });
+    },
+    function (err) {
+      console.error(err);
+    }
+  );
+});
+
+
+app.get("/albums/tracks/:albumId", (req, res, next) => {
+  spotifyApi.getAlbumTracks(req.params.albumId).then(
+    function (data) {
+      //   console.log(req.params.artistId);
+       //  console.log("tracks", data.body);
+       console.log("The received data from the API: ", data.body.items);
+      res.render("tracks", { albumsTrack: data.body.items });
+    },
+    function (err) {
+      console.error(err);
+    }
+  );
 });
 
 
 
-// app.get('/products/:number', function (req, res) {
-// 	const value = req.params.number
-// 	res.render('dashboard', { number: value })
-// })
 
 
 
-
-
-
-
-
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.listen(4000, () => console.log('My Spotify project running on port 4000 🎧 🥁 🎸 🔊'));
