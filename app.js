fc7bb98815927a1afbdca6842649b21a781ebe1a
rubenanlo <85644491+rubenanlo@users.noreply.github.com>
2022-07-09 17:43:43 +0200

    done full assessment wo css or super bonus

--- app.js
+++ app.js
@@ -29,4 +29,36 @@ app.get("/", (req, res) => {
     res.render("home")
 })
 
+app.get("/artist-search", (req, res) => {
+  spotifyApi.searchArtists(req.query.list)
+    .then(data => {
+      data = data.body.artists.items;
+
+      return res.render("artist-search-results", {data})
+    })
+    .catch(error => console.log('The error while searching artists occurred: ', error))
+  
+})
+
+app.get("/albums/:id", (req, res) => {
+  spotifyApi.getArtistAlbums(req.params.id)
+  .then((data) => {
+    const albums = data.body.items;
+    res.render('albums',{albums})
+  })
+  .catch(error => console.log('The error while searching artists occurred: ', error))
+});
+
+app.get("/tracks/:id", (req, res) => {
+  spotifyApi.getAlbumTracks(req.params.id) 
+    // { limit : 5, offset : 1 }) //--> added to the getalbumtracks method if we want to limit the number of tracks
+  .then((data) => {
+    const tracks = data.body.items;
+    res.render('tracks',{tracks})
+  })
+  .catch(error => console.log('The error while searching artists occurred: ', error))
+  
+});
+
+
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
