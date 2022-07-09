d6c971b6cf60484a624b18a23f94f2936db1f3e8
Christoph86 <ChrstphBch@web.de>
2022-07-09 16:39:56 +0200

    add home.hbs, recieving data from spotify

--- app.js
+++ app.js
@@ -17,8 +17,29 @@ const spotifyApi = new SpotifyWebApi({
   //  redirectUri: 'http://www.example.com/callback'
   });
 
+  // Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 
 // Our routes go here:
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/artist-search/", (req, res) => {
+  spotifyApi.searchArtists(req.query.artistSearch)
+  .then(data => {
+    console.log('The received data from the API: ', data.body.artists.items);
+    res.render("artist-search");
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));     
+});
+
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
