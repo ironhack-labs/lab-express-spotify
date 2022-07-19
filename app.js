4d880fef05eadcc1a3e081f4f0c2cfc2e7289042
Juan De Oliveira <juanabrahamd@gmail.com>
2022-07-19 14:59:28 +0200

    Work in progress

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
 
+const SpotifyWebApi = require('spotify-web-api-node');
+
 const app = express();
 
 app.set('view engine', 'hbs');
@@ -13,6 +15,26 @@ app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
 
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+
 // Our routes go here:
+
+app.get('/home', (req, res, next) => {
+    console.log("It's working fine")
+    res.render('index')
+})
+
+
+app.get('/artist-search', (req, res, next) => {
+spotifyApi
+    .searchArtists(/*'HERE GOES THE QUERY ARTIST'*/)
+    .then(data => {
+      console.log('The received data from the API: ', data.body);
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    })
+.catch(err => console.log('The error while searching artists occurred: ', err));
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
