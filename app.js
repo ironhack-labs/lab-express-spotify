2db9dee9fe68a2511301610ba38473fb96f1330c
tiffany_chang <changyunhsuan.fr@gmail.com>
2022-07-22 20:41:25 +0200

    finish the lab with styling

--- app.js
+++ app.js
@@ -30,28 +30,57 @@ spotifyApi
     console.log("Something went wrong when retrieving an access token", error)
   );
 
-// Our routes go here:
+// Routes
+
+// Home Page
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("home");
 });
 
+// Display Artist Search
 app.get("/artist-search", (req, res, next) => {
   const { search } = req.query;
 
   spotifyApi
     .searchArtists(search)
     .then((data) => {
-      console.log("The received data from the API: ", data.body);
+      // console.log("The received data from the API: ", data.body);
       const artists = data.body.artists.items;
-      const href = data.body.artists.href;
-      // console.log(artists);
-      res.render("artist-search-results", { artists, href });
+      res.render("artist-search-results", { artists });
     })
     .catch((err) =>
       console.log("The error while searching artists occurred: ", err)
     );
 
   console.log("artist-search");
+});
+
+// View Albums
+app.get("/albums/:id", (req, res, next) => {
+  spotifyApi
+    .getArtistAlbums(req.params.id)
+    .then((data) => {
+      // console.log("The received data from the API: ", data.body);
+      const albums = data.body.items;
+      res.render("album", { albums });
+    })
+    .catch((err) => {
+      console.log("The error while viewing album occurred: ", err);
+    });
+});
+
+// View Tracks
+app.get("/tracks/:id", (req, res, next) => {
+  spotifyApi
+    .getAlbumTracks(req.params.id)
+    .then((data) => {
+      // console.log("The received data from the API: ", data.body);
+      const tracks = data.body.items;
+      res.render("tracks", { tracks });
+    })
+    .catch((err) => {
+      console.log("The error while viewing tracks occurred: ", err);
+    });
 });
 
 app.listen(3000, () =>
