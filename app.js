0fd5c664c8b4ec00d1f6e9476c6a258ffb2cf680
Tony5300 <tony_tolen@ziggo.nl>
2022-07-25 18:14:56 +0200

    finsihed the logic on the server. Now working on the styling of it.

--- app.js
+++ app.js
@@ -43,7 +43,32 @@ app.get("/artist-search", (req, res) => {
     .catch((err) =>
       console.log("The error while searching artists occurred: ", err)
     );
-  console.log("artist-search");
+});
+
+app.get("/albums/:albumId", (req, res) => {
+  const { albumId } = req.params; // create/destructure a variable to use the params of the url
+  spotifyApi
+    .getArtistAlbums(albumId) // pass the albumId as a parameter to the getArtist Api call.
+    .then((data) => {
+      const albums = data.body.items; // show all the items that is connected to the search in combination with the query we are sending
+      res.render("albums", { albums }); // render the album page if the search is successfull. The albums variable is given to the render albums page.
+    })
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
+});
+
+app.get("/view-tracks/:trackId", (req, res) => {
+  const { trackId } = req.params; // use the parameter to get the actual id of the song of the album
+  spotifyApi
+    .getAlbumTracks(trackId) // pass the param trackId to the api method to be used on the page
+    .then((data) => {
+      const tracks = data.body.items; // get all itemd of the api request
+      res.render("view-tracks", { tracks }); // pas the tracks variable to the view-tracks page
+    })
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
 });
 
 app.listen(3000, () =>
