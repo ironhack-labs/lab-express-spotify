5a696111abc2eb2b05e9f78cca19c1a2b7bff713
Kira Soba <kira.soba@gmx.de>
2022-07-25 22:04:35 +0200

    Finished Iteration 4 & 5

--- app.js
+++ app.js
@@ -35,20 +35,52 @@ app.get("/artist-search", (req, res) => {
   spotifyApi
     .searchArtists(req.query.artistName)
     .then((data) => {
-      console.log("The received data from the API: ", data.body);
+      // console.log("The received data from the API: ", data.body);
       // the following console displays an artist object, just to show that we can use its keys id, images[0] (just to get the first one) and name to pass the info to the next page
-      console.log(
-        "The first object in the items array: ",
-        data.body.artists.items[0]
-      );
+      // console.log(
+      //   "The first object in the items array: ",
+      //   data.body.artists.items[0]
+      // );
+      // // console.log(data.body.artists.items[0].id);
       const artists = data.body.artists.items;
-      // console.log("Img URL: ", data.body.artists.items[0].images[0].url);
       // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
       res.render("artist-search-results", { artists }); // pass data to the hbs file
     })
     .catch((err) =>
       console.log("The error while searching artists occurred: ", err)
     );
+});
+
+app.get("/albums/:artistId", (req, res) => {
+  console.log("You want to see the albums of id: ", req.params);
+  spotifyApi
+    .getArtistAlbums(req.params.artistId)
+    .then((data) => {
+      // console.log("Artist albums", data.body);
+      const albums = data.body.items;
+      const artistName = data.body.items[0].artists[0].name;
+      // console.log("artistName: ", data.body.items[0].artists[0]);
+      res.render("albums", {
+        albums,
+        artistName,
+      });
+    })
+    .catch((err) => {
+      console.log("Something went wrong", err);
+    });
+});
+
+app.get("/tracks/:albumId", (req, res) => {
+  spotifyApi
+    .getAlbumTracks(req.params.albumId, { limit: 15, offset: 0 })
+    .then((data) => {
+      // console.log("The tracks: ", data.body);
+      const tracks = data.body.items;
+      res.render("tracks", { tracks });
+    })
+    .catch((err) => {
+      console.log("Something wen wrong");
+    });
 });
 
 app.listen(3000, () =>
