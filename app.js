080c6116d2fd9a8bced4b6520ad6739de215935c
poncho678 <vincent.tollens@googlemail.com>
2022-08-01 22:02:17 +0200

    removed log

--- app.js
+++ app.js
@@ -78,7 +78,6 @@ app.get("/album/:id", (req, res) => {
         image: images[0],
         tracks: tracks.items,
       });
-      console.log(tracks);
     },
     function (err) {
       console.error(err);
