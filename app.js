3b16d76b79e1876df49f86f3ae3a2b19ac4d973e
Elvan Ipeker <eipekerdogan@hotmail.com>
2022-08-05 11:24:13 +0200

    done

--- app.js
+++ app.js
@@ -30,6 +30,38 @@ spotifyApi
     );
 
 // Our routes go here:
+// Root route of express app
+
+app.get("/", (req, res) => {
+    res.render("search-artists-form");
+});
+
+app.get("/artist-search", (req, res) => {
+    const artistName = req.query.artistName
+    spotifyApi
+        .searchArtists(artistName)
+        .then(data => {
+            res.render("artist-search-results", data.body.artists.items)
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
+app.get("/albums/:id", (req, res) => {
+    spotifyApi.getArtistAlbums(req.params.id)
+        .then(data => {
+            res.render("albums", data.body.items)
+        })
+        .catch(err => console.log('The error while searching albums occurred: ', err));
+})
+
+app.get("/tracks/:id", (req, res) => {
+    spotifyApi.getAlbumTracks(req.params.id)
+        .then(data => {
+            res.render("tracks", data.body.items)
+        })
+        .catch(err => console.log('The error while searching tracks occurred: ', err));
+})
+
 
 app.listen(3000, () =>
     console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
