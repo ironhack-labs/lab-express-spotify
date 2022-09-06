c13ea3a595cedb3b1ea8963d4a1f451a803e7572
Ivan Rubio <smashedpurse@gmail.com>
2022-09-05 22:45:29 -0500

    Se logra generar la busqueda del valor pero al intentar buscarla en la api esta manda error

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
-
 const express = require('express');
 const hbs = require('hbs');
+const SpotifyWebApi = require('spotify-web-api-node');
+require('dotenv').config();
+
 
 // require spotify-web-api-node package here:
 
@@ -12,7 +13,38 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+
+  //Configuration of hbs
+    app.set("views", __dirname + "/views");
+    app.set("view engine", "hbs");
+
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+app.get("/", (req,res) =>{
+    spotifyApi
+  .searchArtists("/:artist")
+  .then(data => {
+    console.log('The received data from the API: ', data.body);
+    res.render("index");
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+   
+})
+
+app.get("*", (req,res)=>{
+    console.log("🚀 ~ file: app.js ~ line 42 ~ app.get ~ res", res)
+    res.send("<h1>404 NOT FOUND</h1>")
+})
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
