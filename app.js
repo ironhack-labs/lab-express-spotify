8f284da7da6e401f256d396e4fea2619c93051d2
Amanda Ngo <amanda.ngo172@gmail.com>
2022-09-09 15:52:27 -0500

    Done with styling!

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -11,8 +10,60 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-// setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
+
+app.get('/', (req,res) => {
+  res.render('home')
+})
+
+app.get('/artist-search', (req,res) => {
+  // console.log('Query: ', req.query.artistName);
+
+  spotifyApi
+    .searchArtists(req.query.artistName)
+    .then(data => {
+      // console.log('The received data from the API: ', data.body.artists.items[0]);
+      res.render('artist-search-results', {
+        artistItems: data.body.artists.items
+      })
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
+
+app.get('/albums/:id', (req,res) => {
+  spotifyApi
+    .getArtistAlbums(req.params.id)
+    .then(data => {
+      // console.log(data.body)
+      res.render('albums', {
+        albums: data.body.items
+      })
+    })
+    .catch(err => console.log('The error while viewing albums occurred: ', err));
+})
+
+app.get('/tracks/:id', (req,res) => {
+  spotifyApi
+  .getAlbumTracks(req.params.id)
+  .then(data => {
+    // console.log(data.body)
+    res.render('tracks', {
+      songs: data.body.items
+    })
+  })
+})
+
 
 // Our routes go here:
-
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
