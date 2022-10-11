c56200f4758ade6bd2847d1898e282afcd68be65
Renata Inojosa <inojosa.renata@gmail.com>
2022-10-10 21:52:18 -0300

    iterations 1 and 3 done

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
+const { get } = require('spotify-web-api-node/src/http-manager');
 
 const app = express();
 
@@ -12,7 +14,32 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+
+  spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+app.get('/artist-search', (req, res) => {
+    spotifyApi
+    .searchArtists('Love')
+    .then((data) => {
+        console.log('The received data from the API: ', data.body)
+        res.json(data.body.artists.items)
+    })
+    .catch((err) => console.error('The error while searching artists occurred: ', err))
+});
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+
+
+
+
+
+
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log('My Spotify project running on port 5000 🎧 🥁 🎸 🔊'));
