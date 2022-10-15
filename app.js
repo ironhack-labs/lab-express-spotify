7554f3e2f89d3cdc9965375fb963fba3ca7adb55
MoniIgnacio <moniignacio02@gmail.com>
2022-10-15 23:38:34 +0400

    add It-4 done

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static(__dirname + '/public'));
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRECT
+    clientSecret: process.env.CLIENT_SECRET
 });
 
 spotifyApi
@@ -22,5 +22,35 @@ spotifyApi
 .catch(error => console.log('Something went wrong', error));
 
 // Our routes go here:
+app.get('/', (req,res,next) =>{
+    res.render('home')
+})
+
+app.get('/artist-search',(req,res,next) => {
+    spotifyApi.searchArtists('artist')
+    .then((response) => {
+        // console.log(response.body.artists);
+        res.render('artist-search-results', {
+            details: response.body.artists.items,
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
+
+app.get('/albums/:artistId',(req,res,next) => {
+    let {id} = req.params
+    spotifyApi.getArtistAlbums(id)
+    .then((response) => {
+        console.log(response);
+        res.render('albums.hbs',{
+            eachArtist: response
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
