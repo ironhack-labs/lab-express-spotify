204d7ce01a78d12e68e05012124a2f94596e43c6
Alfie Collins <alfiecollins44@hotmail.co.uk>
2022-11-10 13:00:49 +0000

    more changes

--- app.js
+++ app.js
@@ -46,4 +46,18 @@ app.get("/artist-search", (req, res) => {
     );
 });
 
+ //To albums search
+app.get('/albums/:id', (req, res) => {
+  const { id } = req.params;
+  spotifyApi
+  .getArtistAlbums(id)
+  .then((data) => {
+    res.render("albums",  data.body );
+  })
+  .catch((err) =>
+    console.log("The error while searching artists occurred: ", err)
+  );
+});
+
+
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
