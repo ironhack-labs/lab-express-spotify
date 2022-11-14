7cbb8d827c621a8aaef120926be654a5d3600a67
Vika Shcherbak <vikashcherbaka@gmail.com>
2022-11-14 18:17:50 +0100

    done with a bit of styling WIP

--- app.js
+++ app.js
@@ -25,17 +25,41 @@ spotifyApi
   );
 
 // Our routes go here:
+// Home:
 app.get("/", (req, res, next) => res.render("index.hbs"));
 
 app.get("/artist-search", (req, res, next) => {
     const {queryArtist} = req.query;
     spotifyApi
-    .searchArtists(queryArtist, {limit: 1})
+    .searchArtists(queryArtist, {limit: 10})
     .then(data => {
-        console.log('The received data from the API: ', data.body.artists.items);
-        res.render("artist-search-results.hbs", {artist: data.body.artists.items[0]});
+        console.log('The received data from the API: ', data.body.artists.items[0]);
+        res.render("artist-search-results.hbs", {artist: data.body.artists.items});
     })
     .catch(err => console.log('The error while searching artists occurred: ', err));
+});
+
+// Albums:
+app.get('/albums/:artistId', (req, res, next) => {
+   const {artistId} = req.params;
+   spotifyApi
+   .getArtistAlbums(artistId)
+   .then(data => {
+    console.log('Artist albums', data.body.items);
+    res.render("albums.hbs", {albums: data.body.items})
+  })
+  .catch(err => console.log('The error while searching albums occurred: ', err));
+});
+
+//Tracks
+app.get('/tracks/:albumsId', (req, res, next) => {
+  const {albumsId} = req.params;
+  spotifyApi.getAlbumTracks(albumsId, { limit : 10, offset : 0 })
+  .then(data => {
+    console.log(data.body);
+    res.render("tracks.hbs", {tracks: data.body.items})
+  })
+  .catch(err => console.log('The error while searching albums occurred: ', err));
 });
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
