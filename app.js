80f4e50752573b1ee4c02c78d9803a4d9bc069eb
Adam Hannath <ahannath@gmail.com>
2022-11-15 14:32:08 +0100

    getting there

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-require('dotenv').config();
+require("dotenv").config();
+
 const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
 const SpotifyWebApi = require ('spotify-web-api-node');
+
+
+
+// setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+.clientCredentialsGrant()
+.then((data) => {
+    // console.log(data);
+    spotifyApi.setAccessToken(data.body.access_token)
+})
+.catch((error) => 
+console.log('Something went wrong when retrieving an access token', error));
 
 const app = express();
 
@@ -12,59 +32,53 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 
-// setting the spotify-api goes here:
-const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-  });
-  
-  // Retrieve an access token
-  spotifyApi
-    .clientCredentialsGrant()
-    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
-    .catch(error => console.log('Something went wrong when retrieving an access token', error));
-
-
-    // Our routes go here:
-    app.get('/', (req, res) => {
-        res.render('search');
+// Our routes go here:
+app.get("/", (req, res) => {
+    res.render("index");
     });
     
 
     app.get('/artist-search', (req, res) => {
-        console.log(req.query.artist);
-        const searchArtist = req.query.artist;
+        // console.log(req.query.theArtistName, "helloooo");
+        // const searchArtist = req.query.theArtistName;
         spotifyApi
-        .searchArtists(searchArtist)
-        .then(data => {
+        .searchArtists(req.query.theArtistName)
+        .then((data) => {
         //   console.log('The received data from the API: ', data.body.artists.items);
-        res.render('artist-search', {artists: data.body.artists.items});
+        // console.log("DATA", data)
+        res.render('artist-search-results', {artists: data.body.artists.items});
         })
-        .catch(err => console.log('The error while searching artists occurred: ', err));res.render('artist-search.hbs');
+        .catch(err => console.log('The error while searching artists occurred: ', err));
     });   
 
 
-    app.get('/albums/:artistId', (req, res, next) => {
-        const artistAlbums = req.params.artistId;
+    app.get("/albums/:artistId", (req, res) => {
+        // console.log(req.params.artistId, "WATCHA")
         spotifyApi
-        .getArtistAlbums(artistAlbums)
+        .getArtistAlbums(req.params.artistId)
         .then((data) => {
-        //   console.log('The received data from the API: ', data.body.items);
-        res.render("albums", {albums: data.body.items});
+            res.render("albums", {
+                albums: data.body.items,
+            });
+            // console.log(
+            //   'The received data from the API: ', 
+            //   data.artists.name
+            // )
         })
-        .catch(err => console.log('The error while searching artists occurred: ', err));res.render('artist-search.hbs');
+        .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
       });
 
     
-      app.get('/tracks/:albumId', (req, res, next) => {
-        const albumTracks = req.params.albumId;
+      app.get('/tracks/:albumId', (req, res) => {
         spotifyApi
-        .getAlbumTracks(albumTracks)
+        .getAlbumTracks(req.params.trackId)
         .then((data) => {
-        //   console.log('The received data from the API: ', data.body.items);
-        res.render("tracks", {tracks: data.body.items});
+            res.render("tracks", {tracks: data.body.items});
+            //   console.log('The received data from the API: ', data.body.items);
         })
-        .catch(err => console.log('The error while searching artists occurred: ', err));res.render('artist-search.hbs');
+        .catch(err => console.log('The error while searching tracks occurred: ', err));res.render('track.hbs');
       });
 
     
