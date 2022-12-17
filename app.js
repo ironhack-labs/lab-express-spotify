9660e4196a4d0d40a70da0466218da002f8838de
Thor Löfgren <lofgrenthor@gmail.com>
2022-12-17 18:57:15 +0100

    Create hbs layout file

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.static(__dirname + "/public"));
 // Our routes go here:
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("home");
 });
 
 app.listen(3000, () =>
