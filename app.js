e5647b2101153ab81b4e625435d3722c325b9f4c
Rudolf Teibtner <rudolfteibtner@mailbox.org>
2022-12-29 15:38:05 +0100

    add routes

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// module that loads environment variables from a .env file into process.env
+//         ^
+//         |
 require("dotenv").config();
 
 const express = require("express");
@@ -5,13 +8,7 @@ const hbs = require("hbs");
 
 // require spotify-web-api-node package here:
 const SpotifyWebApi = require("spotify-web-api-node");
-
-const app = express();
-
-// HBS will be in charge of rendering the HTML:
-app.set("view engine", "hbs");
-app.set("views", __dirname + "/views");
-app.use(express.static(__dirname + "/public"));
+// this package will give you access to the methods to communicate to the DB that holds the data we need
 
 // setting the spotify-api goes here:
 // we are getting the info from th .env file (but it is being ignored by git)
@@ -28,62 +25,50 @@ spotifyApi
     console.log("Something went wrong when retrieving an access token", error)
   );
 
-// Our routes go here:
-// home route
-app.get("/", (req, res, next) => {
+const app = express();
+
+// HBS will be in charge of rendering the HTML:
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
+
+// ***************************************************************
+// ROUTES:
+// ***************************************************************
+
+// home route, ROUTE 1: DISPLAY THE FORM TO USERS SO THEY CAN SEARCH FOR THE ARTISTS
+app.get("/", (req, res) => {
   res.render("home");
 });
 
-//search artist route
-app.get("/artist-search", (req, res, next) => {
+//search artist route, ROUTE 2: SUBMIT THE FORM
+app.get("/artist-search", (req, res) => {
   spotifyApi
-    .searchArtists("Alyona Alyona")
+    .searchArtists(req.query.artistName)
     .then((resultOfSearch) => {
-      const result = {
-        artistName: resultOfSearch.body.artists.items[0]["name"],
-        artistImage: resultOfSearch.body.artists.items[0].images[0]["url"],
-        artistId: resultOfSearch.body.artists.items[0]["id"],
-      };
-      console.log(resultOfSearch.body.artists.items[0]["id"]);
-      res.render("artist-search-results", result);
+      res.render("artist-search-results", {
+        artists: resultOfSearch.body.artists.items,
+      });
     })
     .catch((err) =>
       console.log("The error while searching artists occurred: ", err)
     );
 });
 
-//search album route
-app.get("/artist-search/:artistname", (req, res, next) => {
-  spotifyApi
-    .searchArtists(req.params.artistname)
-    .then((resultOfSearch) => {
-      const result = {
-        artistName: resultOfSearch.body.artists.items[0]["name"],
-        artistImage: resultOfSearch.body.artists.items[0].images[0]["url"],
-        artistId: resultOfSearch.body.artists.items[0]["id"],
-      };
-      res.render("artist-search-results", result);
-    })
-    .catch((err) =>
-      console.log("The error while searching artists occurred: ", err)
-    );
-});
-
+//search album route, ROUTE 3: THE DETAILS OF A SPECIFIC ARTIST BASED ON THE UNIQUE ID (WHICH GETS CAPTURED FROM THE URL)
 // Get albums by a certain artist
-app.get("/albums/:artistId", (req, res, next) => {
-  spotifyApi.getArtistAlbums("artistId").then(
-    (data) => {
-      const result = {
-        albumTitle: resultOfSearch.body.artists.items[0]["name"],
-        albumCover: resultOfSearch.body.artists.items[0].images[0]["url"],
-        artistId: resultOfSearch.body.artists.items[0]["id"],
-      };
-      res.render("album-search-results", data);
-    },
-    (err) => {
-      console.error(err);
-    }
-  );
+app.get("/albums/:artistId", (req, res) => {
+  // console.log("Id is: ", req.params.theId);
+  spotifyApi
+    .getArtistAlbums(req.params.artistId)
+    .then((data) => {
+      res.render("albums", {
+        albums: data.body.items,
+      });
+    })
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
 });
 
 app.listen(4000, () =>
