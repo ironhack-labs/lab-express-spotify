eec1a66ece9f57187e762be56d4011587d1a7185
Nicole <92593875+ozzleme@users.noreply.github.com>
2023-01-02 22:36:15 +0530

    iteration view tracks added

--- app.js
+++ app.js
@@ -56,17 +56,31 @@ app.get("/artist-search-results", (req, res, next) => {
 });
 
 app.get("/albums/:id", (req, res, next) => {
-spotifyApi
-.getArtistAlbums(req.params.id)
-.then((data) => {
-    res.render("albums", {
-        albums: data.body.items,
-    });
-      console.log('Artist albums', data.body.items);
-    })
-    .catch((err) =>
-            console.log("The error while searching artists occurred: ", err)
-  );
+    spotifyApi
+        .getArtistAlbums(req.params.id)
+        .then((data) => {
+            res.render("albums", {
+                albums: data.body.items,
+            });
+            console.log('Artist albums', data.body.items);
+        })
+        .catch((err) =>
+            console.log("The error while searching albums occurred: ", err)
+        );
+});
+
+app.get("/tracks/:id", (req, res, next) => {
+    spotifyApi
+        .getAlbumTracks(req.params.id)
+        .then((data) => {
+            res.render("tracks", {
+                tracks: data.body.items,
+            });
+            console.log('Artist tracks', data.body.items);
+        })
+        .catch((err) =>
+            console.log("The error while searching tracks occurred: ", err)
+        );
 });
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
