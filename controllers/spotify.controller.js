3720d1b296ba14e15875a3b65ff857dda266c981
leandropereyradev <leandro-pereyra@hotmail.es>
2023-01-22 23:04:28 +0100

    Partials and Solved

--- controllers/spotify.controller.js
+++ controllers/spotify.controller.js
@@ -1,14 +1,20 @@
 const spotifyApi = require("../config/spotify.config");
 
 module.exports.home = (req, res) => {
-  res.render("pages/home");
+  res.render("pages/render", { home: true });
 };
 
 module.exports.result = (req, res) => {
   spotifyApi
     .searchArtists(req.query.artist)
     .then((data) => {
-      res.render("pages/artist-search-results", { artists: data.body.artists.items });
+      const newData = data.body.artists.items;
+      newData.forEach((e) => {
+        e.ref = "albums";
+        e.link = "View Albums";
+      });
+
+      res.render("pages/render", { search: true, data: newData });
     })
     .catch((err) => console.log("The error while searching artists occurred: ", err));
 };
@@ -17,7 +23,13 @@ module.exports.albums = (req, res) => {
   spotifyApi
     .getArtistAlbums(req.params.id)
     .then((data) => {
-      res.render("pages/albums", { albums: data.body.items });
+      const newData = data.body.items;
+      newData.forEach((e) => {
+        e.ref = "tracks";
+        e.link = "View Tracks";
+      });
+
+      res.render("pages/render", { search: true, data: newData });
     })
     .catch((err) => console.log("The error while searching artists occurred: ", err));
 };
@@ -26,8 +38,7 @@ module.exports.tracks = (req, res) => {
   spotifyApi
     .getAlbumTracks(req.params.tracks)
     .then((data) => {
-      // console.log(data.body.items)
-      res.render("pages/tracks", { tracks: data.body.items });
+      res.render("pages/render", { track: true, data: data.body.items });
     })
     .catch((err) => console.log("The error while searching artists occurred: ", err));
 };
