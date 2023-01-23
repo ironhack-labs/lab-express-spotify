bff4d5057e4fceea0202ec5b02f2da4bf9fc3ae4
leandropereyradev <leandro-pereyra@hotmail.es>
2023-01-23 08:38:12 +0100

    Remove partials

--- app.js
+++ app.js
@@ -2,15 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 const hbs = require("hbs");
-const path = require("path");
 
 const app = express();
 
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/public"));
-
-hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 const routes = require("./config/routes.config");
 app.use(routes);
