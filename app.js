8897be4baac50d61bdf611b82a3f742ac5974e2d
Erin <erica@Erin.local>
2023-01-29 19:31:50 +0100

    Done without styles

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
+const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
 
-// setting the spotify-api goes here:
+// Router:
 
-// Our routes go here:
+const router = require("./config/routes.config");
+app.use(router);
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.listen(3000, () =>
+  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+);
---
+++ controllers/spotify.controller.js
@@ -0,0 +1,38 @@
+const spotifyApi = require("../config/spotify.config");
+
+module.exports.home = (req, res) => {
+  res.render("home");
+};
+
+module.exports.search = (req, res) => {
+  spotifyApi
+    .searchArtists(req.query.search)
+    .then((data) => {
+      res.render("search", { artists: data.body.artists.items });
+    })
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
+};
+
+module.exports.albums = (req, res) => {
+  spotifyApi
+    .getArtistAlbums(req.params.artistId)
+    .then((data) => {
+      res.render("albums", { albums: data.body.items });
+    })
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
+};
+
+module.exports.tracks = (req, res) => {
+  spotifyApi
+    .getAlbumTracks(req.params.albumId)
+    .then((data) => {
+      res.render("tracks", { tracks: data.body.items });
+    })
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
+};
