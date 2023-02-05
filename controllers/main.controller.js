1eef335130a1964b2f2042808f80d1054c084559
patri <patricia.mareg@gmail.com>
2023-02-05 12:13:17 +0100

    finished

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,3 +1,32 @@
+const spotifyApi = require("../config/spotifyApi.config")
+
 module.exports.home = (req, res, next) => {
   res.render("home")
+}
+
+module.exports.artists = (req, res, next) => {
+  spotifyApi
+    .searchArtists(req.query.artist)
+    .then(data => {
+      console.log("datita", data.body.artists.items)
+      res.render("artist-search-results", { artists: data.body.artists.items, search: req.query.artist });
+      // ----> 'HERE'S WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
+}
+
+module.exports.albums = (req, res, next) => {
+  spotifyApi
+    .getArtistAlbums(req.params.id)
+    .then(data => res.render("artist-albums", { albums: data.body.items }))
+    .catch(next)
+}
+
+module.exports.tracks = (req, res, next) => {
+  spotifyApi
+    .getAlbumTracks(req.params.id)
+    .then(data => {
+      res.render("album-tracks", { tracks: data.body.items })
+    })
+    .catch(next)
 }
--- routers/main.router.js
+++ routers/main.router.js
@@ -3,4 +3,10 @@ const controller = require("../controllers/main.controller")
 
 router.get("/", controller.home)
 
+router.get("/artist-search", controller.artists)
+
+router.get("/albums/:id", controller.albums)
+
+router.get("/tracks/:id", controller.tracks)
+
 module.exports = router
