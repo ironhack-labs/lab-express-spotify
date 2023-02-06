a51462c7be85ad5529ce538f943de1b36322f698
Paul Dombrowski <paul.dombr@googlemail.com>
2023-02-06 17:26:55 +0100

    done without design

---
+++ data from server.js
@@ -0,0 +1,65 @@
+{
+    body: {
+      artists: {
+        href: 'https://api.spotify.com/v1/search?query=fgfg&type=artist&offset=0&limit=20',
+        items: [Array],
+        limit: 20,
+        next: null,
+        offset: 0,
+        previous: null,
+        total: 10
+      }
+    },
+    headers: {
+      'content-type': 'application/json; charset=utf-8',
+      'cache-control': 'public, max-age=7200',
+      'x-robots-tag': 'noindex, nofollow',
+      'access-control-allow-origin': '*',
+      'access-control-allow-headers': 'Accept, App-Platform, Authorization, Content-Type, Origin, Retry-After, Spotify-App-Version, X-Cloud-Trace-Context, client-token, content-access-token',
+      'access-control-allow-methods': 'GET, POST, OPTIONS, PUT, DELETE, PATCH',
+      'access-control-allow-credentials': 'true',
+      'access-control-max-age': '604800',
+      'content-encoding': 'gzip',
+      'strict-transport-security': 'max-age=31536000',
+      'x-content-type-options': 'nosniff',
+      date: 'Mon, 06 Feb 2023 14:41:48 GMT',
+      server: 'envoy',
+      via: 'HTTP/2 edgeproxy, 1.1 google',
+      'alt-svc': 'h3=":443"; ma=2592000,h3-29=":443"; ma=2592000',
+      connection: 'close',
+      'transfer-encoding': 'chunked'
+    },
+    statusCode: 200
+
+
+
+    {
+        external_urls: {
+          spotify: 'https://open.spotify.com/artist/2h9CWr2xFLGOpxweE33PMp'
+        },
+        followers: { href: null, total: 458 },
+        genres: [],
+        href: 'https://api.spotify.com/v1/artists/2h9CWr2xFLGOpxweE33PMp',
+        id: '2h9CWr2xFLGOpxweE33PMp',
+        images: [ [Object], [Object], [Object] ],
+        name: 'Zu',
+        popularity: 33,
+        type: 'artist',
+        uri: 'spotify:artist:2h9CWr2xFLGOpxweE33PMp'
+      }
+
+
+
+
+      external_urls: {
+        spotify: 'https://open.spotify.com/album/3kHmOmKMOSvp5CCpFf1Wve'
+      },
+      href: 'https://api.spotify.com/v1/albums/3kHmOmKMOSvp5CCpFf1Wve',
+      id: '3kHmOmKMOSvp5CCpFf1Wve',
+      images: [ [Object], [Object], [Object] ],
+      name: 'FGFC8x2',
+      release_date: '2012-11-15',
+      release_date_precision: 'day',
+      total_tracks: 16,
+      type: 'album',
+      uri: 'spotify:album:3kHmOmKMOSvp5CCpFf1Wve'
