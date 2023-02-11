8cf5bdc0574ec5031a81ff659e23f78e659281d6
Johannes Chorzempa <jchorzempa@posteo.org>
2023-02-11 16:32:28 +0100

    Solved iterations 1 & 2

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
+const spotifyApi = new SpotifyWebApi({
+	clientId: process.env.CLIENT_ID,
+	clientSecret: process.env.CLIENT_SECRET,
+});
+spotifyApi
+	.clientCredentialsGrant()
+	.then((data) => spotifyApi.setAccessToken(data.body['access_token']))
+	.catch((error) => console.log('Something went wrong when retrieving an access token', error));
 
 const app = express();
 
