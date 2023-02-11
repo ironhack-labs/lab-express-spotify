5b9880d615a82fe4bca14dd4c4aab19b1ff79a4a
Wilkins Morales <wilkinscom@gmail.com>
2023-02-11 19:24:08 +0200

    it 3 step 1

--- app.js
+++ app.js
@@ -15,18 +15,32 @@ app.use(express.static(__dirname + "/public"));
 
 // setting the spotify-api goes here:
 
-  const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-  });
-  
-  // Retrieve an access token
-  spotifyApi
-    .clientCredentialsGrant()
-    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
-    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+  .catch((error) =>
+    console.log("Something went wrong when retrieving an access token", error)
+  );
 
 // Our routes go here:
+
+// home
+app.get("/", (req, res, next) => {
+  res.render("index.hbs");
+});
+
+// 404 page
+app.get("*", (req, res, next) => {
+  res.send(
+    "404 error - Whatever it is that you are looking for... it ain't here."
+  );
+});
 
 app.listen(port, () =>
   console.log(`My Spotify project 🏃 on port ${port} 🎧 🥁 🎸 🔊`)
