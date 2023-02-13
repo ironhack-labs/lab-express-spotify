432ea0b781bb292b7cd08eac3891a370aaa046cd
Fernanda Souza <fecafumee@gmail.com>
2023-02-13 21:50:54 +0100

    Iteration 1-5

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -11,8 +12,57 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch((error) =>
+    console.log('Something went wrong when retrieving an access token', error),
+  );
 
 // Our routes go here:
+app.get('/home-page', (req, res) => {
+  res.render('home-page');
+});
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.get('/artist-search', (req, res) => {
+  spotifyApi
+    .searchArtists(req.query.artist)
+    .then((data) => {
+      res.render('artist-search-results', { artists: data.body.artists.items });
+    })
+    .catch((err) =>
+      console.log('The error while searching artists occurred: ', err),
+    );
+});
+
+app.get('/albums/:artistId', (req, res, next) => {
+  // .getArtistAlbums() code goes here
+  spotifyApi.getArtistAlbums(req.params.artistId).then((data) => {
+    res.render('album-search-results', {
+      albums: data.body.items,
+      name: data.body.items[0].artists[0].name,
+    });
+  });
+});
+
+app.get('/tracks/:albumId', (req, res, next) => {
+  spotifyApi.getAlbumTracks(req.params.albumId).then((data) => {
+    console.log(data.body);
+    res.render('track-search-results', {
+      tracks: data.body.items,
+    });
+  });
+});
+
+app.listen(3000, () =>
+  console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'),
+);
