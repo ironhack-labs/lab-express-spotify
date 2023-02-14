8afa795209d544a3534621ba6c2986c09500bf87
BodyElvis <bodymoliki@gmail.com>
2023-02-14 09:13:42 +0100

    Lab-Solution-Express-Spotify

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
 
+const SpotifyWebApi = require('spotify-web-api-node');
+
 const app = express();
 
 app.set('view engine', 'hbs');
@@ -13,6 +15,89 @@ app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
 
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
 // Our routes go here:
 
+// Route 1: form for displaying artists
+
+app.get('/', (req, res) => {
+    res.render('index', {
+      title: 'Spotify Artist Search'
+    });
+  });
+  
+
+// Route 2: form for submit
+
+app.get('/artist-search', (req, res) => {
+    const query = req.query.artist;
+  
+    spotifyApi
+      .searchArtists(query)
+      .then(data => {
+        const artists = data.body.artists.items;
+  
+        res.render('artist-search-results', {
+          title: `Results for "${query}"`,
+          artists
+        });
+      })
+      .catch(err => {
+        console.log('The error while searching artists occurred: ', err);
+        res.render('artist-search-results', {
+          title: `Error searching for "${query}"`,
+          error: err
+        });
+      });
+  });
+  
+
+// Route 3: Specific artist based on id
+
+app.get('/albums/:artistId', (req, res, next) => {
+    const artistId = req.params.artistId;
+    spotifyApi
+      .getArtistAlbums(artistId)
+      .then(data => {
+        const albums = data.body.items;
+        res.render('albums', { albums });
+      })
+      .catch(err => console.log('The error while retrieving albums occurred: ', err));
+  });
+  
+
+// Route 4: Specific track based
+
+app.get('/tracks/:albumId', (req, res, next) => {
+    const albumId = req.params.albumId;
+  
+    spotifyApi
+      .getAlbumTracks(albumId)
+      .then(data => {
+        console.log('The received data from the API: ', data.body);
+  
+        // ----> 'HERE'S WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+        // You can extract the required information from the received data and render it to the tracks.hbs template.
+        res.render('tracks', {
+          tracks: data.body.items,
+          albumId: albumId
+        });
+      })
+      .catch(err => console.log('The error while getting album tracks occurred: ', err));
+  });
+  
+
+
+
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+
