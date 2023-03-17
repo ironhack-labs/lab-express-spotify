8abe9f4dd893e18d3dcae94e4e6384b7f518388b
valyy151 <marin.valenta1@gmail.com>
2023-03-17 17:26:49 +0100

    Rendered album list with images and buttons

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
 
 const SpotifyWebApi = require('spotify-web-api-node');
 
@@ -25,6 +26,21 @@ spotifyApi
 // Our routes go here:
 app.get('/', (req, res) => {
 	res.render('homepage');
+});
+
+app.get('/artist-search', (req, res) => {
+	console.log(req.query.search);
+	spotifyApi
+		.searchArtists(req.query.search)
+		.then((data) => {
+			const { body } = data;
+			const { artists } = body;
+			const { items } = artists;
+
+			console.log('The received data from the API: ', items[0]);
+			res.render('artist-search-results', { items });
+		})
+		.catch((err) => console.log('The error while searching artists occurred: ', err));
 });
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
