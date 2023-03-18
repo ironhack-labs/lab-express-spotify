f9f5a818b724b6a58210b7ee4aa42a948194689f
Paulius Mockevicius <pmockevicius@gmail.com>
2023-03-18 16:16:19 +0100

    complete all iterations and styling

--- app.js
+++ app.js
@@ -3,80 +3,67 @@ require('dotenv').config();
 const express = require('express');
 const hbs = require('hbs');
 
-// require spotify-web-api-node package here:
+// requiring spotify-web-api-node package
 const SpotifyWebApi = require('spotify-web-api-node');
-
 const app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-// setting the spotify-api goes here:
+// setting the spotify-api:
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
-  });
-  
-  // Retrieve an access token
-  spotifyApi
+});
+
+// Retrieving an access token
+spotifyApi
     .clientCredentialsGrant()
     .then(data => spotifyApi.setAccessToken(data.body['access_token']))
     .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
-// Our routes go here:
+// My routes:
 
-app.get("/", (req, res, next)=>{
+app.get("/", (req, res, next) => {
     res.render("home")
 })
 
-
-
-
 app.get('/artist-search', (req, res) => {
     spotifyApi
-  .searchArtists(req.query.artist)
-  .then(artistsArray => {
-
-    // console.log('The received data from the API: ', artistsArray.body.artists.items);
-
-    const data = {
-        artists: artistsArray.body.artists.items
-    }
-    res.render('artist-search-results', data);
-  })
-  .catch(err => console.log('The error while searching artists occurred: ', err));
+        .searchArtists(req.query.artist)
+        .then(artistsArray => {
+            const data = {
+                artists: artistsArray.body.artists.items
+            }
+            res.render('artist-search-results', data);
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
 })
 
 app.get('/albums/:artistId', (req, res, next) => {
-    // .getArtistAlbums() code goes here
     artistId = req.params.artistId;
     spotifyApi.getArtistAlbums(artistId)
-    .then(function(albumsData) {
-       
-        //   console.log('******** ******** Artist albums', data);
-          res.render('albums', {albumsData});
+        .then(function (albumsData) {
+            res.render('albums', { albumsData });
         },
-        function(err) {
-          console.error(err);
-        }
-      );
-  });
+            function (err) {
+                console.error(err);
+            }
+        );
+});
 
-  app.get('/tracks/:tracksId', (req, res, next) => {
-    // .getArtistAlbums() code goes here
+app.get('/tracks/:tracksId', (req, res, next) => {
     tracksId = req.params.tracksId;
     spotifyApi.getAlbumTracks(tracksId)
-    .then(function(tracksData) {
-       
-          console.log('******** ******** Tracks data ', tracksData.body.items[0].preview_url);
-          res.render('tracks', {tracksData});
+        .then(function (tracksData) {
+            res.render('tracks', { tracksData });
         },
-        function(err) {
-          console.error(err);
-        }
-      );
-  });
+            function (err) {
+                console.error(err);
+            }
+        );
+});
 
 
 
