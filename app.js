a0bc3927dd26abaf4f872cbaede674840f1c31f1
Hugo Silva <dasilvapapis@protonmail.com>
2023-03-20 03:14:56 +0000

    search engine is working, but not able to play the tracks

--- app.js
+++ app.js
@@ -25,32 +25,55 @@ spotifyApi
   .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+
+//Route for homepage
+app.get("/", (req, res, next) => {
+    res.render("home");
+})
+
+
 app.get('/artist-search', (req, res, next) => {
-    const artist = req.query.artist;
+  const artist = req.query.artist;
 
-  spotifyApi.searchArtists('artist')
-    .then(data => {
-      console.log('The received data from the API: ', data.body);
-      // ----> 'HERE'S WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
-    })
-    .catch(err => console.log('The error while searching artists occurred: ', err));
-  });    
-
-
-  app.get('/albums/:artistId', (req, res, next) => {
-    // .getArtistAlbums() code goes here
-    const allAlbums = req.params.artistId;
-    console.log(allAlbums);
-
-    spotifyApi.getArtistAlbums(allAlbums)
+  spotifyApi.searchArtists(artist)
       .then(data => {
-      console.log('Artist albums', data.body);
+          console.log(data.body.artists.items[0])
+          // ----> 'HERE'S WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+          res.render('artist-search-results', data.body)
       })
-      .catch(err => {
-          console.error(err);
-      });
-    
-  });
+      .catch(err => console.log('The error while searching artists occurred: ', err));
+});
+
+
+app.get('/albums/:artistId', (req, res, next) => {
+    const artistId = req.params.artistId;
+    const artistName = req.query.artist;
+
+    spotifyApi.getArtistAlbums(artistId)
+        .then(data => {
+            console.log('Artist albums', data.body);
+            res.render('albums', { items: data.body.items, artistName })
+        })
+        .catch(err => {
+            console.error(err);
+        });
+});
+
+
+  app.get('tracks/:albumId', (req, res, next) => {
+      const albumId = req.params.albumId;
+      const album = req.query.album;
+
+      spotifyApi.getAlbumTracks(albumId)
+        .then(data => {
+            console.log('Album Tracks:', data.body.items);
+            res.render('tracks', { items: data.body.items, album })
+        })
+        .catch(err => {
+            console.error(err);
+        });
+
+  })
 
 
 
