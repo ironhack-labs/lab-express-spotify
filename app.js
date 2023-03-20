06da0362a9bf11002060445daada2bda5b17cdf6
DavidRamires911 <124219071+DavidRamires911@users.noreply.github.com>
2023-03-20 00:24:03 +0000

    Lab completed

--- app.js
+++ app.js
@@ -36,16 +36,50 @@ app.get("/artist-search",(req,res,next)=>{
     spotifyApi
   .searchArtists(artist)
   .then(data => {
-    //console.log('The received data from the API: ', data.body.artists.items[0].images);
+    console.log('The received data from the API: ', data.body.artists.items[0]);
     const artistArray=data.body.artists.items
     res.render("artist-search-results", {artists: artistArray})
 
   })
   .catch(err => console.log('The error while searching artists occurred: ', err));
-  //  res.render("artistsearch")
+  
 })
 
+app.get('/albums/:artistId', (req, res, next) => {
+  //console.log(req.params.artistId)
+  // Get Elvis' albums
+spotifyApi.getArtistAlbums(req.params.artistId).then(
+  function(data) {
+    console.log('Artist albums', data.body.items[0].images);
+    const albums= data.body.items
+    res.render("albums.hbs", {albums: albums})
+  },
+  function(err) {
+    console.error(err);
+  }
+);
+
+
+});
+
+
+ app.get("/view-tracks/:albumId",(req,res,next)=>{
+  
+
+
+  spotifyApi.getAlbumTracks(req.params.albumId) //id
+  .then(function(data) {
+    console.log(data.body.items[0]);
+    const tracks= data.body.items
+    res.render("view-tracks.hbs", {tracks})
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+  
+ })
 
 
 
-app.listen(3000, () => console.log('My Spotify project running on port 3002 🎧 🥁 🎸 🔊'))
+
+
+app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'))
