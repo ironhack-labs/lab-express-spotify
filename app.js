54c0544bbcb57f266586fa4929cd993dc03220b3
Lena Imdahl <mail@lenaimdahl.de>
2023-04-01 16:42:37 +0200

    some changes

--- app.js
+++ app.js
@@ -26,6 +26,19 @@ spotifyApi
 
 // Our routes go here:
 
+app.get("/", (req, res) => {
+  spotifyApi
+    .searchArtists(/*'HERE GOES THE QUERY ARTIST'*/)
+    .then((data) => {
+      console.log("The received data from the API: ", data.body);
+      // ----> 'HERE'S WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    })
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
+  res.render("index");
+});
+
 app.listen(3000, () =>
   console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
 );
