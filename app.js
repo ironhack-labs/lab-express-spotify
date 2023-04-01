e3cc1d1272bd0f02fa4306a2750efd62bc89a24e
“Zuzana <zuzana@getyourguide.com>
2023-04-01 18:52:24 +0200

    iteration 2 done (maybe)

--- app.js
+++ app.js
@@ -32,21 +32,16 @@ app.get("/", (req, res) => {
     res.render("home");
   });
 
-// const baseAPIRoute = "https://api.spotify.com/v1/artists/";
-// console.log(baseAPIRoute);
-
-// artist rearch route 
+// artist search route 
 app.get("/artist-search", (req, res) => {
+    const artist = req.query.artist
     spotifyApi
-        .searchArtists(req.query.artist)
+        .searchArtists(artist)
         .then(data => {
-            res.send("The artist was sent");
-            console.log('The received data from the API: ', data.body);
-            // const apiRouteToQuery = baseAPIRoute + req.query.name;
-            // const response = get(apiRouteToQuery);
-            // res.render("artist-search-results", { artists: response.data.results});
-        })
+          res.render("artist-search-results", {artists: data.body.artists.items});
+      })
         .catch(err => console.log('The error while searching artists occurred: ', err)); 
 });
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+
+app.listen(4000, () => console.log('My Spotify project running on port 4000 🎧 🥁 🎸 🔊'));
