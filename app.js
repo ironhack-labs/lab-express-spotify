a9ad4ea33a4fa0b82d08c182f09511429d14acac
Lena Imdahl <mail@lenaimdahl.de>
2023-04-01 17:02:55 +0200

    fixed Iterations

--- app.js
+++ app.js
@@ -27,16 +27,20 @@ spotifyApi
 // Our routes go here:
 
 app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/artist-search", (req, res) => {
+  console.log(req.query.artist);
   spotifyApi
-    .searchArtists(/*'HERE GOES THE QUERY ARTIST'*/)
+    .searchArtists(req.query.artist)
     .then((data) => {
       console.log("The received data from the API: ", data.body);
-      // ----> 'HERE'S WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+      res.send("The artist was send"); // ----> 'HERE'S WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
     })
     .catch((err) =>
       console.log("The error while searching artists occurred: ", err)
     );
-  res.render("index");
 });
 
 app.listen(3000, () =>
