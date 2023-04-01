5f31990cf03ca09f97f04bdc3957519b88af0e4e
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-04-01 22:40:56 +0200

    Done

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const app = express();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
@@ -27,23 +29,50 @@ spotifyApi
 
 
 app.get('/', (req, res) => {
-    res.render("layout");
+    res.render("main", {layout: 'layout'});
   })
 
 
 
-app.get('/artist-search', (req,res) => {
-   const {search} = req.query;
+app.post('/artist-search', (req,res) => {
+   const {search} = req.body;
   spotifyApi
   .searchArtists(search)
   .then(data => {
-    console.log('The received data from the API: ', data.body);
+    // console.log('The received data from the API: ', data.body.artists);
+    let result = data.body.artists.items;
+    // console.log(result);
     // ----> 'HERE'S WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
-    
+   res.render("artist-search-results", {result: result});
   })
   .catch(err => console.log('The error while searching artists occurred: ', err));
 })
 
+app.post('/albums/:artistId', (req,res, next) => {
+  const {artistId} = req.params;
+  // console.log(artistId);
+spotifyApi.getArtistAlbums(artistId).then(
+  function(data) {
+    // console.log('Artist albums', data.body);
+    res.render("albums", {albums: data.body.items})
+  },
+  function(err) {
+    console.error(err);
+  }
+);
+})
+
+app.post('/tracks/:albumId', (req,res,next) => {
+  const {albumId} = req.params;
+  spotifyApi.getAlbumTracks(albumId, { limit : 5, offset : 1 })
+  .then(function(data) {
+    console.log(data.body);
+    res.render("tracks", {tracks: data.body.items});
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+
+})
 
 // Our routes go here:
 
