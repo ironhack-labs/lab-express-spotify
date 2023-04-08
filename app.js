e9c149832105121dd7146d9feac53a1a64ad941b
pepealacid <ppalacid@gmail.com>
2023-04-09 00:06:22 +0200

    trabajando en tracks

--- app.js
+++ app.js
@@ -42,11 +42,25 @@ app.get("/artist-search", (req, res, next) => {
     .then((data) => {
       console.log("The received data from the API: ", data.body);
       const artists = data.body.artists.items;
+      console.log(artists[0]);
       res.render("artist-search", { artists });
     })
     .catch((err) =>
       console.log("The error while searching artists occurred: ", err)
     );
+});
+
+app.get("/albums/:name/:id", (req, res, next) => {
+  const { name } = req.params;
+  const { id } = req.params;
+  spotifyApi
+    .getArtistAlbums(id)
+    .then((data) => {
+      const albums = data.body.items;
+      res.render("albums", { name, albums });
+      console.log(albums);
+    })
+    .catch((err) => console.error(err));
 });
 
 app.listen(3000, () =>
