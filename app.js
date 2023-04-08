158164a58447e658ecb103c1266b94d99f252af5
kobetoto <106164290+kobetoto@users.noreply.github.com>
2023-04-08 16:06:08 +0200

    iteration 2/3

--- app.js
+++ app.js
@@ -27,6 +27,24 @@ spotifyApi
   );
 
 // Our routes go here:
+app.get('/', function(req,res,next){
+  res.render('homepage', {})
+})
+
+app.get('/artist-search', function(req, res, next){
+  // 1. faire un call a l'api spotify pour recup les artistes trouves
+  spotifyApi
+    .searchArtists(/*'HERE GOES THE QUERY ARTIST'*/)
+    .then(data => {
+      console.log('The received data from the API: ', data.body);
+      // ----> 'HERE'S WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
+
+  // 2. faire le rendu de la page de artist-search, presentant les resultats la recherche
+res.render('artist-search', {})
+})
+
 
 app.listen(3000, () =>
   console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
