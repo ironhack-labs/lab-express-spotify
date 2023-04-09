d9f6dca61cbf9566e06c7ede585829860ba6b54a
pepealacid <ppalacid@gmail.com>
2023-04-09 15:44:27 +0200

    lab done

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.get("/artist-search", (req, res, next) => {
     .then((data) => {
       console.log("The received data from the API: ", data.body);
       const artists = data.body.artists.items;
-      console.log(artists[0]);
+      //   console.log(artists[0]);
       res.render("artist-search", { artists });
     })
     .catch((err) =>
@@ -51,16 +51,24 @@ app.get("/artist-search", (req, res, next) => {
 });
 
 app.get("/albums/:name/:id", (req, res, next) => {
-  const { name } = req.params;
-  const { id } = req.params;
+  const { name, id } = req.params;
   spotifyApi
     .getArtistAlbums(id)
     .then((data) => {
       const albums = data.body.items;
       res.render("albums", { name, albums });
-      console.log(albums);
+      //   console.log(albums);
     })
     .catch((err) => console.error(err));
+});
+
+app.get("/tracks/:name/:id", (req, res, next) => {
+  const { name, id } = req.params;
+  spotifyApi.getAlbumTracks(id).then((data) => {
+    const tracks = data.body.items;
+    res.render("tracks", { name, tracks });
+    console.log(tracks);
+  });
 });
 
 app.listen(3000, () =>
