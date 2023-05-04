390e16d275d0e7554687f466ac22f4a966eb16d6
Astrid <anaguflo@gmail.com>
2023-05-04 18:00:35 +0200

    spotify

---
+++ db/database.js
@@ -0,0 +1,19 @@
+// ℹ️ package responsible to make the connection with mongodb
+// https://www.npmjs.com/package/mongoose
+const mongoose = require("mongoose");
+
+// ℹ️ Sets the MongoDB URI for our app to have access to it.
+// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
+
+const MONGO_URI =
+    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/params-strings";
+
+mongoose
+    .connect(MONGO_URI)
+    .then((x) => {
+        const databaseName = x.connections[0].name;
+        console.log(`Connected to Mongo! Database name: "${databaseName}"`);
+    })
+    .catch((err) => {
+        console.error("Error connecting to mongo: ", err);
+    });
