9cb78d0597eaf50614d2dbda1080ad34c5f42fb8
LourdesGonzalezT <LourdesGonzalezT@github.com>
2023-05-04 17:59:14 +0200

    done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -11,8 +12,61 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
+
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
+
+
+// Retrieve an access token
+spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
 
 // Our routes go here:
+app.get('/', (req, res) => {
+    res.render('index-page')
+})
+
+
+app.get('/results', (req, res) => {
+
+    const { artist_name } = req.query
+    console.log(artist_name)
+
+
+    spotifyApi
+        .searchArtists(artist_name)
+        .then(data => {
+            console.log('The received data from the API: ', data.body);
+            // res.send(data.body.artists.items)
+            res.render('results-page', { items: data.body.artists.items })
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+
+})
+
+
+app.get('/albums/:artistId', (req, res, next) => {
+    const { artistId } = req.params
+    console.log(artistId)
+
+    spotifyApi
+        .getArtistAlbums(artistId)
+        .then(data => {
+            console.log('The received data from the API: ', data.body);
+            res.send(data.body)
+            // res.render('albums-page', { album: data.body.items })
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+
+});
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+
+
