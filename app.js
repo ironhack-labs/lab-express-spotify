c9c5b83150648965dd636e8040c11492ee784d7a
BernardoGuedesAbreu <bernardoabreu1991@gmail.com>
2023-05-05 17:32:26 +0100

    lab done

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
+
+
 
 const app = express();
 
@@ -12,7 +15,70 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+app.get("/", (req,res)=>{
+    res.render("index")
+ })
+ app.get("/artist-search", (req,res)=>{
+    let search = req.query.artist
+    spotifyApi
+  .searchArtists(search)
+  .then(data => {
+   // console.log('The received data from the API: ', data.body);
+    // ----> 'HERE'S WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    res.render("artist-search-results",{response:data.body})
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+    
+ })
+
+
+app.get('/albums/:id', (req, res) => {
+  const albumId = req.params.id;
+
+  spotifyApi.getArtistAlbums(albumId)
+    .then((data) => {
+      
+
+      // Construct the URL for the tracks page
+      //const tracksUrl = `/tracks/${albumId}`;
+
+      // Render the albums template with the album data and the tracks URL
+      res.render('albums', { album: data.body.items });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+  
+ 
+
+app.get("/tracks/:id", (req, res) => {
+    const albumId = req.params.id;
+    spotifyApi.getAlbumTracks(albumId).then(
+      function (data) {
+        console.log("Album tracks", data.body);
+        // Render the tracks view with the list of tracks for the specified album
+        res.render("tracks", { tracks: data.body.items });
+      },
+      function (err) {
+        console.error(err);
+      }
+    );
+  });
+  
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+
