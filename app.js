12c08c772ecd8cc28f1a2956c2a20b080f66052e
Guy Dagan <guydagan18@gmail.com>
2023-05-07 22:34:09 +0100

    Solved lab

--- app.js
+++ app.js
@@ -1,18 +1,69 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
+const express = require("express");
+const hbs = require("hbs");
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+  .catch((error) =>
+    console.log("Something went wrong when retrieving an access token", error)
+  );
 
 // Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.get("/", (req, res) => {
+  res.render("index.hbs");
+});
+
+app.get("/artist-search/", async (req, res) => {
+  const { artist } = req.query;
+  spotifyApi
+    .searchArtists(artist)
+    .then((data) => {
+      res.render("artist-search-results.hbs", {
+        artistData: data.body.artists.items,
+      });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get("/albums/:artistId", (req, res) => {
+  const { artistId } = req.params;
+  spotifyApi
+    .getArtistAlbums(artistId)
+    .then((albums) => {
+      res.render("albums.hbs", { albumData: albums.body.items });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get("/tracks/:albumId", (req, res) => {
+  const { albumId } = req.params;
+  spotifyApi
+    .getAlbumTracks(albumId)
+    .then((albumTracks) => {
+      res.render("tracks.hbs", { trackData: albumTracks.body.items });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.listen(3000, () =>
+  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+);
