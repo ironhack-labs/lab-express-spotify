a87866deb24038ae51d38ca830ab6498f2f35a40
Filipe Ferreira <filipeferreira94@gmail.com>
2023-06-02 22:22:56 +0100

    Main functionality finished. Many tracks had null on preview_url so I introduced an if helper. displayed a "show on spotify" button. Will apply stylings before monday.

--- app.js
+++ app.js
@@ -2,17 +2,79 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
+const SpotifyWebApi = require('spotify-web-api-node');
 
 // require spotify-web-api-node package here:
 
-const app = express();
+const spotifyApi = new SpotifyWebApi({
+	clientId: process.env.CLIENT_ID,
+	clientSecret: process.env.CLIENT_SECRET,
+});
 
+// Retrieve an access token
+spotifyApi
+	.clientCredentialsGrant()
+	.then((data) => {
+		spotifyApi.setAccessToken(data.body['access_token']);
+		console.log('Worked', data.body['access_token']);
+	})
+
+	.catch((error) =>
+		console.log('Something went wrong when retrieving an access token', error)
+	);
+
+// setting the spotify-api goes here:
+
+const app = express();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-// setting the spotify-api goes here:
-
 // Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.get('/artists', async (req, res, next) => {
+	console.log('artists gotten: ', req.query.artistInfo);
+	try {
+		const artists = await spotifyApi.searchArtists(req.query.artistInfo);
+		const artistsArray = artists.body.artists.items;
+		res.render('artist-search-results', { artistsArray });
+	} catch (error) {
+		console.log('Error while searching artists: ', error);
+	}
+});
+
+app.get('/albums/:artistId', async (req, res, next) => {
+	/* console.log(req.params.artistId); */
+	const artistId = req.params.artistId;
+
+	try {
+		const artistAlbums = await spotifyApi.getArtistAlbums(artistId);
+		/* console.log(artistAlbums.body.items[0]); */
+		const artistAlbumsArray = artistAlbums.body.items;
+		res.render('albums', { artistAlbumsArray });
+	} catch (error) {
+		console.log('Unexpected error: ', error);
+	}
+});
+
+app.get('/tracks/:tracksId', async (req, res, next) => {
+	const tracksId = req.params.tracksId;
+	/* console.log(tracksId); */
+
+	try {
+		const artistTracks = await spotifyApi.getAlbumTracks(tracksId);
+		/* console.log(artistTracks.body.items); */
+		const artistTracksObj = artistTracks.body.items;
+		res.render('album-tracks', { artistTracksObj });
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+app.get('/', (req, res, next) => {
+	res.render(__dirname + '/views/home-page');
+});
+
+app.listen(3000, () =>
+	console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')
+);
