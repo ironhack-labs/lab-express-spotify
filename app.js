e621e91679c024c00f0ec793099e5bee61449f0b
hymced <hymced@gmail.com>
2023-06-02 18:52:52 +0200

    solved but no styling (it3.2 + it4 + it5) (no it3 in lab)

--- app.js
+++ app.js
@@ -41,6 +41,48 @@ app.get("/", (req, res, next) => {
 
 app.get("/artist-search", (req, res, next) => {
 	console.log("artist search page requested")
+	spotifyApi
+		.searchArtists(req.query.artist, { limit: 10, offset: 0 })
+		.then(data => {
+			// console.log(data.body);
+			// console.dir(data.body, { depth: null }) // display nested objects instead of [Object]
+			// console.dir(data.body, { depth: 3 })
+			// console.log(data.body.artists.items)
+			// res.render("artist-search-results", {artistsArr: data.body.artists.items})
+			const artistsArr = {artistsArr: data.body.artists.items}
+			res.render("artist-search-results", artistsArr)
+		})
+		.catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
+app.get("/albums/artist-id-:artistId", (req, res, next) => {
+	// console.log("albums page requested")
+	spotifyApi
+		.getArtistAlbums(req.params.artistId, { limit: 10, offset: 0 })
+		.then(data => {
+			// console.log(data.body);
+			// console.dir(data.body, { depth: null }) // display nested objects instead of [Object]
+			// console.dir(data.body, { depth: 3 })
+			// res.render("albums", {albumsArr: data.body.items})
+			const albumsArr = {albumsArr: data.body.items}
+			res.render("albums", albumsArr)
+		})
+		.catch(err => console.log('The error while searching albums occurred: ', err));
+})
+
+app.get("/tracks/album-id-:albumId", (req, res, next) => {
+	// console.log("tracks page requested")
+	spotifyApi
+		.getAlbumTracks(req.params.albumId, { limit: 10, offset: 0 })
+		.then(data => {
+			// console.log(data.body);
+			// console.dir(data.body, { depth: null }) // display nested objects instead of [Object]
+			// console.dir(data.body, { depth: 3 })
+			// res.render("tracks", {tracksArr: data.body.items})
+			const tracksArr = {tracksArr: data.body.items}
+			res.render("tracks", tracksArr)
+		})
+		.catch(err => console.log('The error while searching tracks occurred: ', err));
 })
 
 /******************/
