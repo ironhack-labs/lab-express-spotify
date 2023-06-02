6d3e815bc850b0f3be1088c240d06593eeca70d9
linhvnde <linh.nh.tyty@gmail.com>
2023-06-02 21:04:05 +0200

    Iteration 1-3 done

--- app.js
+++ app.js
@@ -1,18 +1,69 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const hbs = require("hbs");
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
-
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 // setting the spotify-api goes here:
 
-// Our routes go here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID, //credentials we dont want to expose to public, this to ensure the credential is wrap by the env
+  clientSecret: process.env.CLIENT_SECRET,
+});
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+    console.log("workded", data.body["access_token"]);
+  })
+  .catch((error) =>
+    console.log("Something went wrong when retrieving an access token", error)
+  );
+
+//
+//
+// Our routes go here:
+//
+//HOME
+app.get("/", (req, res) => {
+  res.render("home-page");
+});
+//
+// Artist Search
+app.get("/artist-search", (req, res) => {
+  const filter = req.query.artistsSearchInput;
+  console.log(filter);
+  spotifyApi
+    .searchArtists(filter)
+    .then((dataFromSpotifyApi) => {
+      const artistsArr = dataFromSpotifyApi.body.artists.items;
+      console.log(
+        "artistsArr type is",
+        typeof artistsArr,
+        "DETAILS",
+        artistsArr
+      );
+      res.render("artist-search-results", { artistsArr });
+    })
+    .catch((e) => {
+      console.log("FAIL TO SEARCH", e);
+    });
+});
+//
+// Port
+const port = 3000;
+app.listen(port, () =>
+  console.log(
+    `My Spotify project running at http://localhost:${port}/ 🎧 🥁 🎸 🔊`
+  )
+);
