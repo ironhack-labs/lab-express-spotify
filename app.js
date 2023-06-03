9c62810aa498a1e9d042bf5e98dd2460abaaf8e0
Dok <pierredok@outlook.fr>
2023-06-03 17:20:57 +0200

    Iteration 1/2/3 done + style 2

--- app.js
+++ app.js
@@ -3,16 +3,64 @@ require('dotenv').config();
 const express = require('express');
 const hbs = require('hbs');
 
-// require spotify-web-api-node package here:
 
+
+
+
+
+const SpotifyWebApi = require('spotify-web-api-node');
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+
+// Retrieve an access token
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+
+
+  // setting the spotify-api goes here:
+spotifyApi
+.clientCredentialsGrant()
+.then(data => {
+spotifyApi.setAccessToken(data.body['access_token'])
+})
+.catch(error => console.log('Something went wrong when retrieving an access token', error));
+
+// require spotify-web-api-node package here:
 app.use(express.static(__dirname + '/public'));
 
-// setting the spotify-api goes here:
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
 
-// Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+
+app.get("/", (req, res, next) => {
+    
+    res.render("artist-search");
+})
+
+
+app.get("/artist-search", (req, res, next) => {
+    
+    spotifyApi.searchArtists(req.query.artists)
+	.then((ref) => {
+		console.log(ref.body.artists.items[0].id)
+        const dataArr = {
+            dataArr: ref.body.artists.items,
+        }
+    res.render("artist-search-results", dataArr);
+
+	})
+	.catch(error => console.log('The error while searching artists occurred:',error))
+
+})
+
+app.get('/albums/:artistId', (req, res, next) => {
+
+  });
+
+
+
+    // Our routes go here:
+app.listen(3100, () => console.log('My Spotify project running on port 3100 🎧 🥁 🎸 🔊'));
---
+++ models/Pizza.models.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+
+const artistSchema = new Schema({
+    
+
+
+})
