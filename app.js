3ba3ce0945d50a3c1458efa137495d6f4d28c2b0
Lina Zerkly <zerkly.lina@gmail.com>
2023-06-05 00:31:17 +0200

    unstuck on accessing spotify API but stuck on iterations 3,4,5

--- app.js
+++ app.js
@@ -1,66 +1,62 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
-const SpotifyWebApi = require('spotify-web-api-node');
+const express = require("express");
+const hbs = require("hbs");
+const SpotifyWebApi = require("spotify-web-api-node");
 
 // require spotify-web-api-node package here:
 const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-  });
-  
-  // Retrieve an access token
-  spotifyApi
-    .clientCredentialsGrant()
-    .then(data => {
-		spotifyApi.setAccessToken(data.body['access_token'])
-		console.log('Worked', data.body['access_token'])
-	})
-    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
 
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+    console.log("Worked", data.body["access_token"]);
+  })
+  .catch((error) =>
+    console.log("Something went wrong when retrieving an access token", error)
+  );
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
 
 // setting the spotify-api goes here:
-/* app.get("/", (req, res, next) => {
-	spotifyApi.searchArtists('madonna')
-	.then((data) => {
-		console.log(data.body.artists.items[0].images)
-	})
-	.catch(error => console.log(error))
-})
- */
 
-// Our routes go here:
 app.get("/", (req, res, next) => {
-    console.log("homepage")
-    res.sendFile(__dirname + "/views/home-page.html")
-}); 
-
-app.get("/artist-search", (req, res, next) => {
-    const query = req
-    spotifyApi
-    .searchArtists(query.artist)
-    .then(data => { 
-        console.log("The received data from the API :", data.body);
-        res.render("search-results", { artists: data.body.artists.items })
-})
-    .catch(e => console.log("Error while searching for the artist", e));
+  console.log("Homepage");
+  res.render("home");
 });
 
-app.get('/albums/:artistId', (req, res, next) => {
-    spotifyApi.getArtist()
-    .then(function(data) {
-      console.log('Artist information', data.body);
-    }, function(err) {
-      console.error(err);
-    });
-  });
+// Our routes go here:
 
+app.get("/artist-search-results", (req, res, next) => {
+  const query = req;
+  spotifyApi
+    .searchArtists(req.query.artist)
+    .then((data) => {
+      console.log("The received data from the API :", data.body);
+      res.render("artist-search-results", data.body.artist);
+    })
+    .catch((e) => console.log("Error while searching for the artist", e));
+});
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.get("/albums/:artistId", (req, res, next) => {
+  spotifyApi
+    .getArtistAlbums(req.query.id)
+    .then((data) => {
+      console.log("Artist album", data.body);
+      res.render("albums", data.body);
+    })
+    .catch((e) => console.error("error while searching the album", e));
+});
+
+app.listen(3000, () =>
+  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+);
