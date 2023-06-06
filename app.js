9ddd4325312b0b303e995279c97d9703442eb67f
churrundo <churrundo@gmail.com>
2023-06-05 20:53:30 -0600

    stylize albums view

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/public"));
 
-
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
@@ -26,34 +25,44 @@ spotifyApi
   );
 
 // Our routes go here:
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/artist-search', (req, res) => {
-    spotifyApi.searchArtists(req.query.artist)
-      .then(data => {
-        console.log('The received data from the API: ', data.body);
-        res.render('artist-search-results', { artists: data.body.artists.items });
-      })
-      .catch(err => console.log('The error while searching artists occurred: ', err));
+app.get("/artist-search", (req, res) => {
+  spotifyApi
+    .searchArtists(req.query.artist)
+    .then((data) => {
+      console.log("The received data from the API: ", data.body);
+      res.render("artist-search-results", { artists: data.body.artists.items });
+    })
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
+});
+app.get("/albums/:artistId", async (req, res) => {
+  const artistId = req.params.artistId;
 
-      app.get('/albums/:artistId', async (req, res) => {
-        const artistId = req.params.artistId;
-        const data = await spotifyApi.getArtistAlbums(artistId);
-        res.render('albums', { albums: data.body.items });
-      });
+  const artistData = await spotifyApi.getArtist(artistId);
+  const artistName = artistData.body.name;
 
-      app.get('/albums/:albumId/tracks', async (req, res, next) => {
-        try {
-          const data = await spotifyApi.getAlbumTracks(req.params.albumId);
-          const tracks = data.body.items;
-          res.render('tracks', { tracks });
-        } catch(err) {
-          console.error('Error occurred while fetching tracks:', err);
-          next(err);
-        }
-      }); 
+  const albumData = await spotifyApi.getArtistAlbums(artistId);
+
+  res.render("albums", {
+    albums: albumData.body.items,
+    artist: artistName,
+  });
+});
+
+app.get("/albums/:albumId/tracks", async (req, res, next) => {
+  try {
+    const data = await spotifyApi.getAlbumTracks(req.params.albumId);
+    const tracks = data.body.items;
+    res.render("tracks", { tracks });
+  } catch (err) {
+    console.error("Error occurred while fetching tracks:", err);
+    next(err);
+  }
 });
 
 app.listen(3000, () =>
