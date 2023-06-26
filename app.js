49005cd1e087f9430ca0a3bc3bf7e5a75380868d
Florian Luther <hi@gauvainstudios.com>
2023-06-26 13:35:00 +0200

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+CLIENT_ID=83f465ecc1ad42c9afc545b80b8819b3;
+CLIENT_SECRET=5c2efe0507a14ab08c903a9fe362a86f;
 require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -12,7 +15,18 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
