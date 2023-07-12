189d7510e35294c6eb011b7924a346db66e18e01
Marcos Francesquini <francesquini.personal@gmail.com>
2023-07-12 23:51:38 +0200

    showing all albuns

--- app.js
+++ app.js
@@ -28,13 +28,52 @@ app.get("/", (req, res) => {
 })
 //res.render(data.body.artists.items[0].images)
 app.get("/artist-search", (req, res) => {
-    //console.log(req.query.artist)
+    console.log("Artist came from browser - request")
+    console.log(req.query.artist)
+
+    //creating an object
+
+
     spotifyApi.searchArtists(req.query.artist)
-    //.then(data => {console.log(data.body.artists.items)})
-   .then(data => {res.render("artist-search-result",{artist:data.body.artists.items})})
-   .catch(err => console.log(err))     
+        //.then(data => {console.log(data.body.artists.items)})
+        .then((data) => {
+
+            const artistImage = data.body.artists.items[0].images[0].url;
+            const artistName = data.body.artists.items[0].name;
+            const artistId = data.body.artists.items[0].id;
+            // console.log(artistObject)
+            res.render("artist-search-result", { artistImage: artistImage, artistName: artistName, artistId: artistId })
+        })
+
+        //res.render("artist-search-result",{artist:data.body.artists.items})})
+        .catch(err => console.log(err))
 })
 
+
+
+app.get("/albums/:artistId/", (req, res, next) => {
+    // .getArtistAlbums() code goes here
+
+    spotifyApi
+        .getArtistAlbums(req.params.artistId)
+        .then((data) => {
+            const albumItems = data.body.items
+            let arrayDisc = [];
+            albumItems.forEach(itemArray => {
+                let albumComplete = {
+                  albumName: itemArray.name,
+                  albumImg: itemArray.images[0].url,
+                  albumId: itemArray.id
+                };
+                //push na array que criei
+                arrayDisc.push(albumComplete);
+              });
+
+            //console.log(arrayDisc)
+            res.render("albums", { arrayDisc });
+        })
+        .catch(err => console.log(err))
+})
 // Our routes go here:
 
 //
