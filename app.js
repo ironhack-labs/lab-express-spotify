d66328b9800ccdc0db390a5d0cbee024e6fa6575
tngknzr <agustinparedes95@gmail.com>
2023-07-28 18:37:13 +0200

    wepawepawepa

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const hbs = require('hbs');
 
-// require spotify-web-api-node package here:
+
 const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
@@ -12,28 +12,61 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/public/views');
 app.use(express.static(__dirname + '/public'));
 
-// setting the spotify-api goes here:
+
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
   });
   
-  // Retrieve an access token
+  
   spotifyApi
     .clientCredentialsGrant()
     .then(data => spotifyApi.setAccessToken(data.body['access_token']))
     .catch(error => console.log('Something went wrong when retrieving an access token', error));
-// Our routes go here:
+
 app.get('/', (req,res)=>{
     res.render('index')
 
 })
-app.get('/search', (req,res)=>{
+app.get('/albums/:id', (req, res, next) => {
+    const artistId = req.params.id;
+  
+    
+    spotifyApi
+      .getArtistAlbums(artistId)
+      .then((data) => {
+        const albums = data.body.items;
+        
+        const artistName = albums.length ? albums[0].artists[0].name : 'Unknown Artist';
+        res.render('albums', { albums, artistName });
+      })
+      
+      .catch((err) => {
+        console.log('error', err);
+        next(err);
+      });
+  });
+  app.get('/tracks/:id', (req, res, next) => {
+    const albumId = req.params.id;
+  
+    
+    spotifyApi
+      .getAlbumTracks(albumId)
+      .then((data) => {
+        const tracks = data.body.items;
+        res.render('tracks', { tracks });
+      })
+      .catch((err) => {
+        console.log('Error ', err);
+        next(err);
+      });
+  })
+app.get('/artist-search', (req,res)=>{
     const searchQuery = req.query.artist;
-    SpotifyWebApi
-    .searchArtist(searchQuery)
-    .then((art)=>{
-        const band = data.body.artist.items;
+    spotifyApi
+    .search(searchQuery, ['artist'])
+    .then((data)=>{
+        const artists = data.body.artists.items;
         res.render('search',{artists})
     })
     .catch(err=>{
@@ -42,6 +75,7 @@ app.get('/search', (req,res)=>{
     
 
 })
+
 
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
