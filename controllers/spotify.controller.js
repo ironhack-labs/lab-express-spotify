68838d9cdd11662588b8e2c601c1235d6d51554d
Veronica Galvez <veronicagalvez88@gmail.com>
2023-08-03 15:28:28 +0200

    Lab Spotify

---
+++ controllers/spotify.controller.js
@@ -0,0 +1,17 @@
+const spotifyApi = require('../models/spotify');
+
+module.exports.home = (req, res) => {
+  res.render("index", {});
+};
+
+//module.exports.searchArtist
+spotifyApi
+  .searchArtists(artistName)
+  .then(data => {
+    console.log('The received data from the API: ', data.body);
+    res.render('artist/artist-search-resoults', {
+      data: data.body
+    })
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+
