8a106ad30df97e954415619097430a87bf877210
Irene Buceta <irenebuzalocen@gmail.com>
2023-08-03 17:55:56 +0200

    done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const hbs = require('hbs');
 
-// require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');// require spotify-web-api-node package here:
 
 const app = express();
 
@@ -11,8 +11,41 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-// setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));// setting the spotify-api goes here:
 
 // Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/artist-search', (req, res) => {
+    //res.send(req.query.artistName)
+
+
+    spotifyApi
+        .searchArtists(req.query.artistName)
+        .then(data => {
+            console.log('ESTO ES LO QUE TE DEVUELVE LA API AL PASARLE LA QUERY AL METODO SEARCHARTIST: ', data.body.artists.items);
+
+            res.render('artists-search-results', { artists: data.body.artists.items })
+
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+
+})
+
+
+
+
+
+app.listen(5005, () => console.log('My Spotify project running on port 5005 🎧 🥁 🎸 🔊'));
