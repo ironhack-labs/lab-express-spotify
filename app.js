b229c5518b43928462a2901279eb8658bb18e338
Edoardo Giambuzzi <edoardo.giambuzzi@gmail.com>
2023-11-13 21:04:08 +0100

    lab done

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
+const express = require("express");
+const hbs = require("hbs");
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+// Set up view engine and static files
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
 
-// setting the spotify-api goes here:
+// Get Spotify access token
 
-// Our routes go here:
+// Set up Spotify API client
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+    console.log("Spotify access token received and set");
+  })
+  .catch((error) => {
+    console.log("Error retrieving Spotify access token", error);
+  });
+
+// Render index page
+app.get("/", (req, res) => {
+  res.render("index");
+  console.log("OK");
+});
+
+app.get("/artist-search", (req, res) => {
+  res.render("/artist-search");
+});
+
+// Start the server
+app.listen(3000, () =>
+  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+);
