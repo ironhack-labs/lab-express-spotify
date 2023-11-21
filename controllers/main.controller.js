8f9ca85f5d6fdf19dff8bedb903136e671676e98
Lorena Barrera <l.barreranicolas@gmail.com>
2023-11-21 20:58:22 +0100

    solved lab

---
+++ controllers/main.controller.js
@@ -0,0 +1,47 @@
+const spotifyApi = require('../config/spotifySetup');
+
+module.exports.renderHome = (req, res, next) => {
+  res.render('home');
+}
+
+module.exports.getArtistsSearch = (req, res, next) => {
+  const { artist } = req.query;
+
+  if (!artist) {
+    res.redirect('/');
+  }
+
+  spotifyApi
+    .searchArtists(artist)
+    .then(data => {
+      console.log('The received data from the API: ', data.body);
+
+      console.log(data.body.artists.items[0]);
+
+      res.render('artists/artistsSearch', { artists: data.body.artists.items });
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
+}
+
+module.exports.getAlbums = (req, res, next) => {
+  const { artistId } = req.params;
+
+  spotifyApi.getArtistAlbums(artistId)
+    .then(data => {
+      console.log(data.body.items[0])
+      res.render('albums/albums', { albums: data.body.items });
+    })
+    .catch(err => console.log('The error while searching artist albums occurred: ', err))
+
+}
+
+module.exports.getAlbumTracks = (req, res, next) => {
+  const { albumId } = req.params;
+
+  spotifyApi.getAlbumTracks(albumId)
+    .then(data => {
+      console.log(data.body.items);
+      res.render('albums/albumTracks', { tracks: data.body.items });
+    })
+    .catch(err => console.log('The error while searching artist albums occurred: ', err))
+}
---
+++ routes/routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router()
+const mainController = require('../controllers/main.controller');
+
+
+
+router.get('/', mainController.renderHome);
+
+
+router.get('/artist-search', mainController.getArtistsSearch);
+
+
+router.get('/albums/:artistId', mainController.getAlbums);
+
+
+router.get('/albums/:albumId/tracks', mainController.getAlbumTracks);
+
+module.exports = router;
