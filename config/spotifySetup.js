c1e00b2b6bbe2137436fe024a97607a791d26315
Victor Junquera <victorjunquera@eventalista.es>
2023-11-22 16:09:13 +0100

    lab done

---
+++ config/spotifySetup.js
@@ -0,0 +1,9 @@
+const SpotifyWebApi = require('spotify-web-api-node');
+
+// setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  });
+
+module.exports = spotifyApi
